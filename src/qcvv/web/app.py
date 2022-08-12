26bbc48810b0458ab0a7a7ee919373e5f1b1f6a3
Stavros <35475381+stavros11@users.noreply.github.com>
2022-08-12 19:56:19 +0400

    Simplify dash code

--- src/qcvv/web/app.py
+++ src/qcvv/web/app.py
@@ -2,8 +2,7 @@
 import os
 
 import pandas as pd
-import yaml
-from dash import MATCH, Dash, Input, Output, dcc, html
+from dash import Dash, Input, Output, dcc, html
 
 from qcvv import plots
 from qcvv.data import Dataset
@@ -17,37 +16,27 @@ app = Dash(
 app.layout = html.Div(
     [
         dcc.Location(id="url", refresh=False),
-        html.Div(id="page-content"),
-        html.Div(id="blank-output"),
+        dcc.Graph(id="graph"),
+        dcc.Interval(
+            id="interval",
+            # TODO: Perhaps the user should be allowed to change the refresh rate
+            interval=1000,
+            n_intervals=0,
+            disabled=False,
+        ),
     ]
 )
 
 
-@app.callback(Output("page-content", "children"), Input("url", "pathname"))
-def display_dash(url):
-    path = os.path.join(*url.split("/")[2:])
-    return html.Div(
-        [
-            dcc.Graph(id={"type": "graph", "index": path}),
-            dcc.Interval(
-                id={"type": "interval", "index": path},
-                # TODO: Perhaps the user should be allowed to change the refresh rate
-                interval=1000,
-                n_intervals=0,
-                disabled=False,
-            ),
-        ]
-    )
-
-
 @app.callback(
-    Output({"type": "graph", "index": MATCH}, "figure"),
-    Input({"type": "interval", "index": MATCH}, "n_intervals"),
-    Input({"type": "graph", "index": MATCH}, "id"),
-    Input({"type": "graph", "index": MATCH}, "figure"),
+    Output("graph", "figure"),
+    Input("interval", "n_intervals"),
+    Input("graph", "figure"),
+    Input("url", "pathname"),
 )
-def get_graph(n, graph_id, current_figure):
-    folder, format = os.path.split(graph_id.get("index"))
+def get_graph(n, current_figure, url):
+    path = os.path.join(*url.split("/")[2:])
+    folder, format = os.path.split(path)
     folder, method = os.path.split(folder)
     folder, routine = os.path.split(folder)
     folder, _ = os.path.split(folder)
