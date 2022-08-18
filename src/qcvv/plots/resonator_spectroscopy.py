91e3bdfab302fa3c8f8d134a3046cb42e002b15f
DavidSarlle <trapeya25@gmail.com>
2022-08-18 14:58:58 +0400

    qubit spectroscopy flux

--- src/qcvv/plots/resonator_spectroscopy.py
+++ src/qcvv/plots/resonator_spectroscopy.py
@@ -116,3 +116,47 @@ class resonator_punchout:
             yaxis_title="MSR (V)",
         )
         return fig
+
+class resonator_spectroscopy_flux:
+    @staticmethod
+    def frequency_vs_current(data):
+        fig = make_subplots(
+            rows=1,
+            cols=2,
+            horizontal_spacing=0.1,
+            vertical_spacing=0.1,
+            subplot_titles=(
+                "MSR (V)",
+                "phase (deg)",
+            ),
+        )
+
+        fig.add_trace(
+            go.Heatmap(
+                x=data.get_values("frequency", "GHz"),
+                y=data.get_values("current", "A"),
+                z=data.get_values("MSR", "V"),
+                colorbar_x=0.45,
+            ),
+            row=1,
+            col=1,
+        )
+        fig.add_trace(
+            go.Heatmap(
+                x=data.get_values("frequency", "GHz"),
+                y=data.get_values("current", "A"),
+                z=data.get_values("phase", "deg"),
+                colorbar_x=1.0,
+            ),
+            row=1,
+            col=2,
+        )
+        fig.update_layout(
+            showlegend=False,
+            uirevision="0",  # ``uirevision`` allows zooming while live plotting
+            xaxis_title="Frequency (GHz)",
+            yaxis_title="Current (A)",
+            xaxis2_title="Frequency (GHz)",
+            yaxis2_title="Current (A)",
+        )
+        return fig
