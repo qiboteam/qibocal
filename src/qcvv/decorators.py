173a02b94deb8abeecbc41d20459050453cc9ffb
Stavros <35475381+stavros11@users.noreply.github.com>
2022-08-27 23:25:53 +0400

    Add docstrings

--- src/qcvv/decorators.py
+++ src/qcvv/decorators.py
@@ -30,8 +30,17 @@ def store(f):
 
 
 def plot(header, method):
+    """Decorator for adding plots in the report and live plotting page.
+
+    Args:
+        header (str): Header of the plot to use in the report.
+        method (Callable): Plotting method defined under ``qcvv.plots``.
+    """
+
     def wrapped(f):
         if hasattr(f, "plots"):
+            # insert in the beginning of the list to have
+            # proper plot ordering in the report
             f.plots.insert(0, (header, method))
         else:
             f.plots = [(header, method)]
