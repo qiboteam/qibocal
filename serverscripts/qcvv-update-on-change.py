0876db5b2010f26a229ca5a3a0bb5010a8ad9c47
Stefano Carrazza <stefano.carrazza@cern.ch>
2022-08-30 12:26:16 +0200

    using inotify

--- serverscripts/qcvv-update-on-change.py
+++ serverscripts/qcvv-update-on-change.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import argparse
 
 import curio
-from asyncwatch import EVENTS, watch
+import inotify.adapters
+import inotify.constants
 from curio import subprocess
 
 
 async def main(folder, exe_args):
-    watch_events = EVENTS.CREATE | EVENTS.DELETE | EVENTS.MODIFY
-    while True:
-        async with watch(folder, watch_events):
-            await subprocess.run(exe_args)
+    i = inotify.adapters.Inotify()
+    i.add_watch(folder)
+
+    for event in i.event_gen(yield_nones=False):
+        if event is not None:
+            (header, _, _, _) = event
+            if (
+                (header.mask & inotify.constants.IN_CREATE)
+                or (header.mask & inotify.constants.IN_DELETE)
+                or (header.mask & inotify.constants.IN_MODIFY)
+            ):
+                await subprocess.run(exe_args)
 
 
 if __name__ == "__main__":
