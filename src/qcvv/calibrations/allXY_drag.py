fixing allXY probabilitties calculation + improved live plotting for calibrate qubit states

src/qcvv/calibrations/allXY_drag.py
@@ -42,10 +42,16 @@ def allXY(
         ["RY(pi/2)", "RY(pi/2)"],
     ]
 
-    gnd = complex(platform.characterization["single_qubit"][qubit]["state1_voltage"])
-    exc = complex(platform.characterization["single_qubit"][qubit]["state0_voltage"])
+    state0_voltage = complex(
+        platform.characterization["single_qubit"][qubit]["state0_voltage"]
+    )
+    state1_voltage = complex(
+        platform.characterization["single_qubit"][qubit]["state1_voltage"]
+    )
+
     data = Dataset(
-        name=f"data_q{qubit}", quantities={"probability": "dimensionless", "gateNumber": "dimensionless"}
+        name=f"data_q{qubit}",
+        quantities={"probability": "dimensionless", "gateNumber": "dimensionless"},
     )
 
     count = 0
@@ -61,8 +67,19 @@ def allXY(
             msr, phase, i, q = platform.execute_pulse_sequence(seq, nshots=1024)[
                 ro_pulse.serial
             ]
-            prob = np.abs(msr * 1e6 - gnd) / (exc - gnd)
-            prob = (2 * prob) - 1
+
+            if platform.resonator_type == "3D":
+                prob = np.abs(msr * 1e6 - state1_voltage) / (
+                    state0_voltage - state1_voltage
+                )
+                prob = (2 * prob) - 1
+
+            else:
+                prob = np.abs(msr * 1e6 - state1_voltage) / (
+                    state1_voltage - state0_voltage
+                )
+                prob = (2 * prob) - 1
+
             results = {
                 "MSR[V]": msr,
                 "i[V]": i,
@@ -88,7 +105,7 @@ def allXY_iteration(
     points=10,
 ):
     platform.reload_settings()
-    
+
     # allXY rotations
     gatelist = [
         ["I", "I"],
@@ -114,11 +131,20 @@ def allXY_iteration(
         ["RY(pi/2)", "RY(pi/2)"],
     ]
 
-    gnd = complex(platform.characterization["single_qubit"][qubit]["state1_voltage"])
-    exc = complex(platform.characterization["single_qubit"][qubit]["state0_voltage"])
+    state0_voltage = complex(
+        platform.characterization["single_qubit"][qubit]["state0_voltage"]
+    )
+    state1_voltage = complex(
+        platform.characterization["single_qubit"][qubit]["state1_voltage"]
+    )
+
     data = Dataset(
         name=f"data_q{qubit}",
-        quantities={"probability": "dimensionless", "gateNumber": "dimensionless", "beta_param": "dimensionless"},
+        quantities={
+            "probability": "dimensionless",
+            "gateNumber": "dimensionless",
+            "beta_param": "dimensionless",
+        },
     )
 
     count = 0
@@ -135,8 +161,19 @@ def allXY_iteration(
                 msr, phase, i, q = platform.execute_pulse_sequence(seq, nshots=1024)[
                     ro_pulse.serial
                 ]
-                prob = np.abs(msr * 1e6 - gnd) / (exc - gnd)
-                prob = (2 * prob) - 1
+
+                if platform.resonator_type == "3D":
+                    prob = np.abs(msr * 1e6 - state1_voltage) / (
+                        state0_voltage - state1_voltage
+                    )
+                    prob = (2 * prob) - 1
+
+                else:
+                    prob = np.abs(msr * 1e6 - state1_voltage) / (
+                        state1_voltage - state0_voltage
+                    )
+                    prob = (2 * prob) - 1
+
                 results = {
                     "MSR[V]": msr,
                     "i[V]": i,
@@ -195,7 +232,7 @@ def _get_sequence_from_gate_pair(platform, gates, qubit, beta_param):
                     start=pulse_start,
                 )
             else:
-                RX90_pulse = platform.RX90_drag_pulse(
+                RX90_pulse = platform.create_RX90_drag_pulse(
                     qubit,
                     start=pulse_start,
                     beta=beta_param,
@@ -228,7 +265,7 @@ def _get_sequence_from_gate_pair(platform, gates, qubit, beta_param):
                     relative_phase=np.pi / 2,
                 )
             else:
-                RY90_pulse = platform.RX90_drag_pulse(
+                RY90_pulse = platform.create_RX90_drag_pulse(
                     qubit,
                     start=pulse_start,
                     relative_phase=np.pi / 2,

src/qcvv/plots/scatters.py
@@ -106,6 +106,7 @@ def frequency_attenuation_msr_phase__cut(folder, routine, qubit, format):
     )
     return fig
 
+
 def frequency_msr_phase(folder, routine, qubit, format):
     fig = make_subplots(
         rows=1,
@@ -118,16 +119,17 @@ def frequency_msr_phase(folder, routine, qubit, format):
         ),
     )
     import os.path
+
     file = f"{folder}/data/{routine}/data_q{qubit}.csv"
     if os.path.exists(file):
         data = Dataset.load_data(folder, routine, format, f"data_q{qubit}")
-        data.df.sort_values(by='frequency', inplace=True)
+        data.df.sort_values(by="frequency", inplace=True)
         fig.add_trace(
             go.Scatter(
                 x=data.get_values("frequency", "Hz"),
                 y=data.get_values("MSR", "uV"),
                 name="MSR",
-                mode='lines+markers',
+                mode="lines+markers",
                 marker=dict(size=5, color="LightSeaGreen"),
             ),
             row=1,
@@ -138,7 +140,7 @@ def frequency_msr_phase(folder, routine, qubit, format):
                 x=data.get_values("frequency", "Hz"),
                 y=data.get_values("phase", "rad"),
                 name="phase",
-                mode='lines+markers',
+                mode="lines+markers",
                 marker=dict(size=5, color="LightSeaGreen"),
             ),
             row=1,
@@ -228,7 +230,7 @@ def gain_msr_phase(folder, routine, qubit, format):
         ),
         row=1,
         col=2,
-    )    
+    )
     fig.update_layout(
         showlegend=True,
         uirevision="0",  # ``uirevision`` allows zooming while live plotting
@@ -367,12 +369,14 @@ def exc_gnd(folder, routine, qubit, formato):
 
         i_mean_exc = i_exc.mean()
         q_mean_exc = q_exc.mean()
+        iq_mean_exc = complex(i_mean_exc, q_mean_exc)
+        mod_iq_exc = abs(iq_mean_exc) * 1e6
 
         fig.add_trace(
             go.Scatter(
                 x=[i_mean_exc],
                 y=[q_mean_exc],
-                name="mean_exc_state",
+                name=f" state1_voltage: {mod_iq_exc} <br> mean_exc_state: {iq_mean_exc}",
                 mode="markers",
                 marker_size=16,
             ),
@@ -400,12 +404,14 @@ def exc_gnd(folder, routine, qubit, formato):
 
         i_mean_gnd = i_gnd.mean()
         q_mean_gnd = q_gnd.mean()
+        iq_mean_gnd = complex(i_mean_gnd, q_mean_gnd)
+        mod_iq_gnd = abs(iq_mean_gnd) * 1e6
 
         fig.add_trace(
             go.Scatter(
                 x=[i_mean_gnd],
                 y=[q_mean_gnd],
-                name="mean_gnd_state",
+                name=f" state0_voltage: {mod_iq_gnd} <br> mean_gnd_state: {iq_mean_gnd}",
                 mode="markers",
                 marker_size=16,
             ),
@@ -471,8 +477,12 @@ def prob_gate_iteration(folder, routine, qubit, format):
     for n in range(gates // 21):
         data_start = n * 21
         data_end = data_start + 21
-        beta_param = np.array(data.get_values("beta_param", "dimensionless"))[data_start]
-        gates = np.array(data.get_values("gateNumber", "dimensionless"))[data_start:data_end]
+        beta_param = np.array(data.get_values("beta_param", "dimensionless"))[
+            data_start
+        ]
+        gates = np.array(data.get_values("gateNumber", "dimensionless"))[
+            data_start:data_end
+        ]
         probabilities = np.array(data.get_values("probability", "dimensionless"))[
             data_start:data_end
         ]