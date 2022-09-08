81d24a2bbdeaf7d9a471b603a29077fb3de5a859
maxime.hantute@live.fr <maxime.hantute@live.fr>
2022-09-08 11:27:47 +0400

    adding string paramters instead of list

--- src/qcvv/calibrations/resonator_spectroscopy.py
+++ src/qcvv/calibrations/resonator_spectroscopy.py
@@ -278,6 +278,14 @@ def resonator_spectroscopy_flux_matrix(
 
     count = 0
 
+    if fluxlines == "diag":
+        fluxlines = [qubit]
+    elif fluxlines == "all":
+        fluxlines = range(platform.nqubits)
+    elif fluxlines == "outer":
+        fluxlines = np.arange(platform.nqubits)
+        fluxlines = fluxlines[fluxlines != qubit]
+
     for fluxline in fluxlines:
         fluxline = int(fluxline)
         print(fluxline)
