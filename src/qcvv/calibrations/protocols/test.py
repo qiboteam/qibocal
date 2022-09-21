a3beecf1978936c47438b1a682bf9ee3b91227a2
andrea-pasquale <andreapasquale97@gmail.com>
2022-09-21 12:56:06 +0400

    Update simulation example

--- src/qcvv/calibrations/protocols/test.py
+++ src/qcvv/calibrations/protocols/test.py
@@ -6,16 +6,18 @@ from qcvv.data import Data
 
 def test(
     platform,
-    qubit,
+    qubit: list,
     nshots,
     points=1,
 ):
-    data = Data("test", quantities=["nshots"])
-    circuit = models.Circuit(1)
-    circuit.add(gates.H(0))
+    data = Data("test", quantities=["nshots", "probabilities"])
+    nqubits = len(qubit)
+    circuit = models.Circuit(nqubits)
+    circuit.add(gates.H(qubit[0]))
+    circuit.add(gates.H(qubit[1]))
     # circuit.add(gates.H(1))
-    circuit.add(gates.M(0))
+    circuit.add(gates.M(*qubit))
     execution = circuit(nshots=nshots)
 
-    data.add({"nshots": nshots})
+    data.add({"nshots": nshots, "probabilities": execution.probabilities()})
     yield data
