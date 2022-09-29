8c14b5ac37b06d8f5172897d65ba3658d0fd2bea
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2022-09-29 19:29:45 +0400

    Add qubit_sections option

--- src/qcvv/cli/builders.py
+++ src/qcvv/cli/builders.py
@@ -210,22 +210,24 @@ class ReportBuilder:
         """Prettify routine's name for report headers."""
         return routine.__name__.replace("_", " ").title()
 
-    def get_figure(self, routine, method):
+    def get_figure(self, routine, method, qubit=None):
         """Get html figure for report.
 
         Args:
             routine (Callable): Calibration method.
             method (Callable): Plot method.
+            qubit (int): Qubit id. If ``None`` it uses all qubits.
         """
         import tempfile
-
-        figure = method(self.path, routine.__name__, self.qubits, self.format)
+        if qubit is None:
+            qubit = self.qubits
+        figure = method(self.path, routine.__name__, qubit, self.format)
         with tempfile.NamedTemporaryFile() as temp:
             figure.write_html(temp.name, include_plotlyjs=False, full_html=False)
             fightml = temp.read().decode("utf-8")
         return fightml
 
-    def get_live_figure(self, routine, method):
+    def get_live_figure(self, routine, method, qubit=None):
         """Get url to dash page for live plotting.
 
         This url is used by :meth:`qcvv.web.app.get_graph`.
@@ -233,13 +235,15 @@ class ReportBuilder:
         Args:
             routine (Callable): Calibration method.
             method (Callable): Plot method.
+            qubit (int): Qubit id. If ``None`` it uses all qubits.
         """
         # Bad way of parsing qubits to string
-        qubits = "_".join(str(q) for q in self.qubits)
+        if qubit is None:
+            qubit = "_".join(str(q) for q in self.qubits)
         return os.path.join(
             method.__name__,
             self.path,
             routine.__name__,
-            qubits,
+            qubit,
             self.format,
         )
--- src/qcvv/decorators.py
+++ src/qcvv/decorators.py
@@ -5,21 +5,23 @@ import os
 from qcvv.config import raise_error
 
 
-def plot(header, method):
+def plot(header, method, qubit_sections=False):
     """Decorator for adding plots in the report and live plotting page.
 
     Args:
         header (str): Header of the plot to use in the report.
         method (Callable): Plotting method defined under ``qcvv.plots``.
+        qubit_sections (bool): If ``True`` each qubit is plotted in a different HTML section.
+            Default is ``False``.
     """
 
     def wrapped(f):
         if hasattr(f, "plots"):
             # insert in the beginning of the list to have
             # proper plot ordering in the report
-            f.plots.insert(0, (header, method))
+            f.plots.insert(0, (header, method, qubit_sections))
         else:
-            f.plots = [(header, method)]
+            f.plots = [(header, method, qubit_sections)]
         return f
 
     return wrapped
