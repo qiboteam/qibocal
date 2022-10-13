5dfb68d840f1cc765a37dcb24c5203c25e223983
maxime.hantute@live.fr <maxime.hantute@live.fr>
2022-10-13 11:10:41 +0400

    adding plots

--- src/qibocal/plots/scatters.py
+++ src/qibocal/plots/scatters.py
@@ -1260,3 +1260,47 @@ def dispersive_frequency_msr_phase(folder, routine, qubit, formato):
         yaxis2_title="Phase (rad)",
     )
     return fig
+
+
+def frequency_fidelity(folder, routine, qubit, format):
+    try:
+        data = Dataset.load_data(folder, routine, format, f"data_q{qubit}")
+    except:
+        data = Dataset(quantities={"frequency": "Hz", "fidelity": "dimensionless"})
+
+    fig = go.Figure(
+        go.Scatter(
+            x=data.get_values("frequency", "GHz"),
+            y=data.get_values("fidelity", "dimensionless"),
+        )
+    )
+    fig.update_layout(
+        showlegend=False,
+        uirevision="0",  # ``uirevision`` allows zooming while live plotting
+        xaxis_title="Frequency (GHz)",
+        yaxis_title="Fidelity (prob)",
+    )
+    return fig
+
+
+def power_fidelity(folder, routine, qubit, format):
+    try:
+        data = Dataset.load_data(folder, routine, format, f"data_q{qubit}")
+    except:
+        data = Dataset(
+            quantities={"amplitude": "dimensionless", "fidelity": "dimensionless"}
+        )
+
+    fig = go.Figure(
+        go.Scatter(
+            x=data.get_values("amplitude", "dimensionless"),
+            y=data.get_values("fidelity", "dimensionless"),
+        )
+    )
+    fig.update_layout(
+        showlegend=False,
+        uirevision="0",  # ``uirevision`` allows zooming while live plotting
+        xaxis_title="Amplitude (a.u.)",
+        yaxis_title="Fidelity (prob)",
+    )
+    return fig
