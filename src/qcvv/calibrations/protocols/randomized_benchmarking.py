4c358486d59a935edcb01437f1099c86abc8f8e4
Jadwiga Wilkens <wilkensjadwiga@gmail.com>
2022-10-19 10:40:18 +0400

    started to write the dummy rb function, not working yet

--- src/qcvv/calibrations/protocols/randomized_benchmarking.py
+++ src/qcvv/calibrations/protocols/randomized_benchmarking.py
@@ -1,16 +1,41 @@
 
+from qcvv.calibrations.protocols.experiments import Experiment
+from qcvv.calibrations.protocols.generators import *
+from qcvv.plots.scatters import rb_plot
+from qibo.noise import PauliError, NoiseModel
+from qcvv.data import Data
+from qcvv.calibrations.protocols.utils import effective_depol
+from qcvv.decorators import plot
+from qcvv import plots
 
 
+@plot("Test Standard RB", plots.rb_plot)
 def dummyrb(
     platform,
-    qubit : list,
-    experiment_name : str,
-    inject_nois : list,
-    nshots: int
+    qubits : list,
+    circuit_generator_class : str,
+    invert : bool,
+    sequence_lengths : list,
+    runs : int,
+    nshots: int,
+    inject_noise : list,
 ):
-    # There are several types of experiment objects
-    #   1. the circuits are drawn randomly beforehand and stored (the numbers of it is a 
-    # discrete set, don't forget the order/numbering!)
-    #   2. They are drawn on the fly and the gate has to be known and stored somehow
-    # experiment = convert_to_function(experiment_name)
-    pass
+    # Make the generator class out of the name.
+    circuit_generator_class = eval(circuit_generator_class)
+    # Make a generator object out of the generator class.
+    circuit_generator = circuit_generator_class(qubits, invert=invert)
+    # Initiate the Experiment object, not filled with circuits yet. 
+    experiment = Experiment(
+        circuit_generator, sequence_lengths, qubits, runs, nshots)
+    # Build the circuits and store them.
+    directory = experiment.build_a_save(yield_data=True)
+    # Execute the circuits and store the outcome.
+    experiment.execute_a_save(
+        yield_data=True, paulierror_noisparams=inject_noise)
+    # Store the effective depol parameter.
+    pauli = PauliError(*inject_noise)
+    noise = NoiseModel()
+    noise.add(pauli, gates.Unitary)
+    data_depol = Data("effectivedepol", quantities=["effective_depol"])
+    data_depol.add({"effective_depol": effective_depol(pauli)})
+    yield data_depol
