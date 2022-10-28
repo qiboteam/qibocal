8805ceef2e464486869c16992373e359363dd8b2
Sergi <igres26@gmail.com>
2022-10-28 18:04:12 +0400

    adding gaussian sampling to resonator

---
+++ src/qibocal/calibrations/characterization/resonator_spectroscopy_sample.py
@@ -0,0 +1,173 @@
+import numpy as np
+from qibolab.platforms.abstract import AbstractPlatform
+from qibolab.pulses import PulseSequence
+
+from qibocal.calibrations.characterization.utils import snr, choose_freq, get_noise, update_f, plot_punchout, plot_flux
+
+
+def scan_level(best_f, best_msr, max_runs, thr, span, resolution, noise, platform, ro_pulse, qubit):
+    for _ in max_runs:
+        if _ == 0:
+            freq = best_f
+        else:
+            freq = choose_freq(best_f, span, resolution)
+        platform.ro_port[qubit].lo_frequency = freq - ro_pulse.frequency
+        msr, phase, i, q = platform.execute_pulse_sequence(sequence)[ro_pulse.serial]
+        
+        if platform.resonator_type == "3D":
+            if msr > best_msr:
+                if abs(snr(msr, noise)) >= thr:
+                    best_f, best_msr = freq, msr
+                    return best_f, best_msr
+        else:
+            if msr < best_msr:
+                if abs(snr(msr, noise)) >= thr:
+                    best_f, best_msr = freq, msr
+                    return best_f, best_msr
+    return best_f, best_msr
+
+
+def scan_small(best_f, best_msr, max_runs, thr, span, resolution, noise, platform, ro_pulse, qubit):
+    start_f = best_f
+    scan = np.linspace(-span/2, span/2, resolution)
+    for s in scan:
+        freq = start_f + s
+        platform.ro_port[qubit].lo_frequency = freq - ro_pulse.frequency
+        msr, phase, i, q = platform.execute_pulse_sequence(sequence)[ro_pulse.serial]
+        if platform.resonator_type == "3D":
+            if msr > best_msr:
+                best_f, best_msr = freq, msr
+        else:
+            if msr < best_msr:
+                best_f, best_msr = freq, msr
+    return best_f, best_msr
+    
+               
+def resonator_punchout_sample(
+    platform: AbstractPlatform,
+    qubit: int,
+    min_att,
+    max_att,
+    step_att,
+    max_runs,
+    thr,
+    spans,
+    small_spans,
+    resolution
+    ):
+        
+    platform.reload_settings()
+    sequence = PulseSequence()
+    ro_pulse = platform.create_qubit_readout_pulse(qubit, start=0)
+    sequence.add(ro_pulse)
+    
+    resonator_frequency = platform.characterization["single_qubit"][qubit]["resonator_freq"]
+    
+    attenuation_range = np.arange(min_att, max_att, step_att)
+    best_f = resonator_frequency
+    
+    opt_att = 0
+    opt_snr = 0
+    freqs = []
+    for att in attenuation_range:
+        platform.ro_port[qubit].attenuation = att
+        
+        background = [best_f+1e7, best_f-1e7]
+        noise = get_noise(background, platform, ro_pulse, qubit, sequence)
+        best_msr = noise
+        for span in spans:
+            best_f, best_msr = scan_level(best_f, best_msr, max_runs, thr, span, resolution, noise, platform, ro_pulse, qubit)
+        for span in small_spans:
+            best_f, best_msr = scan_small(best_f, best_msr, max_runs, thr, span, 10, noise, platform, ro_pulse, qubit)
+        freqs.append(best_f)
+        if att >= 30:
+            if abs(snr(best_msr, noise)) > opt_snr:
+                opt_snr = abs(snr(best_msr, noise))
+                opt_att = 0
+                opt_f = best_f
+                
+    plot_punchout(attenuation_range, freqs, qubit)
+    
+    print(f'For qubit {qubit}:')
+    print(f'Best response found at frequency {opt_f} Hz for attenuation value of {opt_att} dB.\n')
+    
+    return freqs, attenuation_range, opt_f, opt_att
+
+
+def resonator_flux_sample(
+    platform: AbstractPlatform,
+    qubit: int,
+    current_min,
+    current_max, 
+    current_step,
+    fluxline,
+    max_runs, 
+    thr,
+    spans,
+    small_spans,
+    resolution,
+    ):
+        
+    platform.reload_settings()
+    
+    sequence = PulseSequence()
+    ro_pulse = platform.create_qubit_readout_pulse(qubit, start=0)
+    sequence.add(ro_pulse)
+    
+    resonator_frequency = platform.characterization["single_qubit"][qubit]["resonator_freq"]
+    qubit_biasing_current = platform.characterization["single_qubit"][qubit]["sweetspot"]
+    
+    platform.qf_port[fluxline].current = qubit_biasing_current
+    
+    current_range = np.arange(current_min, current_max, current_step)
+    
+    start = np.argwhere(current_range==np.around(qubit_biasing_current, 2))[0][0]
+    
+    start_f = resonator_frequency
+    
+    #platform.ro_port[qubit].attenuation = att
+    
+    background = [start_f+1e7, start_f-1e7]
+    noise = get_noise(background, platform, ro_pulse, qubit, sequence)
+    
+    freqs1 = []
+    best_f = start_f
+    for curr in current_range[start:]:
+        best_msr = noise
+        platform.qf_port[fluxline].current = curr
+        for span in spans:
+            best_f, best_msr = scan_level(best_f, best_msr, max_runs, thr, span, resolution, noise, platform, ro_pulse, qubit)
+        for span in small_spans:
+            best_f, best_msr = scan_small(best_f, best_msr, max_runs, thr, span, 10, noise, platform, ro_pulse, qubit)
+        freqs1.append(best_f)
+        
+    freqs2 = []
+    best_f = start_f
+    for curr in reversed(current_range[:start]):
+        best_msr = noise
+        platform.qf_port[fluxline].current = curr
+        for span in spans:
+            best_f, best_msr = scan_level(best_f, best_msr, max_runs, thr, span, resolution, noise, platform, ro_pulse, qubit)
+        for span in small_spans:
+            best_f, best_msr = scan_small(best_f, best_msr, max_runs, thr, span, 10, noise, platform, ro_pulse, qubit)
+        freqs2.append(best_f)
+        
+    freqs = np.array(list(reversed(freqs2))+freqs1)
+    
+    sweet_freq = np.max(freqs)
+    sweet_curr = current_range[np.argmax(freqs)]
+    
+    plot_flux(current_range, freqs, qubit)
+    
+    print(f'For qubit {qubit}:')
+    print(f'Sweet spot found at frequency {sweet_f} Hz at current value of {sweet_curr} A.\n')
+    
+    return freqs, current_range, sweet_freq, sweet_curr
+                         
+        
+    
+    
+    
+    
+    
+    
--- src/qibocal/calibrations/characterization/utils.py
+++ src/qibocal/calibrations/characterization/utils.py
@@ -13,3 +13,50 @@ def variable_resolution_scanrange(
             np.arange(highres_width, lowres_width, lowres_step),
         )
     )
+
+
+def snr(signal, noise):
+    """Signal to Noise Ratio to detect peaks and valleys."""
+    return 20*np.log(signal/noise)
+
+
+def choose_freq(freq, span, resolution):
+    """Choose a new frequency gaussianly distributed around initial one."""
+    g = np.random.normal(0, span/5, 1)
+    f = np.linspace(-span/2, span/2, resolution)
+    for ff in f:
+        if g <= ff:
+            break
+    return freq+ff
+
+
+def get_noise(background, platform, ro_pulse, qubit, sequence):
+    noise = 0
+    for b_freq in background:
+        platform.ro_port[qubit].lo_frequency = b_freq - ro_pulse.frequency
+        res = platform.execute_pulse_sequence(sequence, 1024)
+        msr, phase, i, q = platform.execute_pulse_sequence(sequence)[ro_pulse.serial]
+        noise += msr
+    return noise/len(background)
+
+
+def update_f(best_f, f, best_v, v, noise, thr):
+    """Updates the best frequency and voltage found if SNR is above a threshold."""
+    if abs(snr(v, noise)) >= thr:
+        best_v = v
+        best_f = f
+    return best_f, best_v
+
+
+def plot_flux(currs, freqs, qubit):
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize=(8,8))
+    plt.plot(currs, freqs)
+    plt.savefig(f'qubit_{qubit}_flux.png', bbox_inches='tight')
+    
+    
+def plot_punchout(atts, freqs, qubit):
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize=(8,8))
+    plt.plot(freqs, atts)
+    plt.savefig(f'qubit_{qubit}_punchout.png', bbox_inches='tight')
