adcefa8babe61a01d25a5df119634f7c669f1ced
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-11-17 10:56:54 +0100

    rtd -> furo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,7 +73,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#6400FF",
+        "color-brand-secondary": "#6400FF",
+        "color-brand-content": "#6400FF",
+    }
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
