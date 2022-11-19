729699a5934d5745005787d945b0ad6777eeb7f0
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2022-11-19 12:44:46 +0400

    run qiboview when the platform file has changed

--- serverscripts/queue.sh
+++ serverscripts/queue.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
-
-
-while true
+# In order to run the script in background it can be used the command ./queue.sh & 
+PLATFORMS_PATH="../../qibolab/src/qibolab/runcards"
+platformNames=("tii1q" "tii5q")
+lastEditDates=()
+# Save the last edit date of the platforms' file *.yml
+for t in ${platformNames[@]}; do
+    lastEditDates+=($(date +%s -r ${PLATFORMS_PATH}/${t}.yml ))
+done 
+# run an infinite loop 
+while true; 
 do 
-    python qiboview.py --platform_name tii1q --json_path monitor/monitor_tii1q.json
-    python qiboview.py --platform_name tii15q --json_path monitor/monitor_tii5q.json
+    
+    for i in ${!platformNames[@]}; do
+        # date in second of the last edit
+        newLastEditDate=$(date +%s -r ${PLATFORMS_PATH}/${platformNames[$i]}.yml )
+        echo ${platformNames[i]} #TODO:remove 
+        echo $newLastEditDate #TODO:remove 
+        echo ${lastEditDates[$i]} #TODO:remove 
+        # if the file's edit date is later than lastEditDate, the script update the *json file
+        if [[ ${lastEditDates[$i]} < $newLastEditDate ]]; then
+            python qiboview.py --platform_name ${platformNames[i]} --json_path monitor/monitor_${platformNames[i]}.json
+            lastEditDates[$i]=$newLastEditDate
+        fi 
+        
+    done
+
     python qiboqueue.py 
-    sleep 5
+    sleep 2
+
 done
