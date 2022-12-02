4b70fa4405beba865000ab601d78e8ca455c3c60
Alejandro <alexsopena15@gmail.com>
2022-12-02 13:14:35 +0400

    Added plot method

--- src/qibocal/plots/scatters.py
+++ src/qibocal/plots/scatters.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 from qibocal.data import Data, DataUnits
-from qibocal.fitting.utils import cos, exp, flipping, lorenzian, rabi, ramsey, line
+from qibocal.fitting.utils import cos, exp, flipping, lorenzian, rabi, ramsey, line, freq_r_transmon, freq_r_mathieu
 
 
 def frequency_msr_phase__fast_precision(folder, routine, qubit, format):
@@ -1270,141 +1270,7 @@ def dispersive_frequency_msr_phase(folder, routine, qubit, formato):
     return fig
 
 
-def frequency_current_flux(folder, routine, qubit, format):
-    """ Plot of the experimental data for the flux resonator spectroscopy and its corresponding fit.
-        Args:
-        folder (str): Folder where the data files with the experimental and fit data are.
-        routine (str): Routine name (resonator_flux_sample_matrix)
-        qubit (int): qubit coupled to the resonator for which we want to plot the data.
-        format (str): format of the data files.
-
-    Returns:
-        fig (Figure): Figure associated to data.
-
-    """
-    fluxes = []
-    fluxes_fit = []
-    for i in range(5):  # FIXME: 5 is hardcoded
-        file1 = f"{folder}/data/{routine}/data_q{qubit}_f{i}.csv"
-        file2 = f"{folder}/data/{routine}/fit_q{qubit}_f{i}.csv"
-        if os.path.exists(file1):
-            fluxes += [i]
-        if os.path.exists(file2):
-            fluxes_fit += [i]
-
-    if len(fluxes) < 1:
-        nb = 1
-    else:
-        nb = len(fluxes)
-    fig = make_subplots(
-        rows=1,
-        cols=nb,
-        horizontal_spacing=0.05,
-        vertical_spacing=0.1,
-        x_title="Current (A)",
-        y_title="Frequency (GHz)",
-        shared_xaxes=True,
-        shared_yaxes=True,
-    )
-
-    for j in fluxes:
-
-        data_spec = DataUnits.load_data(folder, routine, format, f"data_q{qubit}_f{j}")
-        fig.add_trace(
-            go.Scatter(
-                x=data_spec.get_values("current", "A"),
-                y=data_spec.get_values("frequency", "GHz"),
-                name=f"fluxline: {j}"
-            ),
-            row=1,
-            col=j,
-        )
-
-        if j in fluxes_fit:   
-
-            data_fit = Data.load_data(folder, routine, format, f"fit_q{qubit}_f{j}")
-            if len(data_spec) > 0 and len(data_fit) > 0:
-                curr_range = np.linspace(
-                    min(data_spec.get_values("current", "A")),
-                    max(data_spec.get_values("current", "A")),
-                    100,
-                )
-                params = [i for i in list(data_fit.df.keys()) if "popt" not in i]
-                if j==qubit:
-                    fig.add_trace(
-                        go.Scatter(
-                            x=curr_range,
-                            y=cos(
-                                curr_range,
-                                data_fit.get_values("popt0"),
-                                data_fit.get_values("popt1"),
-                                data_fit.get_values("popt2"),
-                                data_fit.get_values("popt3"),
-                            ),
-                            name=f"Fit fluxline {j}",
-                            line=go.scatter.Line(dash="dot"),
-                        ),
-                        row=1,
-                        col=j,
-                    )
-
-                else:
-                    fig.add_trace(
-                        go.Scatter(
-                            x=curr_range,
-                            y=line(
-                                curr_range,
-                                data_fit.get_values("popt0"),
-                                data_fit.get_values("popt1"),
-                            ),
-                            name=f"Fit fluxline {j}",
-                            line=go.scatter.Line(dash="dot"),
-                        ),
-                        row=1,
-                        col=j,
-                    )
-
-                fig.update_layout(margin=dict(l=20, r=20, t=20, b=170))
-                if j==qubit:
-                    fig.add_annotation(
-                        dict(
-                            font=dict(color="black", size=12),
-                            x=0,
-                            y=-0.25-0.1*j,
-                            showarrow=False,
-                            #text=f"Qubit: {qubit} Fluxline: {j} C_{qubit}{j} = {data_fit.df[params[3]][0]:.3f} +- {data_fit.df[params[2]][0]:.1f} GHz/A. freq_{qubit} = {data_fit.df[params[1]][0]:.5f} +- {data_fit.df[params[0]][0]:.1f} GHz. {params[7]} = {data_fit.df[params[7]][0]:.3} +- {data_fit.df[params[6]][0]:.1f} A. {params[5]} = {data_fit.df[params[5]][0]:.3f} +- {data_fit.df[params[4]][0]:.1f} GHz. ",
-                            text=f"Qubit: {qubit} Fluxline: {j} C_{qubit}{j} = {data_fit.df[params[3]][0]:.3f} GHz/A. freq_{qubit} = {np.round_(data_fit.df[params[1]][0],4)} GHz. {params[7]} = {np.round_(data_fit.df[params[7]][0],3)} A. {params[5]} = {np.round(data_fit.df[params[5]][0],4)} GHz. ",
-                            xanchor="left",
-                            xref="paper",
-                            yref="paper",
-                        )
-                    )
-                else:
-                    fig.add_annotation(
-                        dict(
-                            font=dict(color="black", size=12),
-                            x=0,
-                            y=-0.25-0.1*j,
-                            showarrow=False,
-                            #text=f"Qubit: {qubit} Fluxline: {j} C_{qubit}{j} = {data_fit.df[params[1]][0]:.3f} +- {data_fit.df[params[0]][0]:.1f} GHz/A.",
-                            text=f"Qubit: {qubit} Fluxline: {j} C_{qubit}{j} = {data_fit.df[params[1]][0]:.3f} GHz/A.",
-                            xanchor="left",
-                            xref="paper",
-                            yref="paper",
-                        )
-                    )
-
-
-
-    fig.update_layout(
-        showlegend=False,
-        uirevision="0",  # ``uirevision`` allows zooming while live plotting
-    )
-    
-    #fig.write_image(f"fig_{q}.png")
-    return fig
-
-def frequency_attenuation(folder, routine, qubit, format):
+def frequency_current(folder, routine, qubit, format):
     try:
         data = DataUnits.load_data(folder, routine, format, f"data_q{qubit}")
     except:
@@ -1456,4 +1322,225 @@ def frequency_attenuation(folder, routine, qubit, format):
     fig.update_layout(
         uirevision="0",  # ``uirevision`` allows zooming while live plotting
     )
+    return fig
+
+
+def frequency_attenuation(folder, routine, qubit, format):
+    try:
+        data = DataUnits.load_data(folder, routine, format, f"data_q{qubit}")
+    except:
+        data = DataUnits(quantities={"frequency": "Hz", "attenuation": "dB"})
+
+    try:
+        data1 = DataUnits.load_data(folder, routine, format, f"results_q{qubit}")
+    except:
+        data1 = DataUnits(quantities={"snr": "none" ,"frequency": "Hz", "attenuation": "dB"})
+
+    opt_f = data1.get_values("frequency", "GHz")[0]
+    opt_att = data1.get_values("attenuation", "dB")[0]
+    opt_snr = data1.get_values("snr", "dimensionless")[0]
+
+    fig = make_subplots(
+        rows=1,
+        cols=1,
+        horizontal_spacing=0.1,
+        vertical_spacing=0.1,
+        x_title="Frequency (GHz)",
+        y_title="Attenuation (db)",
+        )
+
+    fig.add_trace(
+        go.Scatter(
+            x=data.get_values("frequency", "GHz"),
+            y=data.get_values("attenuation", "dB"),
+            name="Punchout",
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig.add_annotation(
+        dict(
+            font=dict(color="black", size=12),
+            x=0,
+            y=-0.30,
+            showarrow=False,
+            text=f"Best response found at frequency {round(opt_f,len(str(opt_f))-3)} GHz <br> for attenuation value of {opt_att} dB with snr {round(opt_snr,3)}.\n",
+            textangle=0,
+            xanchor="left",
+            xref="paper",
+            yref="paper",
+        )
+    )
+
+    # last part
+    fig.update_layout(
+        margin=dict(l=60, r=20, t=20, b=130),
+        autosize=False,
+        width=500,
+        height=500,
+        uirevision="0",  # ``uirevision`` allows zooming while live plotting
+    )
+    return fig
+
+
+def frequency_current_flux(folder, routine, qubit, format):
+    """ Plot of the experimental data for the flux resonator spectroscopy and its corresponding fit.
+        Args:
+        folder (str): Folder where the data files with the experimental and fit data are.
+        routine (str): Routine name (resonator_flux_sample_matrix)
+        qubit (int): qubit coupled to the resonator for which we want to plot the data.
+        format (str): format of the data files.
+
+    Returns:
+        fig (Figure): Figure associated to data.
+
+    """
+    fluxes = []
+    fluxes_fit = []
+    for i in range(5):  # FIXME: 5 is hardcoded
+        file1 = f"{folder}/data/{routine}/data_q{qubit}_f{i}.csv"
+        file2 = f"{folder}/data/{routine}/fit1_q{qubit}_f{i}.csv"
+        if os.path.exists(file1):
+            fluxes += [i]
+        if os.path.exists(file2):
+            fluxes_fit += [i]
+
+    if len(fluxes) < 1:
+        nb = 1
+    else:
+        nb = len(fluxes)
+
+    fig = make_subplots(
+        rows=1,
+        cols=nb,
+        horizontal_spacing=0.05,
+        vertical_spacing=0.1,
+        x_title="Current (A)",
+        y_title="Frequency (GHz)",
+        shared_xaxes=False,
+        shared_yaxes=True,
+    )
+    
+    for k, j in enumerate(fluxes):
+
+        data_spec = DataUnits.load_data(folder, routine, format, f"data_q{qubit}_f{j}")
+        fig.add_trace(
+            go.Scatter(
+                x=data_spec.get_values("current", "A"),
+                y=data_spec.get_values("frequency", "GHz"),
+                name=f"fluxline: {j}"
+            ),
+            row=1,
+            col=k+1,
+        )
+
+        if j in fluxes_fit:   
+
+            data_fit = Data.load_data(folder, routine, format, f"fit1_q{qubit}_f{j}")
+            if len(data_spec) > 0 and len(data_fit) > 0:
+                curr_range = np.linspace(
+                    min(data_spec.get_values("current", "A")),
+                    max(data_spec.get_values("current", "A")),
+                    100,
+                )
+                params = [i for i in list(data_fit.df.keys()) if "popt" not in i]
+                if int(j)==int(qubit):
+                    if len(data_fit.df.keys()) == 10:
+                        y = freq_r_transmon(
+                                curr_range,
+                                data_fit.get_values("curr_sp"),
+                                data_fit.get_values("xi"),
+                                data_fit.get_values("d"),
+                                data_fit.get_values("f_q/f_rh"),
+                                data_fit.get_values("g"),
+                                data_fit.get_values("f_rh")
+                            )/10**9
+                    else:
+                        y = freq_r_mathieu(
+                            curr_range,
+                            data_fit.get_values("f_rh"),
+                            data_fit.get_values("g"),
+                            data_fit.get_values("curr_sp"),
+                            data_fit.get_values("xi"),
+                            data_fit.get_values("d"),
+                            data_fit.get_values("Ec"),
+                            data_fit.get_values("Ej")
+                        )/10**9
+                    fig.add_trace(
+                        go.Scatter(
+                            x=curr_range,
+                            y=y,
+                            name=f"Fit fluxline {j}",
+                            line=go.scatter.Line(dash="dot"),
+                        ),
+                        row=1,
+                        col=k+1,
+                    )
+
+                else:
+                    fig.add_trace(
+                        go.Scatter(
+                            x=curr_range,
+                            y=line(
+                                curr_range,
+                                data_fit.get_values("popt0"),
+                                data_fit.get_values("popt1"),
+                            )/10**9,
+                            name=f"Fit fluxline {j}",
+                            line=go.scatter.Line(dash="dot"),
+                        ),
+                        row=1,
+                        col=k+1,
+                    )
+
+                if int(j)==int(qubit):
+                    f_qs=data_fit.get_values("f_qs")[0]
+                    f_rs=data_fit.get_values("f_rs")[0]
+                    curr_qs=data_fit.get_values("curr_sp")[0]
+                    g=data_fit.get_values("g")[0]
+                    d=data_fit.get_values("d")[0]
+                    xi=data_fit.get_values("xi")[0]
+                    C_ii=data_fit.get_values("C_ii")[0]
+                    text=f"Fluxline: {j} <br> freq_r{qubit}_sp = {f_rs :.4e} Hz <br> freq_q{qubit}_sp = {f_qs :.4e} Hz <br> curr_{qubit}_sp = {curr_qs :.2e} A <br> g = {g :.2e} Hz <br> d = {d :.2e} <br> xi = {xi :.2e} 1/A <br> C_{qubit}{j} = {C_ii :.4e} Hz/A"
+                    if len(data_fit.df.keys()) != 10:
+                        Ec=data_fit.get_values("Ec")[0]
+                        Ej=data_fit.get_values("Ej")[0]
+                        text += f" <br> Ec = {Ec :.3e} Hz <br> Ej = {Ej :.3e} Hz"
+                    fig.add_annotation(
+                        dict(
+                            font=dict(color="black", size=12),
+                            x=0,
+                            y=-0.9,
+                            showarrow=False,
+                            text=text,
+                            #xanchor="left",
+                            xref=f"x{k+1}",
+                            yref="paper"#"y1",
+                        )
+                    )
+                else:
+                    C_ij=data_fit.get_values("popt0")[0]
+                    fig.add_annotation(
+                        dict(
+                            font=dict(color="black", size=12),
+                            x=0,
+                            y=-0.3,
+                            showarrow=False,
+                            text=f"Fluxline: {j} <br> C_{qubit}{j} = {C_ij :.4e} Hz/A.",
+                            #xanchor="left",
+                            xref=f"x{k+1}",
+                            yref="paper"#"y1",
+                        )
+                    )
+
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=20, b=230),
+        showlegend=False,
+        autosize=False,
+        width=500*len(fluxes),
+        height=500,
+        uirevision="0",  # ``uirevision`` allows zooming while live plotting
+    )
+    
     return fig
