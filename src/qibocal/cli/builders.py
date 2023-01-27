8f5f904963e7b244af2cdb97d349ac86bf7211d3
andrea-pasquale <andreapasquale97@gmail.com>
2023-01-27 10:30:51 +0400

    Minor fix

--- src/qibocal/cli/builders.py
+++ src/qibocal/cli/builders.py
@@ -170,7 +170,7 @@ class ActionBuilder:
         except FileNotFoundError:
             return None
 
-        params = data_fit.df.to_dict("index")[qubit - 1]
+        params = data_fit.df.to_dict("index")[qubit if qubit == 0 else qubit - 1]
         settings = load_yaml(f"{self.folder}/new_platform.yml")
         for param in params:
             if param in list(self.qubits[qubit].__annotations__.keys()):
