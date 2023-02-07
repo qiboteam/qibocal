31c3e3d2494f746979f4f6599a091f15e47ae921
Edoardo-Pedicillo <62623482+Edoardo-Pedicillo@users.noreply.github.com>
2023-02-07 18:16:43 +0400

    Update src/qibocal/tree/operation.py
    Co-authored-by: Alessandro Candido <candido.ale@gmail.com>

--- src/qibocal/tree/operation.py
+++ src/qibocal/tree/operation.py
@@ -1,19 +1,29 @@
 from enum import Enum
 
+from dataclasses import dataclass
+from typing import Callable
 
-def command_1():
+@dataclass
+class Routine:
+    name: str
+    routine: Callable
+
+def _command_1():
     print("command 1")
 
+command_1 = Routine("1", routine=_command_1)
 
-def command_2():
+def _command_2():
     print("command 2")
 
+command_2 = Routine("2", routine=_command_2)
 
-def command_3():
+def _command_3():
     print("command 3")
 
+command_3 = Routine("3", routine=_command_3)
 
 class Operation(Enum):
-    command_1 = command_1  # TODO: this is not working
+    command_1 = command_1
     command_2 = command_2
     command_3 = command_3
