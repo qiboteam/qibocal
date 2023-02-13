26d744ccaad521ad0fb8c26dddee9fd1183dff90
Jadwiga Wilkens <jadwiga01@physik.fu-berlin.de>
2023-02-13 15:26:59 +0400

    added import again

--- src/qibocal/calibrations/niGSC/basics/experiment.py
+++ src/qibocal/calibrations/niGSC/basics/experiment.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import pickle
 from collections.abc import Iterable
 from os.path import isfile
+from typing import Callable
 
 import numpy as np
 import pandas as pd
