032d62e1f268b2cef5bf6a54c131de3c7d4012e9
Jadwiga Wilkens <jadwiga01@physik.fu-berlin.de>
2023-02-14 09:46:32 +0400

    Andreas comments

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=testrb
-#SBATCH --partition=tii1q_b1
-
-qq runcards/standardrb.yml
--- run_quantumhardware.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=rbtest
-#SBATCH --partition=qpu5q
-
-qq runcards/actions_qq_5q.yml
