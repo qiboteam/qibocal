Fix averaging

src/qibocal/calibrations/characterization/resonator_spectroscopy.py
@@ -100,7 +100,7 @@ def resonator_spectroscopy(
             # average msr, phase, i and q over the number of shots defined in the runcard
             result = results[ro_pulses[qubit].serial]
             # store the results
-            r = result.to_dict()
+            r = result.to_dict(average=False)
             r.update(
                 {
                     "frequency[Hz]": delta_frequency_range + ro_pulses[qubit].frequency,
@@ -183,7 +183,7 @@ def resonator_spectroscopy(
         for qubit, ro_pulse in ro_pulses.items():
             # average msr, phase, i and q over the number of shots defined in the runcard
             result = results[ro_pulse.serial]
-            r = result.to_dict()
+            r = result.to_dict(average=False)
             # store the results
             r.update(
                 {
@@ -312,7 +312,7 @@ def resonator_punchout_attenuation(
                 len(attenuation_range)
                 * list(delta_frequency_range + ro_pulse.frequency)
             ).flatten()
-            r = result.to_dict()
+            r = result.to_dict(average=False)
             r.update(
                 {
                     "frequency[Hz]": freqs,
@@ -434,7 +434,7 @@ def resonator_punchout(
             freqs = np.array(
                 len(amplitude_range) * list(delta_frequency_range + ro_pulse.frequency)
             ).flatten()
-            r = {k: v.ravel() for k, v in result.to_dict().items()}
+            r = {k: v.ravel() for k, v in result.to_dict(average=False).items()}
             r.update(
                 {
                     "frequency[Hz]": freqs,
@@ -556,7 +556,7 @@ def resonator_spectroscopy_flux(
                 * list(delta_frequency_range + ro_pulses[qubit].frequency)
             ).flatten()
             # store the results
-            r = {k: v.ravel() for k, v in result.to_dict().items()}
+            r = {k: v.ravel() for k, v in result.to_dict(average=False).items()}
             r.update(
                 {
                     "frequency[Hz]": freqs,