168243fe99f3e2656564b4eb45c413f5b279adb0
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2023-03-03 17:38:22 +0400

    upddate gaussian process

--- src/qibocal/fitting/classifier/gaussian_process.py
+++ src/qibocal/fitting/classifier/gaussian_process.py
@@ -5,7 +5,7 @@ from . import utils
 
 
 def constructor(**hyperparams):
-    return GaussianProcessClassifier(1.0 * RBF(1.0), **hyperparams)
+    return GaussianProcessClassifier(1.0 * RBF(1.0)).set_params(**hyperparams)
 
 
 def hyperopt(_x_train, _y_train, _path):
