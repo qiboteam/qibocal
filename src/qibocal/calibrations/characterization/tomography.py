29dbbbb6d82d383b201c3b7a551240b1dcfd96ec
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2023-03-13 13:52:15 +0400

    Pass arbitrary sequence

--- src/qibocal/calibrations/characterization/tomography.py
+++ src/qibocal/calibrations/characterization/tomography.py
@@ -7,8 +7,7 @@ from qibocal.data import DataUnits
 def state_tomography(
     platform,
     qubits,
-    flux_pulse_duration,  # TODO: Should be read from the runcard
-    flux_pulse_amplitude,
+    sequence,
     nshots=1024,
     relaxation_time=50000,
 ):
@@ -22,6 +21,9 @@ def state_tomography(
     basis_rotations = {
         "I": None,
         "RX": lambda qubit, start: platform.create_RX_pulse(qubit, start),
+        "RY": lambda qubit, start: platform.create_RX_pulse(
+            qubit, start, relative_phase=np.pi / 2
+        ),
         "RX90": lambda qubit, start: platform.create_RX90_pulse(qubit, start),
         "RY90": lambda qubit, start: platform.create_RX90_pulse(
             qubit, start, relative_phase=np.pi / 2
@@ -40,48 +42,44 @@ def state_tomography(
     for label1, basis1 in basis_rotations.items():
         for label2, basis2 in basis_rotations.items():
             if not (label1 == "RX" and label2 == "RX"):
-                sequence = PulseSequence()
-                # entangler sequence
-                sequence.add(
-                    platform.create_RX90_pulse(
-                        qubit1, start=0, relative_phase=np.pi / 2
-                    )
-                )
-                sequence.add(
-                    platform.create_RX90_pulse(
-                        qubit2, start=0, relative_phase=np.pi / 2
-                    )
-                )
-                sequence.add(
-                    FluxPulse(
-                        start=sequence.finish,
-                        duration=flux_pulse_duration,
-                        amplitude=flux_pulse_amplitude,
-                        shape=Rectangular(),
-                        channel=platform.qubits[qubit2].flux.name,
-                        qubit=qubit2,
-                    )
-                )
-                sequence.add(
-                    platform.create_RX90_pulse(
-                        qubit1, start=sequence.finish, relative_phase=np.pi / 2
-                    )
-                )
+                total_sequence = PulseSequence()
+                # state preperation sequence
+                for moment in sequence:
+                    start = total_sequence.finish
+                    for pulse_description in moment:
+                        pulse_type, qubit = pulse_description[:2]
+                        if pulse_type == "FluxPulse":
+                            flux_duration, flux_amplitude = pulse_description[2:]
+                            total_sequence.add(
+                                FluxPulse(
+                                    start=start,
+                                    duration=flux_duration,
+                                    amplitude=flux_amplitude,
+                                    shape=Rectangular(),
+                                    channel=platform.qubits[qubit2].flux.name,
+                                    qubit=qubit,
+                                )
+                            )
+                        elif pulse_type in basis_rotations:
+                            total_sequence.add(
+                                basis_rotations[pulse_type](qubit, start)
+                            )
+
                 # basis rotation sequence
-                start = sequence.finish
+                start = total_sequence.finish
                 if basis1 is not None:
-                    sequence.add(basis1(qubit1, start))
+                    total_sequence.add(basis1(qubit1, start))
                 if basis2 is not None:
-                    sequence.add(basis2(qubit2, start))
+                    total_sequence.add(basis2(qubit2, start))
                 # measurements
-                start = sequence.finish
+                start = total_sequence.finish
                 measure1 = platform.create_MZ_pulse(qubit1, start=start)
                 measure2 = platform.create_MZ_pulse(qubit2, start=start)
-                sequence.add(measure1)
-                sequence.add(measure2)
+                total_sequence.add(measure1)
+                total_sequence.add(measure2)
 
                 results = platform.execute_pulse_sequence(
-                    sequence, nshots=nshots, relaxation_time=relaxation_time
+                    total_sequence, nshots=nshots, relaxation_time=relaxation_time
                 )
 
                 # store the results
