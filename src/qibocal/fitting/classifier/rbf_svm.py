b11ddef1bcb757d6f2d2cdb0e65ed2c0523c9041
Edoardo-Pedicillo <62623482+Edoardo-Pedicillo@users.noreply.github.com>
2023-03-20 10:17:31 +0400

    Update src/qibocal/fitting/classifier/rbf_svm.py
    Co-authored-by: Alessandro Candido <candido.ale@gmail.com>

--- src/qibocal/fitting/classifier/rbf_svm.py
+++ src/qibocal/fitting/classifier/rbf_svm.py
@@ -19,7 +19,7 @@ def hyperopt(x_train, y_train, _path):
     clf = SVC(gamma="auto")
 
     cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
-    space = dict()
+    space = {}
     space["C"] = np.linspace(0.01, 2, num=50)
     space["degree"] = [2, 3, 4]
     search = GridSearchCV(clf, space, scoring="accuracy", n_jobs=-1, cv=cv)
