b30bee5ec5efd8958c75e858bd7bffcb2b00861b
Andrea <andreapasquale97@gmail.com>
2023-03-22 16:09:23 +0400

    Other suggestions from Alessandro

--- src/qibocal/cli/auto_builder.py
+++ src/qibocal/cli/auto_builder.py
@@ -1,19 +1,25 @@
 import datetime
 import os
+import tempfile
 from pathlib import Path
 
 import yaml
 
-from qibocal.auto.execute import Executor
+from qibocal.auto.execute import Executor, History
 
 from .builders import ActionBuilder, load_yaml
+
+META = "meta.yml"
+RUNCARD = "runcard.yml"
 
 
 class AutoCalibrationBuilder(ActionBuilder):
     def __init__(self, runcard, folder=None, force=False):
         super().__init__(runcard, folder, force)
+        # TODO: modify folder in Path in ActionBuilder
+        self.folder = Path(self.folder)
         self.executor = Executor.load(
-            self.runcard, self.platform, self.qubits, Path(self.folder)
+            self.runcard, self.platform, self.qubits, self.folder
         )
 
     def run(self):
@@ -32,38 +38,37 @@ class AutoCalibrationBuilder(ActionBuilder):
         from qibocal.web.report import create_autocalibration_report
 
         # update end time
-        meta = load_yaml(f"{self.folder}/meta.yml")
+        meta = yaml.safe_load((self.folder / META).read_text())
         e = datetime.datetime.now(datetime.timezone.utc)
         meta["end-time"] = e.strftime("%H:%M:%S")
-        with open(f"{self.folder}/meta.yml", "w") as file:
+        with open(self.folder / META, "w") as file:
             yaml.dump(meta, file)
 
         create_autocalibration_report(self.folder, self.executor.history)
 
 
 class AutoCalibrationReportBuilder:
-    def __init__(self, path, history):
+    def __init__(self, path: Path, history: History):
         self.path = path
-        self.metadata = load_yaml(os.path.join(path, "meta.yml"))
+        self.metadata = yaml.safe_load((path / META).read_text())
 
         # find proper path title
-        base, self.title = os.path.join(os.getcwd(), path), ""
+        base, self.title = os.getcwd() / path, ""
         while self.title in ("", "."):
             base, self.title = os.path.split(base)
 
-        self.runcard = load_yaml(os.path.join(path, "runcard.yml"))
+        self.runcard = yaml.safe_load((path / RUNCARD).read_text())
         self.format = self.runcard.get("format")
         self.qubits = self.runcard.get("qubits")
 
         self.history = history
 
-    def get_routine_name(self, routine, iteration):
+    def routine_name(self, routine, iteration):
         """Prettify routine's name for report headers."""
-        return routine.replace("_", " ").title() + f" {iteration}"
+        name = routine.replace("_", " ").title()
+        return f"{name} - {iteration}"
 
     def plot(self, routine_name, iteration, qubit):
-        import tempfile
-
         node = self.history[(routine_name, iteration)]
         data = node.task.operation.data_type.load_data(
             self.path, "data", f"{routine_name}_{iteration}", "csv", "data"
