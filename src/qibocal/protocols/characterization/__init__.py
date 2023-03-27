ba1582430beb9402f2ebf599b7b7d68d3284db1a
Andrea <andreapasquale97@gmail.com>
2023-03-27 12:24:08 +0400

    Add rabi length

--- src/qibocal/protocols/characterization/__init__.py
+++ src/qibocal/protocols/characterization/__init__.py
@@ -3,6 +3,7 @@ from enum import Enum
 from .classification import single_shot_classification
 from .qubit_spectroscopy import qubit_spectroscopy
 from .rabi import rabi_amplitude
+from .rabi_length import rabi_length
 from .ramsey import ramsey
 from .resonator_punchout import resonator_punchout
 from .resonator_spectroscopy import resonator_spectroscopy
@@ -13,5 +14,6 @@ class Operation(Enum):
     resonator_punchout = resonator_punchout
     qubit_spectroscopy = qubit_spectroscopy
     rabi_amplitude = rabi_amplitude
+    rabi_length = rabi_length
     ramsey = ramsey
     single_shot_classification = single_shot_classification
