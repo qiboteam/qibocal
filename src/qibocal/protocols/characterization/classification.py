fix classification fit units and adding mean state 1

src/qibocal/protocols/characterization/classification.py
@@ -30,9 +30,8 @@ class SingleShotClassificationData(DataUnits):
 class SingleShotClassificationResults(Results):
     threshold: Dict[List[Tuple], str] = field(metadata=dict(update="threshold"))
     rotation_angle: Dict[List[Tuple], str] = field(metadata=dict(update="iq_angle"))
-    mean_gnd_state: Dict[List[Tuple], str] = field(
-        metadata=dict(update="mean_gnd_state")
-    )
+    average_state0: Dict[List[Tuple], str]
+    average_state1: Dict[List[Tuple], str]
     fidelity: Dict[List[Tuple], str]
     assignment_fidelity: Dict[List[Tuple], str]
 
@@ -134,7 +133,8 @@ def _fit(data: SingleShotClassificationData) -> SingleShotClassificationResults:
     nshots = data.df["nshots"].unique()
     thresholds, rotation_angles = {}, {}
     fidelities, assignment_fidelities = {}, {}
-    mean_gnd_state = {}
+    average_state0 = {}
+    average_state1 = {}
 
     for qubit in qubits:
         qubit_data = data.df[data.df["qubit"] == qubit].drop(
@@ -190,14 +190,17 @@ def _fit(data: SingleShotClassificationData) -> SingleShotClassificationResults:
         errors_state0 = cum_distribution_state0[argmax]
         fidelity = cum_distribution_diff[argmax] / nshots
         assignment_fidelity = 1 - (errors_state1 + errors_state0) / nshots / 2
+        rotation_angle = (rotation_angle * 360 / (2 * np.pi)) % 360
+
         thresholds[qubit] = threshold
         rotation_angles[qubit] = rotation_angle
         fidelities[qubit] = fidelity[0]
-        mean_gnd_state[qubit] = origin
+        average_state0[qubit] = iq_mean_state0
+        average_state1[qubit] = iq_mean_state1
         assignment_fidelities[qubit] = assignment_fidelity[0]
 
     return SingleShotClassificationResults(
-        thresholds, rotation_angles, mean_gnd_state, fidelities, assignment_fidelities
+        thresholds, rotation_angles, average_state0, average_state1, fidelities, assignment_fidelities
     )
 
 
@@ -273,14 +276,15 @@ def _plot(
         state0_data["q"].min(),
         state1_data["q"].min(),
     )
-    print(fit.mean_gnd_state)
+
     fitting_report = (
         fitting_report
-        + f"q{qubit}/r{report_n} | threshold: {fit.threshold[qubit]:.3f} <br>"
-        + f"q{qubit}/r{report_n} | rotation_angle: {fit.rotation_angle[qubit]:.3f} rad <br>"
-        + f"q{qubit}/r{report_n} | mean_gnd_state: {fit.mean_gnd_state[qubit]:.3f} <br>"
+        + f"q{qubit}/r{report_n} | average state 0: {fit.average_state0[qubit]:.7f} <br>"
+        + f"q{qubit}/r{report_n} | average state 1: {fit.average_state1[qubit]:.7f} <br>"
+        + f"q{qubit}/r{report_n} | rotation_angle: {fit.rotation_angle[qubit]:.3f} degrees <br>"
+        + f"q{qubit}/r{report_n} | threshold: {fit.threshold[qubit]:.6f} <br>"
         + f"q{qubit}/r{report_n} | fidelity: {fit.fidelity[qubit]:.3f} <br>"
-        + f"q{qubit}/r{report_n} | assignment_fidelity: {fit.assignment_fidelity[qubit]:.3f} <br>"
+        + f"q{qubit}/r{report_n} | assignment fidelity: {fit.assignment_fidelity[qubit]:.3f} <br>"
     )
 
     fig.update_layout(