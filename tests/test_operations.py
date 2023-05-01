344c43e4a906e2cd9254f04ddedfea3de8a40017
Andrea <andreapasquale97@gmail.com>
2023-05-01 14:13:18 +0400

    Remove line used for debugging

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -16,7 +16,6 @@ platform = Platform("dummy")
 def test_data_acquisition(action):
     """Test data acquisition for all routines using dummy"""
     task = Task(action)
-    print(task)
     if "low attenuation" in task.id or "high attenuation" in task.id:
         with pytest.raises(NotImplementedError):
             task.operation.acquisition(task.parameters, platform, platform.qubits)
