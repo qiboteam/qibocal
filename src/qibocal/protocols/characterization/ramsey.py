bad2771a97fe61ebce2c74dbada46203778dbd92
andrea-pasquale <andreapasquale97@gmail.com>
2023-05-18 17:29:51 +0400

    Better explanation in comment

--- src/qibocal/protocols/characterization/ramsey.py
+++ src/qibocal/protocols/characterization/ramsey.py
@@ -215,7 +215,7 @@ def _fit(data: RamseyData) -> RamseyResults:
             delta_phys = data.detuning_sign * int(
                 (delta_fitting - data.n_osc / data.t_max) * 1e9
             )
-            # FIXME: for qblox the correct formula is
+            # FIXME: for qblox the correct formula is the following (there is a bug related to the phase)
             # corrected_qubit_frequency = int(qubit_freq + delta_phys)
             corrected_qubit_frequency = int(qubit_freq - delta_phys)
             t2 = 1.0 / popt[4]
