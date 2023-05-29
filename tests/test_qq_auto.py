95b1f0201a39860335faa7b5a44ba26450f414bd
andrea-pasquale <andreapasquale97@gmail.com>
2023-05-30 00:24:04 +0400

    Increase coverage

--- tests/test_qq_auto.py
+++ tests/test_qq_auto.py
@@ -10,10 +10,11 @@ PATH_TO_RUNCARD = pathlib.Path(__file__).parent / "runcards/test_autocalibration
 
 # FIXME: https://github.com/qiboteam/qibolab/issues/454
 @pytest.mark.skip(reason="Waiting for dummy platform support.")
-def test_qq_auto_dummy():
+@pytest.mark.parametrize("update", [True, False])
+def test_qq_auto_dummy(update):
     """Test full calibration pipeline for autocalibration."""
     folder = tempfile.mkdtemp()
-    builder = AutoCalibrationBuilder(PATH_TO_RUNCARD, folder, force=True)
+    builder = AutoCalibrationBuilder(PATH_TO_RUNCARD, folder, force=True, update=update)
     builder.run()
     builder.dump_platform_runcard()
     builder.dump_report()
