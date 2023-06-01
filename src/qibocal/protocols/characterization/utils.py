d5b2fcb5149c600b3c52adde6c4cf3dc7b7a3ffd
andrea-pasquale <andreapasquale97@gmail.com>
2023-06-01 18:08:10 +0400

    Remove amplitude update in high power regime

--- src/qibocal/protocols/characterization/utils.py
+++ src/qibocal/protocols/characterization/utils.py
@@ -153,6 +153,8 @@ def spectroscopy_plot(data, fit: Results, qubit):
 
     if fit.amplitude[qubit] is not None:
         fitting_report += f"{qubit} | amplitude: {fit.amplitude[qubit]} <br>"
+        if data.power_level is PowerLevel.low:
+            fit.amplitude.clear()
 
     if data.__class__.__name__ == "ResonatorSpectroscopyAttenuationData":
         if fit.attenuation[qubit] is not None and fit.attenuation[qubit] != 0:
