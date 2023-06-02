337723e04ec3c0a3d588cbdfdc2bbce3e836885f
Liza Vodovozova <vodovozovalizza@gmail.com>
2023-06-02 10:24:22 +0400

    Updated documentation and utils

--- src/qibocal/protocols/characterization/RB/result.py
+++ src/qibocal/protocols/characterization/RB/result.py
@@ -24,12 +24,14 @@ class DecayResult(Results):
     # x and y data.
     x: Union[List[Number], np.ndarray]
     y: Union[List[Number], np.ndarray]
-    # Fitting parameters.
+    # Fitting parameters and errors.
     A: Optional[Number] = None
     Aerr: Optional[Number] = None
     p: Optional[Number] = None
     perr: Optional[Number] = None
+    # Model and the fitting function
     model: Iterable = field(default=exp1_func)
+    fit_func: Iterable = field(default=fit_exp1_func)
     """The result data should behave according to that to that model."""
 
     def __post_init__(self):
@@ -38,11 +40,11 @@ class DecayResult(Results):
         """
         if len(self.y) != len(self.x):
             raise ValueError(
-                "Length of y and x must agree. len(x)={} != len(y)={}".format(
-                    len(self.x), len(self.y)
-                )
+                f"Length of y and x must agree. len(x)={len(self.x)} != len(y)={len(self.y)}"
             )
         self.y_scatter = None
+        self.error_y = None
+        self.fig = None
         if isinstance(self.y[0], Iterable):
             self.y_scatter = self.y
             self.y = [np.mean(y_row) for y_row in self.y]
@@ -67,11 +69,21 @@ class DecayResult(Results):
     def fitting_errors(self, value):
         self.Aerr, self.perr = value
 
-    def fit_func(self, x, y, **kwargs):
-        return fit_exp1_func(x, y, **kwargs)
+    def fit(
+        self, n_bootstrap: int = 0, sample_size: int = 1, uncertainties=None, **kwargs
+    ):
+        """Fits the data, all parameters given through
+        kwargs will be passed on to the optimization function.
 
-    def fit(self, n_bootstrap=0, sample_size=1, uncertainties=None, **kwargs):
-        """Fits the data, all parameters given through kwargs will be passed on to the optimization function."""
+        Args:
+            n_bootstrap (int): number of bootstrap iterations.
+            sample_size (int): number of "corrected" samples from the binomial distribution.
+            uncertainties: method of computing the uncertainties if ``sigma`` in ``kwargs``
+            is not given. If ``std``, computes the standard deviation. If type ``float`` between
+            0 and 1, computes the corresponding confidence interval. If ``None``, does not compute
+            the uncertainties. Defaults to ``None``.
+
+        """
 
         def samples_to_p0(samples):
             ground = np.array([0] * len(samples[0]))
@@ -172,21 +184,23 @@ class DecayResult(Results):
     def __str__(self):
         """Overwrite the representation of the object with the fitting parameters if there are any."""
 
-        if self.perr is not None:
+        if all(param is not None for param in self.fitting_params):
             return "Fit: y=Ap^x<br>A: {}<br>p: {}".format(
                 number_to_str(self.A, self.Aerr), number_to_str(self.p, self.perr)
             )
-        else:
-            return "DecayResult: Ap^x"
+        return "DecayResult: Ap^x"
 
 
 @dataclass
 class DecayWithOffsetResult(DecayResult):
     """Data being described by a single decay with offset, Ap^x + B."""
 
+    # Fitting parameters and errors
     B: Optional[Number] = None
     Berr: Optional[Number] = None
+    # Model and the fitting function
     model: Iterable = field(default=exp1B_func)
+    fit_func: Iterable = field(default=fit_exp1B_func)
     """The result data should behave according to that to that model."""
 
     def __post_init__(self):
@@ -210,20 +224,16 @@ class DecayWithOffsetResult(DecayResult):
     def fitting_errors(self, value):
         self.Aerr, self.perr, self.Berr = value
 
-    def fit_func(self, x, y, **kwargs):
-        return fit_exp1B_func(x, y, **kwargs)
-
     def __str__(self):
         """Overwrite the representation of the object with the fitting parameters if there are any."""
 
-        if self.perr is not None:
+        if all(param is not None for param in self.fitting_params):
             return "Fit: y=Ap^x+B<br>A: {}<br>p: {}<br>B: {}".format(
                 number_to_str(self.A, self.Aerr),
                 number_to_str(self.p, self.perr),
                 number_to_str(self.B, self.Berr),
             )
-        else:
-            return "DecayResult: Ap^x+B"
+        return "DecayResult: Ap^x+B"
 
 
 def plot_decay_result(
@@ -246,7 +256,7 @@ def plot_decay_result(
         go.Scatter(
             x=result.x,
             y=result.y,
-            line=dict(color="#aa6464"),
+            line={"color": "#aa6464"},
             mode="markers",
             name="average",
         )
@@ -254,28 +264,22 @@ def plot_decay_result(
     # If result.error_y is given, create a dictionary for the error bars
     if hasattr(result, "error_y") and result.error_y is not None:
         if isinstance(result.error_y, Number):
-            error_y_dict = dict(
-                type="constant",
-                value=result.error_y,
-            )
-        elif isinstance(result.error_y[0], Iterable) is True:
-            error_y_dict = dict(
-                type="data",
-                symmetric=False,
-                array=result.error_y[1],
-                arrayminus=result.error_y[0],
-            )
+            error_y_dict = {"type": "constant", "value": result.error_y}
+        elif isinstance(result.error_y[0], Iterable) is False:
+            error_y_dict = {"type": "data", "array": result.error_y}
         else:
-            error_y_dict = dict(
-                type="data",
-                array=result.error_y,
-            )
+            error_y_dict = {
+                "type": "data",
+                "symmetric": False,
+                "array": result.error_y[1],
+                "arrayminus": result.error_y[0],
+            }
         fig.add_trace(
             go.Scatter(
                 x=result.x,
                 y=result.y,
                 error_y=error_y_dict,
-                line=dict(color="#aa6464"),
+                line={"color": "#aa6464"},
                 mode="markers",
                 name="error bars",
             )
@@ -315,7 +319,7 @@ def plot_hists_result(result: DecayResult) -> go.Figure:
                 f"rgba(101, 151, 170, {count/max(counts_list)})"
                 for count in counts_list
             ],
-            text=[count for count in counts_list],
+            text=counts_list,
             hovertemplate="<br>x:%{x}<br>y:%{y}<br>count:%{text}",
             name="iterations",
         )
--- src/qibocal/protocols/characterization/RB/utils.py
+++ src/qibocal/protocols/characterization/RB/utils.py
@@ -1,4 +1,4 @@
-from math import isinf, log10
+from math import ceil, isinf, log10
 from numbers import Number
 from typing import Callable, Iterable, List, Optional, Tuple, Union
 
@@ -8,68 +8,100 @@ from pandas import DataFrame
 from qibocal.config import raise_error
 
 
+def significant_digit(number: Number):
+    """Computes the position of the first significant digit of a given number.
+
+    Args:
+        number (Number)
+
+    Returns:
+        int: position of the first significant digit or ``1`` if the given number
+        is integer, ``inf`` or ``0``. Returns ``-1`` if ``number`` is ``None``.
+    """
+    if number is None:
+        return -1
+
+    position = (
+        ceil(-log10(abs(np.real(number))))
+        if not isinf(np.real(number)) and np.real(number) != 0
+        else 1
+    )
+    if np.imag(number) != 0:
+        position = max(
+            position,
+            ceil(-log10(abs(np.imag(number))))
+            if not isinf(np.imag(number)) and np.imag(number) != 0
+            else 1,
+        )
+    return max(position, 1)
+
+
 def number_to_str(
-    value,
-    confidence: Optional[Union[float, list, np.ndarray]] = None,
+    value: Number,
+    uncertainty: Optional[Union[Number, Iterable]] = None,
     precision: Optional[int] = None,
 ):
     """Converts a number into a string.
 
     Args:
-        value (int or float or complex): the number to display
-        confidence (float or list, optional): number or 2-element interval with the unvertainty of the value.
-        Defaults to ``None``
-        precision (int, optional): number of floating points of the displayed value. If ``None`` and
-        ``confidence`` is not given, defaults to 3. Defaults to ``None``.
+        value (Number): the number to display
+        uncertainty (Number or Iterable, optional): number or 2-element
+        interval with the low and high uncertainties of the ``value``. Defaults to ``None``.
+        precision (int, optional): nonnegative number of floating points of the displayed value.
+        If ``None``, defaults to the first significant digit of ``uncertainty``
+        or ``3`` if ``uncertainty`` is ``None``. Defaults to ``None``.
 
     Returns:
-        str: The number expressed as a string, with the confidence interval if given.
+        str: The number expressed as a string, with the uncertainty if given.
     """
-
-    if confidence is None:
+    # If uncertainty is not given, return the value with precision
+    if uncertainty is None:
         precision = precision if precision is not None else 3
         return f"{value:.{precision}f}"
 
-    if isinstance(confidence, Iterable) is True:
-        if len(confidence) != 2:
+    if precision is not None:
+        if not isinstance(precision, int):
             raise_error(
-                ValueError,
-                f"`confidence` list must contain 2 elements. Got {len(confidence)} instead.",
+                TypeError,
+                f"`precision` must be of type int. Got {type(precision)} instead.",
             )
-        if any(confidence < 0):
+        if precision < 0:
             raise_error(
                 ValueError,
-                f"`confidence` values cannot be negative. Got {confidence} instead.",
+                f"`precision` cannot be negative. Got {precision}.",
             )
 
-        if all(c == 0 or isinf(c) for c in confidence):
-            precision = precision if precision is not None else 3
-            return f"{value:.{precision}f}"
+    if isinstance(uncertainty, Number):
         if precision is None:
-            precision = (
-                max(*[-int(log10(c)) if not isinf(c) else 0 for c in confidence], 0) + 1
-            )
-        if abs(confidence[0] - confidence[1]) < 10 ** (-precision):
-            return f"{value:.{precision}f} \u00B1 {confidence[0]:.{precision}f}"
-        return f"{value:.{precision}f} +{confidence[1]:.{precision}f} / -{confidence[0]:.{precision}f}"
+            precision = significant_digit(uncertainty)
+        return f"{value:.{precision}f} \u00B1 {uncertainty:.{precision}f}"
 
-    if isinstance(confidence, Number) is False:
+    if isinstance(uncertainty, Iterable) is False:
         raise_error(
             TypeError,
-            f"`confidence` must be iterable or a number. Got {type(confidence)} instead.",
+            f"`uncertainty` must be of type Iterable or a Number. Got {type(uncertainty)} instead.",
         )
 
-    if confidence < 0:
+    if len(uncertainty) != 2:
         raise_error(
             ValueError,
-            f"`confidence` cannot be negative. Got {confidence} instead.",
+            f"`uncertainty` list must contain 2 elements. Got {len(uncertainty)} instead.",
         )
-    if confidence == 0 or isinf(confidence):
+
+    # If the is a None uncertainty, return the value with precision
+    if any(error is None for error in uncertainty):
         precision = precision if precision is not None else 3
         return f"{value:.{precision}f}"
+
+    # If precision is None, get the first significant digit of the uncertainty
     if precision is None:
-        precision = max(-int(log10(confidence)), 0) + 1
-    return f"{value:.{precision}f} \u00B1 {confidence:.{precision}f}"
+        precision = max(significant_digit(error) for error in uncertainty)
+
+    # Check if both uncertainties are equal up to precision
+    if np.round(uncertainty[0], precision) == np.round(uncertainty[1], precision):
+        return f"{value:.{precision}f} \u00B1 {uncertainty[0]:.{precision}f}"
+
+    return f"{value:.{precision}f} +{uncertainty[1]:.{precision}f} / -{uncertainty[0]:.{precision}f}"
 
 
 def extract_from_data(
