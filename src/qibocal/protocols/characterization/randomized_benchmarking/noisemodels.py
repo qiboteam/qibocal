162d19b64a8ef479e48495fae82e9fabb2bd8343
Liza Vodovozova <vodovozovalizza@gmail.com>
2023-06-05 12:27:58 +0400

    fix tests

--- src/qibocal/protocols/characterization/randomized_benchmarking/noisemodels.py
+++ src/qibocal/protocols/characterization/randomized_benchmarking/noisemodels.py
@@ -36,7 +36,7 @@ class PauliErrorOnAll(NoiseModel):
     def build(self, *params):
         # TODO for qibo v.0.1.14 change *params to list(zip(["X", "Y", "Z"], params))
         # Add PauliError to gates.Gate
-        self.add(PauliError(list(zip(["X", "Y", "Z"], params))))
+        self.add(PauliError(*params))
 
 
 class PauliErrorOnX(PauliErrorOnAll):
