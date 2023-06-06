58b5d46722ed8caeac6caa7a5254f541d7bea698
Liza Vodovozova <vodovozovalizza@gmail.com>
2023-06-06 16:25:25 +0400

    Fix tests.

--- tests/test_qq_auto.py
+++ tests/test_qq_auto.py
@@ -6,12 +6,24 @@ import pytest
 from qibocal.cli.auto_builder import AutoCalibrationBuilder
 
 PATH_TO_RUNCARD = pathlib.Path(__file__).parent / "runcards/test_autocalibration.yml"
+PATH_TO_RUNCARD_SIM = (
+    pathlib.Path(__file__).parent / "runcards/test_autocalibration_sim.yml"
+)
 
 
 def test_qq_auto_dummy():
     """Test full calibration pipeline for autocalibration."""
     folder = tempfile.mkdtemp()
     builder = AutoCalibrationBuilder(PATH_TO_RUNCARD, folder, force=True)
+    builder.run()
+    builder.dump_platform_runcard()
+    builder.dump_report()
+
+
+def test_qq_auto_numpy():
+    """Test full calibration pipeline for autocalibration."""
+    folder = tempfile.mkdtemp()
+    builder = AutoCalibrationBuilder(PATH_TO_RUNCARD_SIM, folder, force=True)
     builder.run()
     builder.dump_platform_runcard()
     builder.dump_report()
