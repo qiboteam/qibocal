8d324e73912a617693e223fff055fde8bfcb19fc
Liza Vodovozova <vodovozovalizza@gmail.com>
2023-06-12 12:20:54 +0400

    Set global backend

--- src/qibocal/auto/runcard.py
+++ src/qibocal/auto/runcard.py
@@ -2,6 +2,7 @@
 from functools import cached_property
 from typing import Any, Dict, List, NewType, Optional, Union
 
+import qibo
 from pydantic import Field
 from pydantic.dataclasses import dataclass
 from qibo.backends import Backend, construct_backend
@@ -53,6 +54,7 @@ class Runcard:
     @cached_property
     def backend_obj(self) -> Backend:
         """Allocate backend."""
+        qibo.set_backend(self.backend, self.platform)
         return construct_backend(self.backend, self.platform)
 
     @property
