e232ad7cf86480e99d89b7b412b0b17aafc144c8
Jacfomg <juan.a.c.f.99@gmail.com>
2023-06-13 17:51:41 +0400

    1st commit

--- src/qibocal/protocols/characterization/fast_reset/fast_reset.py
+++ src/qibocal/protocols/characterization/fast_reset/fast_reset.py
@@ -2,14 +2,14 @@ from dataclasses import dataclass
 from typing import Dict, Optional, Union
 
 import numpy as np
-from qibolab import AcquisitionType, AveragingMode, ExecutionParameters
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from qibolab import ExecutionParameters
 from qibolab.platform import Platform
 from qibolab.pulses import PulseSequence
 
 from qibocal.auto.operation import Parameters, Qubits, Results, Routine
 from qibocal.data import DataUnits
-
-from .utils import signal_0_1
 
 # TODO: IBM Fast Reset until saturation loop
 # https://quantum-computing.ibm.com/lab/docs/iql/manage/systems/reset/backend_reset
@@ -42,7 +42,7 @@ class FastResetData(DataUnits):
     def __init__(self):
         super().__init__(
             "data",
-            options=["qubit", "sample", "state"],
+            options=["probability", "qubit", "state", "iteration", "fast_reset"],
         )
 
 
@@ -51,77 +51,110 @@ def _acquisition(
 ) -> FastResetData:
     """Data acquisition for resonator spectroscopy."""
 
-    state0_sequence = PulseSequence()
-    state1_sequence = PulseSequence()
+    data = FastResetData()
 
     RX_pulses = {}
     ro_pulses = {}
+    sequence = PulseSequence()
     for qubit in qubits:
         RX_pulses[qubit] = platform.create_RX_pulse(qubit, start=0)
         ro_pulses[qubit] = platform.create_qubit_readout_pulse(
             qubit, start=RX_pulses[qubit].finish
         )
 
-        state0_sequence.add(ro_pulses[qubit])
-        state1_sequence.add(RX_pulses[qubit])
-        state1_sequence.add(ro_pulses[qubit])
+        sequence.add(RX_pulses[qubit])
+        sequence.add(ro_pulses[qubit])
 
-    # create a DataUnits object to store the results
-    data = DataUnits(
-        name="data",
-        quantities={"weights": "dimensionless"},
-        options=["qubit", "sample", "state"],
-    )
-
-    data = FastResetData()
-
-    # execute the first pulse sequence
-    state0_results = platform.execute_pulse_sequence(
-        state0_sequence,
-        options=ExecutionParameters(
+    # execute the pulse sequence
+    results = platform.execute_pulse_sequence(
+        sequence,
+        ExecutionParameters(
             nshots=params.nshots,
             relaxation_time=params.relaxation_time,
-            acquisition_type=AcquisitionType.RAW,
-            averaging_mode=AveragingMode.CYCLIC,
+            fast_reset=True,
         ),
     )
 
     # retrieve and store the results for every qubit
-    for qubit in qubits:
-        r = state0_results[ro_pulses[qubit].serial].serialize
-        number_of_samples = len(r["MSR[V]"])
-        r.update(
-            {
-                "qubit": [qubit] * number_of_samples,
-                "sample": np.arange(number_of_samples),
-                "state": [0] * number_of_samples,
-            }
-        )
+    for ro_pulse in ro_pulses.values():
+        r = {
+            "sample": results[ro_pulse.serial].samples,
+            "qubit": [ro_pulse.qubit] * params.nshots,
+            "iteration": np.arange(params.nshots),
+            "state": [1] * params.nshots,
+            "fast_reset": ["True"] * params.nshots,
+        }
         data.add_data_from_dict(r)
 
-    # execute the second pulse sequence
-    state1_results = platform.execute_pulse_sequence(
-        state1_sequence,
-        options=ExecutionParameters(
+    results = platform.execute_pulse_sequence(
+        sequence,
+        ExecutionParameters(
             nshots=params.nshots,
             relaxation_time=params.relaxation_time,
-            acquisition_type=AcquisitionType.RAW,
-            averaging_mode=AveragingMode.CYCLIC,
+            fast_reset=False,
         ),
     )
+
     # retrieve and store the results for every qubit
-    for qubit in qubits:
-        # average msr, phase, i and q over the number of shots defined in the runcard
-        r = state1_results[ro_pulses[qubit].serial].serialize
-        number_of_samples = len(r["MSR[V]"])
-        r.update(
-            {
-                "qubit": [qubit] * number_of_samples,
-                "sample": np.arange(number_of_samples),
-                "state": [1] * number_of_samples,
-            }
-        )
+    for ro_pulse in ro_pulses.values():
+        r = {
+            "sample": results[ro_pulse.serial].samples,
+            "qubit": [ro_pulse.qubit] * params.nshots,
+            "iteration": np.arange(params.nshots),
+            "state": [1] * params.nshots,
+            "fast_reset": ["False"] * params.nshots,
+        }
         data.add_data_from_dict(r)
+
+    # # Is it useful to inspect state 0 ?
+    # ro_pulses = {}
+    # sequence = PulseSequence()
+    # for qubit in qubits:
+    #     ro_pulses[qubit] = platform.create_qubit_readout_pulse(
+    #         qubit, start=0
+    #     )
+    #     sequence.add(ro_pulses[qubit])
+
+    # # execute the pulse sequence
+    # results = platform.execute_pulse_sequence(
+    #     sequence,
+    #     ExecutionParameters(
+    #         nshots=params.nshots,
+    #         relaxation_time=params.relaxation_time,
+    #         fast_reset = True,
+    #     ),
+    # )
+
+    # # retrieve and store the results for every qubit
+    # for ro_pulse in ro_pulses.values():
+    #     r = {
+    #         "sample":results[ro_pulse.serial].samples,
+    #         "qubit": [ro_pulse.qubit] * params.nshots,
+    #         "iteration": np.arange(params.nshots),
+    #         "state": [0] * params.nshots,
+    #         "fast_reset": ["True"] * params.nshots,
+    #     }
+    #     data.add_data_from_dict(r)
+
+    # results = platform.execute_pulse_sequence(
+    #     sequence,
+    #     ExecutionParameters(
+    #         nshots=params.nshots,
+    #         relaxation_time=params.relaxation_time,
+    #         fast_reset = False,
+    #     ),
+    # )
+
+    # # retrieve and store the results for every qubit
+    # for ro_pulse in ro_pulses.values():
+    #     r = {
+    #         "sample":results[ro_pulse.serial].samples,
+    #         "qubit": [ro_pulse.qubit] * params.nshots,
+    #         "iteration": np.arange(params.nshots),
+    #         "state": [0] * params.nshots,
+    #         "fast_reset": ["False"] * params.nshots,
+    #     }
+    #     data.add_data_from_dict(r)
 
     return data
 
@@ -129,50 +162,81 @@ def _acquisition(
 def _fit(data: FastResetData) -> FastResetResults:
     """Post-processing function for FastReset."""
 
-    qubits = data.df["qubit"].unique()
+    # Getting some kind of fidelity
 
-    # np.conj to account the two phase-space evolutions of the readout state
-    integration_weights = {}
-
-    for qubit in qubits:
-        qubit_data_df = data.df[data.df["qubit"] == qubit]
-
-        qubit_state0_data_df = qubit_data_df[qubit_data_df["state"] == 0]
-        qubit_state1_data_df = qubit_data_df[qubit_data_df["state"] == 1]
-
-        state0 = (
-            qubit_state0_data_df["i"].pint.to("uV").pint.magnitude
-            + 1j * qubit_state0_data_df["q"].pint.to("uV").pint.magnitude
-        )
-        state1 = (
-            qubit_state1_data_df["i"].pint.to("uV").pint.magnitude
-            + 1j * qubit_state1_data_df["q"].pint.to("uV").pint.magnitude
-        )
-
-        number_of_samples = len(qubit_state0_data_df["i"].pint.to("uV").pint.magnitude)
-
-        state0 = state0.to_numpy()
-        state1 = state1.to_numpy()
-
-        samples_kernel = np.conj(state1 - state0)
-        # Remove nans
-        samples_kernel = samples_kernel[~np.isnan(samples_kernel)]
-
-        samples_kernel_origin = (
-            samples_kernel - samples_kernel.real.min() - 1j * samples_kernel.imag.min()
-        )  # origin offsetted
-        samples_kernel_normalized = (
-            samples_kernel_origin / np.abs(samples_kernel_origin).max()
-        )  # normalized
-
-        integration_weights[qubit] = abs(samples_kernel_normalized)
-
-    return FastResetResults(integration_weights)
+    return FastResetResults({})
 
 
 def _plot(data: FastResetData, fit: FastResetResults, qubit):
     """Plotting function for FastReset."""
-    return signal_0_1(data, fit, qubit)
+
+    # Maybe the plot can just be something like a confusion matrix between 0s and 1s ???
+
+    figures = []
+    fig = go.Figure()
+
+    fitting_report = ""
+
+    # qubit_data = data.df[data.df["qubit"] == qubit]
+
+    # state0_data = qubit_data[data.df["state"] == 0].drop(
+    #     columns=["MSR", "phase", "qubit"]
+    # )
+    # state1_data = qubit_data[data.df["state"] == 1].drop(
+    #     columns=["MSR", "phase", "qubit"]
+    # )
+
+    iterations = data.df["iteration"]
+    truncate_index = data.df.fast_reset[data.df.fast_reset == "False"].index.tolist()
+
+    fr_df = data.df.truncate(after=truncate_index[0] - 1)
+    fr_states = fr_df["sample"]
+
+    nfr_df = data.df.truncate(before=truncate_index[0])
+    nfr_states = nfr_df["sample"]
+
+    state0_count = fr_states.value_counts()[0]
+    state1_count = fr_states.value_counts()[1]
+    error = (state1_count - state0_count) / len(fr_states)
+
+    fig.add_trace(
+        go.Scatter(
+            x=iterations,
+            y=fr_states,
+            mode="markers",
+            name=f"q{qubit}/r_fast_reset",
+            showlegend=True,
+            legendgroup=f"q{qubit}/r_fast_reset",
+        ),
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=iterations,
+            y=nfr_states,
+            mode="markers",
+            name=f"q{qubit}/r_no_fast_reset",
+            showlegend=True,
+            legendgroup=f"q{qubit}/r_no_fast_reset",
+        ),
+    )
+
+    fitting_report += f"q{qubit}/r | state0 count: {state0_count:.0f}<br>"
+    fitting_report += f"q{qubit}/r | state1 count: {state1_count:.0f}<br>"
+    fitting_report += f"q{qubit}/r | Error: {error:.6f}<br>"
+    fitting_report += f"q{qubit}/r | Fidelity(add 0 to 1 error): {(1 - error):.6f}<br>"
+
+    # last part
+    fig.update_layout(
+        showlegend=True,
+        uirevision="0",  # ``uirevision`` allows zooming while live plotting
+        xaxis_title="Shot",
+        yaxis_title="State",
+    )
+
+    figures.append(fig)
+
+    return figures, fitting_report
 
 
 fast_reset = Routine(_acquisition, _fit, _plot)
