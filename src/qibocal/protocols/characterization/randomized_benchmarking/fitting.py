812d01243c4d226d699d7cb055fc6c54b83ccf48
Andrea <andreapasquale97@gmail.com>
2023-06-16 11:59:12 +0400

    Fix rb results

--- src/qibocal/protocols/characterization/randomized_benchmarking/fitting.py
+++ src/qibocal/protocols/characterization/randomized_benchmarking/fitting.py
@@ -125,6 +125,7 @@ def fit_exp1B_func(
                 ydata,
                 **kwargs,
             )
+            popt = tuple(popt)
             perr = tuple(np.sqrt(np.diag(pcov)))
         except Exception as e:
             log.warning("Ap^x+B fit: the fitting was not succesful. %s", e)
