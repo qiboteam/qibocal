8bfcee32c307c747606316fd7ea18988a51347fc
Jacfomg <juan.a.c.f.99@gmail.com>
2023-06-22 11:03:13 +0400

    import

--- src/qibocal/protocols/characterization/signal_experiments/time_of_flight_readout.py
+++ src/qibocal/protocols/characterization/signal_experiments/time_of_flight_readout.py
@@ -4,7 +4,6 @@ from typing import Dict, Optional
 import numpy as np
 import numpy.typing as npt
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 from qibolab import AcquisitionType, AveragingMode, ExecutionParameters
 from qibolab.platform import Platform
 from qibolab.pulses import PulseSequence
