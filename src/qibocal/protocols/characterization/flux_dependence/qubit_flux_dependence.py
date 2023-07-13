fixed units problems

src/qibocal/protocols/characterization/flux_dependence/qubit_flux_dependence.py
@@ -15,6 +15,7 @@ from qibocal.auto.operation import Data, Parameters, Qubits, Results, Routine
 from qibocal.config import log
 
 from . import utils
+from ..utils import HZ_TO_GHZ
 
 
 # TODO: implement cross-talk
@@ -230,6 +231,7 @@ def _fit(data: QubitFluxData) -> QubitFluxResults:
                     # p0=[max_c, xi, 0, f_q_0],
                     p0=[max_c, xi, 0, f_q_0 / scaler],
                     bounds=((-np.inf, 0, 0, 0), (np.inf, np.inf, np.inf, np.inf)),
+                    maxfev=2000000
                 )[0]
                 popt[3] *= scaler
                 f_qs = popt[3]  # Qubit frequency at sweet spot.
@@ -240,7 +242,7 @@ def _fit(data: QubitFluxData) -> QubitFluxResults:
                     0
                 ]  # Corresponding flux matrix element.
 
-                frequency[qubit] = f_qs
+                frequency[qubit] = f_qs * HZ_TO_GHZ
                 sweetspot[qubit] = popt[0]
                 # fitted_parameters = xi, d, f_q_offset, C_ii
                 fitted_parameters[qubit] = {
@@ -260,6 +262,7 @@ def _fit(data: QubitFluxData) -> QubitFluxResults:
                     # p0=[max_c, xi, 0, Ec, Ej],
                     p0=[max_c, xi, 0, Ec / scaler, Ej / scaler],
                     bounds=((-np.inf, 0, 0, 0), (np.inf, np.inf, np.inf, np.inf)),
+                    maxfev=2000000
                 )[0]
                 popt[3] *= scaler
                 popt[4] *= scaler
@@ -273,7 +276,7 @@ def _fit(data: QubitFluxData) -> QubitFluxResults:
                     0
                 ]  # Corresponding flux matrix element.
 
-                frequency[qubit] = f_qs
+                frequency[qubit] = f_qs * HZ_TO_GHZ
                 sweetspot[qubit] = popt[0]
                 # fitted_parameters = xi, d, Ec, Ej, f_q_offset, C_ii
                 fitted_parameters[qubit] = {

src/qibocal/protocols/characterization/flux_dependence/resonator_flux_dependence.py
@@ -15,6 +15,8 @@ from qibocal.auto.operation import Data, Parameters, Qubits, Results, Routine
 from qibocal.config import log
 
 from . import utils
+from ..utils import HZ_TO_GHZ
+
 
 
 # TODO: implement cross-talk (maybe separate routine?)
@@ -231,6 +233,7 @@ def _fit(data: ResonatorFluxData) -> ResonatorFluxResults:
                         (-np.inf, 0, 0, 0, 0, 0),
                         (np.inf, np.inf, np.inf, np.inf, np.inf, np.inf),
                     ),
+                    maxfev=2000000
                 )[0]
                 popt[4] *= scaler
                 popt[5] *= scaler
@@ -245,7 +248,7 @@ def _fit(data: ResonatorFluxData) -> ResonatorFluxResults:
                     0
                 ]  # Corresponding flux matrix element.
 
-                frequency[qubit] = f_rs
+                frequency[qubit] = f_rs * HZ_TO_GHZ
                 sweetspot[qubit] = popt[0]
                 # fitted_parameters = xi, d, f_q/bare_resonator_frequency, g, bare_resonator_frequency, f_qs, f_r_offset, C_ii
                 fitted_parameters[qubit] = {
@@ -279,6 +282,7 @@ def _fit(data: ResonatorFluxData) -> ResonatorFluxResults:
                         (0, 0, -np.inf, 0, 0, 0, 0),
                         (np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf),
                     ),
+                    maxfev=2000000
                 )[0]
                 popt[0] *= scaler
                 popt[1] *= scaler
@@ -297,7 +301,7 @@ def _fit(data: ResonatorFluxData) -> ResonatorFluxResults:
                     2
                 ]  # Corresponding flux matrix element.
 
-                frequency[qubit] = f_rs
+                frequency[qubit] = f_rs * HZ_TO_GHZ
                 sweetspot[qubit] = popt[2]
                 # fitted_parameters = xi, d, g, Ec, Ej, bare_resonator_frequency, f_qs, f_r_offset, C_ii
                 fitted_parameters[qubit] = {

src/qibocal/protocols/characterization/flux_dependence/utils.py
@@ -4,7 +4,7 @@ from plotly.subplots import make_subplots
 from scipy.special import mathieu_a, mathieu_b
 from sklearn.linear_model import Ridge
 
-from ..utils import HZ_TO_GHZ, V_TO_UV
+from ..utils import HZ_TO_GHZ, V_TO_UV, GHZ_TO_HZ
 
 
 def flux_dependence_plot(data, fit, qubit):
@@ -91,7 +91,7 @@ def flux_dependence_plot(data, fit, qubit):
                     fit.sweetspot[qubit],
                     params["Xi"],
                     params["d"],
-                    fit.frequency[qubit],
+                    fit.frequency[qubit] * GHZ_TO_HZ,
                 ]
                 freq_fit = freq_q_transmon(biases1, *popt) * HZ_TO_GHZ
 
@@ -129,7 +129,7 @@ def flux_dependence_plot(data, fit, qubit):
     fig.update_yaxes(title_text="Bias (V)", row=1, col=2)
 
     if fit.frequency[qubit] != 0:
-        fitting_report += f"{qubit} | {fitting_report_label}: {fit.frequency[qubit]:,.1f} Hz<br>"
+        fitting_report += f"{qubit} | {fitting_report_label}: {fit.frequency[qubit]:,.5f} GHz<br>"
     else:
         fitting_report += f"{qubit} | {fitting_report_label}: Fitting not successful<br>"
 