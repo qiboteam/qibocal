926157d86377c94a6b288d2df25019a677ef3b01
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2023-07-25 11:04:46 +0400

    remove comment

--- tests/test_classifiers.py
+++ tests/test_classifiers.py
@@ -1,7 +1,6 @@
 from qibocal.fitting.classifier import run
 
 
-# @pytest.fixture
 def test_load_model(tmp_path):
     classifier = run.Classifier(run.import_classifiers(["qubit_fit"])[0], tmp_path)
     classifier.create_model({"par1": 1})
