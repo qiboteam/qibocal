6abcc34c3afe5a3883c3aa49624dbe7717e3068d
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2023-07-27 15:18:03 +0400

    add nboot parameter

--- src/qibocal/protocols/characterization/ramsey.py
+++ src/qibocal/protocols/characterization/ramsey.py
@@ -8,6 +8,7 @@ from qibolab import AcquisitionType, AveragingMode, ExecutionParameters
 from qibolab.platform import Platform
 from qibolab.pulses import PulseSequence
 from qibolab.qubits import QubitId
+from qibolab.sweeper import Parameter, Sweeper, SweeperType
 from scipy.optimize import curve_fit
 
 from qibocal.auto.operation import Data, Parameters, Qubits, Results, Routine
@@ -34,6 +35,8 @@ class RamseyParameters(Parameters):
     """Number of shots."""
     relaxation_time: Optional[int] = None
     """Relaxation time (ns)."""
+    nboot: int = 0
+    """Number of bootstrap samples"""
 
 
 @dataclass
@@ -72,6 +75,8 @@ class RamseyData(Data):
     """Final delay between RX(pi/2) pulses in ns."""
     detuning_sign: int
     """Sign for induced detuning."""
+    nboot: int
+    """Number of bootstrap samples"""
     qubit_freqs: dict[QubitId, float] = field(default_factory=dict)
     """Qubit freqs for each qubit."""
     data: dict[QubitId, npt.NDArray[RamseyType]] = field(default_factory=dict)
@@ -80,7 +85,6 @@ class RamseyData(Data):
     def register_qubit(self, qubit, wait, msr, phase):
         """Store output for single qubit."""
         # to be able to handle the non-sweeper case
-        # shape = (1,) if np.isscalar(wait) else wait.shape
         ar = np.empty(msr.shape, dtype=RamseyType)
         ar["wait"] = wait
         ar["msr"] = msr
@@ -126,73 +130,76 @@ def _acquisition(
         params.delay_between_pulses_step,
     )
 
-    # create a DataUnits object to store the results,
-    # DataUnits stores by default MSR, phase, i, q
-    # additionally include wait time and t_max
     data = RamseyData(
         n_osc=params.n_osc,
         t_max=params.delay_between_pulses_end,
         detuning_sign=+1,
+        nboot=params.nboot,
         qubit_freqs=freqs,
     )
 
-    # if params.n_osc != 0:
-    # sweep the parameter
-    for wait in waits:
-        for qubit in qubits:
-            RX90_pulses2[qubit].start = RX90_pulses1[qubit].finish + wait
-            ro_pulses[qubit].start = RX90_pulses2[qubit].finish
-            if params.n_osc != 0:
-                RX90_pulses2[qubit].relative_phase = (
-                    RX90_pulses2[qubit].start
-                    * (-2 * np.pi)
-                    * (params.n_osc)
-                    / params.delay_between_pulses_end
-                )
+    if params.n_osc == 0 and params.nboot == 0:
+        sweeper = Sweeper(
+            Parameter.start,
+            waits,
+            [RX90_pulses2[qubit] for qubit in qubits],
+            type=SweeperType.ABSOLUTE,
+        )
 
-        # execute the pulse sequence
-        results = platform.execute_pulse_sequence(
+        # execute the sweep
+        results = platform.sweep(
             sequence,
             ExecutionParameters(
                 nshots=params.nshots,
                 relaxation_time=params.relaxation_time,
                 acquisition_type=AcquisitionType.INTEGRATION,
-                averaging_mode=AveragingMode.SINGLESHOT,
+                averaging_mode=AveragingMode.CYCLIC,
             ),
+            sweeper,
         )
         for qubit in qubits:
             result = results[ro_pulses[qubit].serial]
-            print(type(result.average), result.average, "CCCCCC")
             data.register_qubit(
-                qubit, wait=wait, msr=result.magnitude, phase=result.phase
+                qubit, wait=waits, msr=result.magnitude, phase=result.phase
             )
 
-    # else:
-    #     # TODO: change this part consistently
-    #     sweeper = Sweeper(
-    #         Parameter.start,
-    #         waits,
-    #         [RX90_pulses2[qubit] for qubit in qubits],
-    #         type=SweeperType.ABSOLUTE,
-    #     )
+    else:
+        for wait in waits:
+            for qubit in qubits:
+                RX90_pulses2[qubit].start = RX90_pulses1[qubit].finish + wait
+                ro_pulses[qubit].start = RX90_pulses2[qubit].finish
+                if params.n_osc != 0:
+                    RX90_pulses2[qubit].relative_phase = (
+                        RX90_pulses2[qubit].start
+                        * (-2 * np.pi)
+                        * (params.n_osc)
+                        / params.delay_between_pulses_end
+                    )
 
-    #     # execute the sweep
-    #     results = platform.sweep(
-    #         sequence,
-    #         ExecutionParameters(
-    #             nshots=params.nshots,
-    #             relaxation_time=params.relaxation_time,
-    #             acquisition_type=AcquisitionType.INTEGRATION,
-    #             averaging_mode=AveragingMode.SINGLESHOT,
-    #         ),
-    #         sweeper,
-    #     )
-    #     for qubit in qubits:
-    #         result = results[ro_pulses[qubit].serial]
-    #         print(type(result), result, "CCCCCC")
-    #         data.register_qubit(
-    #             qubit, wait=waits, msr=result.magnitude, phase=result.phase
-    #         )
+            # execute the pulse sequence
+            if params.nboot != 0:
+                averaging_mode = AveragingMode.SINGLESHOT
+            else:
+                averaging_mode = AveragingMode.CYCLIC
+
+            results = platform.execute_pulse_sequence(
+                sequence,
+                ExecutionParameters(
+                    nshots=params.nshots,
+                    relaxation_time=params.relaxation_time,
+                    acquisition_type=AcquisitionType.INTEGRATION,
+                    averaging_mode=averaging_mode,
+                ),
+            )
+            for qubit in qubits:
+                result = results[ro_pulses[qubit].serial]
+                data.register_qubit(
+                    qubit,
+                    wait=wait,
+                    msr=np.array([result.magnitude]),
+                    phase=np.array([result.phase]),
+                )
+
     return data
 
 
@@ -214,7 +221,7 @@ def _fit(data: RamseyData) -> RamseyResults:
     """
     qubits = data.qubits
     waits = np.unique(data[qubits[0]].wait)
-    nboot = 200
+    # nboot = 200
     popts = {}
     freq_av = {}
     freq_err = {}
@@ -227,160 +234,71 @@ def _fit(data: RamseyData) -> RamseyResults:
         qubit_data = data[qubit]
         qubit_freq = data.qubit_freqs[qubit]
         msrs = np.array(qubit_data[["msr"]].tolist()) * V_TO_UV
-        msrs = msrs.reshape((len(waits), -1))
-        bootstrap_samples = bootstrap(msrs, nboot)
-        print(bootstrap_samples.shape)
 
-        voltages = np.mean(bootstrap_samples, axis=1)
-        print("NNNNNN", voltages.shape)
+        msrs = msrs.reshape((len(waits), -1))
         t2s = []
         deltas_phys_list = []
-
         new_freqs = []
-        for i in range(nboot):
-            import matplotlib.pyplot as plt
+        if data.nboot != 0:
+            bootstrap_samples = bootstrap(msrs, data.nboot)
+            voltages = np.mean(bootstrap_samples, axis=1)
+            for i in range(data.nboot):
+                y = voltages[:, i]
+                x = waits
+                try:
+                    popt = fitting(x, y)
+                    delta_fitting = popt[2] / (2 * np.pi)
+                    delta_phys = data.detuning_sign * int(
+                        (delta_fitting - data.n_osc / data.t_max) * GHZ_TO_HZ
+                    )
 
-            y = voltages[:, i]
-            x = waits
-            plt.subplots()
-            plt.scatter(x, y)
-            try:
-                popt = fitting(x, y)
-                plt.plot(x, ramsey_fit(x, *popt))
-                plt.savefig(f"BOOOOT{i}.png")
-                delta_fitting = popt[2] / (2 * np.pi)
-                delta_phys = data.detuning_sign * int(
-                    (delta_fitting - data.n_osc / data.t_max) * GHZ_TO_HZ
-                )
+                    # FIXME: for qblox the correct formula is the following (there is a bug related to the phase)
+                    # corrected_qubit_frequency = int(qubit_freq + delta_phys)
+                    corrected_qubit_frequency = int(qubit_freq - delta_phys)
+                    deltas_phys_list.append(delta_phys)
+                    new_freqs.append(corrected_qubit_frequency)
+                    t2s.append(1.0 / popt[4])
+                    new_freqs.append(corrected_qubit_frequency)
 
-                # FIXME: for qblox the correct formula is the following (there is a bug related to the phase)
-                # corrected_qubit_frequency = int(qubit_freq + delta_phys)
-                corrected_qubit_frequency = int(qubit_freq - delta_phys)
-                deltas_phys_list.append(delta_phys)
-                new_freqs.append(corrected_qubit_frequency)
-                t2s.append(1.0 / popt[4])
-                new_freqs.append(corrected_qubit_frequency)
+                except Exception as e:
+                    log.warning(f"ramsey_fit: the fitting was not succesful. {e}")
+                    t2s.append(0)
+                    deltas_phys_list.append(0)
+                    new_freqs.append(0)
 
-            except Exception as e:
-                log.warning(f"ramsey_fit: the fitting was not succesful. {e}")
-                t2s.append(0)
-                deltas_phys_list.append(0)
-                new_freqs.append(0)
+                freq_av[qubit] = np.mean(new_freqs)
+                freq_err[qubit] = np.std(new_freqs)
+                t2_av[qubit] = np.mean(t2s)
+                t2_err[qubit] = np.std(t2s)
+                delta_phys_av[qubit] = np.mean(deltas_phys_list)
+                delta_phys_err[qubit] = np.std(deltas_phys_list)
 
-        freq_av[qubit] = np.mean(new_freqs)
-        freq_err[qubit] = np.std(new_freqs)
-        t2_av[qubit] = np.mean(t2s)
-        t2_err[qubit] = np.std(t2s)
-        delta_phys_av[qubit] = np.mean(deltas_phys_list)
-        delta_phys_err[qubit] = np.std(deltas_phys_list)
-
-        print(t2s)
-        print(deltas_phys_list)
-
-    for qubit in qubits:
-        qubit_data = data[qubit]
-        print("FFFFFFFF", qubit_data)
-        voltages = qubit_data.msr * V_TO_UV
-        voltages = np.array(qubit_data[["msr"]].tolist()) * V_TO_UV
-        voltages = voltages.reshape((len(waits), -1))
-
-        voltages = np.mean(voltages, axis=1).flatten()
+        msrs = np.mean(msrs, axis=1).flatten()
 
         try:
-            popts[qubit] = fitting(waits, voltages)
+            popts[qubit] = fitting(waits, msrs)
 
         except Exception as e:
             log.warning(f"ramsey_fit: the fitting was not succesful. {e}")
             popts[qubit] = [0] * 5
-        plt.subplots()
-        plt.scatter(waits, voltages)
-        plt.plot(waits, ramsey_fit(waits, *popts[qubit]))
-        plt.savefig(f"BOOOOTZZZZ{qubit}.png")
 
-    #     fitted_parameters[qubit] = popt
-    #     corrected_qubit_frequencies[qubit] = corrected_qubit_frequency * HZ_TO_GHZ
-    #     t2s[qubit] = t2
-    #     freqs_detuning[qubit] = delta_phys
+        if data.nboot == 0:
+            delta_fitting = popts[qubit][2] / (2 * np.pi)
+            delta_phys = data.detuning_sign * int(
+                (delta_fitting - data.n_osc / data.t_max) * GHZ_TO_HZ
+            )
+            # FIXME: for qblox the correct formula is the following (there is a bug related to the phase)
+            # corrected_qubit_frequency = int(qubit_freq + delta_phys)
+            corrected_qubit_frequency = int(qubit_freq - delta_phys)
+            t2 = 1.0 / popts[qubit][4]
 
-    # t2s = {}
-    # corrected_qubit_frequencies = {}
-    # freqs_detuning = {}
-    # fitted_parameters = {}
+            freq_av[qubit] = corrected_qubit_frequency
+            freq_err[qubit] = 0.0
+            t2_av[qubit] = t2
+            t2_err[qubit] = 0.0
+            delta_phys_av[qubit] = delta_phys
+            delta_phys_err[qubit] = 0.0
 
-    # for qubit in qubits:
-    #     qubit_data = data[qubit]
-    #     voltages = qubit_data.msr * V_TO_UV
-    #     times = qubit_data.wait
-    #     qubit_freq = data.qubit_freqs[qubit]
-
-    #     try:
-    #         y_max = np.max(voltages)
-    #         y_min = np.min(voltages)
-    #         y = (voltages - y_min) / (y_max - y_min)
-    #         x_max = np.max(times)
-    #         x_min = np.min(times)
-    #         x = (times - x_min) / (x_max - x_min)
-
-    #         ft = np.fft.rfft(y)
-    #         freqs = np.fft.rfftfreq(len(y), x[1] - x[0])
-    #         mags = abs(ft)
-    #         index = np.argmax(mags) if np.argmax(mags) != 0 else np.argmax(mags[1:]) + 1
-    #         f = freqs[index] * 2 * np.pi
-    #         p0 = [
-    #             0.5,
-    #             0.5,
-    #             f,
-    #             0,
-    #             0,
-    #         ]
-    #         popt = curve_fit(
-    #             ramsey_fit,
-    #             x,
-    #             y,
-    #             p0=p0,
-    #             maxfev=2000000,
-    #             bounds=(
-    #                 [0, 0, 0, -np.pi, 0],
-    #                 [1, 1, np.inf, np.pi, np.inf],
-    #             ),
-    #         )[0]
-    #         popt = [
-    #             (y_max - y_min) * popt[0] + y_min,
-    #             (y_max - y_min) * popt[1] * np.exp(x_min * popt[4] / (x_max - x_min)),
-    #             popt[2] / (x_max - x_min),
-    #             popt[3] - x_min * popt[2] / (x_max - x_min),
-    #             popt[4] / (x_max - x_min),
-    #         ]
-    #         delta_fitting = popt[2] / (2 * np.pi)
-    #         delta_phys = data.detuning_sign * int(
-    #             (delta_fitting - data.n_osc / data.t_max) * GHZ_TO_HZ
-    #         )
-    #         # FIXME: for qblox the correct formula is the following (there is a bug related to the phase)
-    #         # corrected_qubit_frequency = int(qubit_freq + delta_phys)
-    #         corrected_qubit_frequency = int(qubit_freq - delta_phys)
-    #         t2 = 1.0 / popt[4]
-
-    #     except Exception as e:
-    #         log.warning(f"ramsey_fit: the fitting was not succesful. {e}")
-    #         popt = [0] * 5
-    #         t2 = 5.0
-    #         corrected_qubit_frequency = int(qubit_freq)
-    #         delta_phys = 0
-
-    #     fitted_parameters[qubit] = popt
-    #     corrected_qubit_frequencies[qubit] = corrected_qubit_frequency * HZ_TO_GHZ
-    #     t2s[qubit] = t2
-    #     freqs_detuning[qubit] = delta_phys
-    print(
-        "WWWWWWWW",
-        freq_av,
-        freq_err,
-        t2_av,
-        t2_err,
-        delta_phys_av,
-        delta_phys_err,
-        popts,
-    )
     return RamseyResults(
         freq_av,
         freq_err,
@@ -403,13 +321,14 @@ def _plot(data: RamseyData, fit: RamseyResults, qubit):
     qubit_data = data.data[qubit]
     # qubit_freq = data.qubit_freqs[qubit]
     waits = np.unique(data.data[qubit].wait)
-    msrs = np.array(qubit_data[["msr"]].tolist()) * V_TO_UV
-    msrs = msrs.reshape((len(waits), -1))
-    nsamples = msrs.shape[1]
-    print("UUUUUU ", nsamples)
-    error_bars = np.std(msrs, axis=1).flatten() / np.sqrt(nsamples)
-    print("ZZZZZZZZZZZZ", error_bars)
-    msrs = np.mean(msrs, axis=1).flatten()
+    msrs = np.array(qubit_data[["msr"]].tolist()).flatten() * V_TO_UV
+    if data.nboot != 0:
+        msrs = msrs.reshape((len(waits), -1))
+        nsamples = msrs.shape[1]
+        error_bars = np.std(msrs, axis=1).flatten() / np.sqrt(nsamples)
+        msrs = np.mean(msrs, axis=1).flatten()
+    else:
+        error_bars = np.zeros(len(msrs))
     fig.add_trace(
         go.Scatter(
             x=waits,
@@ -455,10 +374,10 @@ def _plot(data: RamseyData, fit: RamseyResults, qubit):
             f"{qubit} | Delta_frequency: {fit.delta_phys[qubit]:,.1f} {chr(177)} {fit.error_delta_phys[qubit]:,.1f} Hz<br>"
         )
         + (
-            f"{qubit} | Drive_frequency: {fit.frequency[qubit] } {chr(177)} {fit.error_frequency[qubit]:,.1f} Hz<br>"
+            f"{qubit} | Drive_frequency: {fit.frequency[qubit] } {chr(177)} {fit.error_frequency[qubit]:,.0f} Hz<br>"
         )
         + (
-            f"{qubit} | T2: {fit.t2[qubit]:,.0f} {chr(177)} {fit.error_t2[qubit]:,.1f} ns.<br><br>"
+            f"{qubit} | T2*: {fit.t2[qubit]:,.0f} {chr(177)} {fit.error_t2[qubit]:,.0f} ns.<br><br>"
         )
     )
 
