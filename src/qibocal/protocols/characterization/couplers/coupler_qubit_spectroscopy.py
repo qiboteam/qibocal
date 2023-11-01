9d0d97e3d57c172be0ce087600d98c30aa76da0e
Jacfomg <juan.a.c.f.99@gmail.com>
2023-11-01 11:28:36 +0400

    docs

--- src/qibocal/protocols/characterization/couplers/coupler_qubit_spectroscopy.py
+++ src/qibocal/protocols/characterization/couplers/coupler_qubit_spectroscopy.py
@@ -14,7 +14,17 @@ from .utils import CouplerSpectroscopyData, CouplerSpectroscopyParameters
 def _acquisition(
     params: CouplerSpectroscopyParameters, platform: Platform, qubits: Qubits
 ) -> CouplerSpectroscopyData:
-    """Data acquisition for CouplerQubit spectroscopy."""
+    """
+    Data acquisition for CouplerQubit spectroscopy.
+
+    This consist on a frequency sweep on the qubit frequency while we change the flux coupler pulse amplitude of
+    the coupler pulse. We expect to enable the coupler during the amplitude sweep and detect an avoided crossing
+    that will be followed by the frequency sweep. This needs the qubits at resonance, the routine assumes a sweetspot
+    value for the higher frequency qubit that moves it to the lower frequency qubit instead of trying to calibrate both pulses at once. This should be run after
+    qubit_spectroscopy to further adjust the coupler sweetspot if needed and get some information
+    on the flux coupler pulse amplitude requiered to enable 2q interactions.
+
+    """
     # create a sequence of pulses for the experiment:
     # MZ
 
--- src/qibocal/protocols/characterization/couplers/coupler_resonator_spectroscopy.py
+++ src/qibocal/protocols/characterization/couplers/coupler_resonator_spectroscopy.py
@@ -19,7 +19,16 @@ from .utils import (
 def _acquisition(
     params: CouplerSpectroscopyParameters, platform: Platform, qubits: Qubits
 ) -> CouplerSpectroscopyData:
-    """Data acquisition for CouplerResonator spectroscopy."""
+    """
+    Data acquisition for CouplerResonator spectroscopy.
+
+    This consist on a frequency sweep on the readout frequency while we change the flux coupler pulse amplitude of
+    the coupler pulse. We expect to enable the coupler during the amplitude sweep and detect an avoided crossing
+    that will be followed by the frequency sweep. No need to have the qubits at resonance. This should be run after
+    resonator_spectroscopy to detect couplers and adjust the coupler sweetspot if needed and get some information
+    on the flux coupler pulse amplitude requiered to enable 2q interactions.
+
+    """
     # create a sequence of pulses for the experiment:
     # Coupler pulse - MZ
 
