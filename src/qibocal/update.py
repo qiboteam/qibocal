5cb3a423977a3aa40f8437ad96f87ef9c159275b
DavidSarlle <trapeya25@gmail.com>
2023-11-01 11:23:23 +0400

    minor changes

--- src/qibocal/update.py
+++ src/qibocal/update.py
@@ -199,17 +199,22 @@ def bare_resonator_frequency_sweetspot(bare_resonator_frequency_sweetspot: float
 def flux_to_bias(flux_to_bias: float, platform: Platform, qubit: QubitId):
     platform.qubits[qubit].flux_to_bias = float(flux_to_bias)
 
+
 def asymmetry(asymmetry: float, platform: Platform, qubit: QubitId):
     platform.qubits[qubit].asymmetry = float(asymmetry)
+
 
 def ratio_sweetspot_qubit_freq_bare_resonator_freq(ssf_brf: float, platform: Platform, qubit: QubitId):
     platform.qubits[qubit].ssf_brf = float(ssf_brf)
 
+
 def charging_energy(Ec: float, platform: Platform, qubit: QubitId):
     platform.qubits[qubit].Ec = float(Ec)
 
+
 def josephson_energy(Ej: float, platform: Platform, qubit: QubitId):
     platform.qubits[qubit].Ej = float(Ej)
+
 
 def coupling(g: float, platform: Platform, qubit: QubitId):
     platform.qubits[qubit].g = float(g) 
