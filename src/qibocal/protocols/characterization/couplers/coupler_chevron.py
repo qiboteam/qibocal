8cab3a73b303f8528d71650102666e16b6adbacd
andrea-pasquale <andreapasquale97@gmail.com>
2023-12-13 11:39:00 +0400

    refactor: general improvements from review comments

--- src/qibocal/protocols/characterization/couplers/coupler_chevron.py
+++ src/qibocal/protocols/characterization/couplers/coupler_chevron.py
@@ -1,22 +1,37 @@
+from dataclasses import dataclass
+
 import numpy as np
 from qibolab import AcquisitionType, AveragingMode, ExecutionParameters
 from qibolab.platform import Platform
 from qibolab.pulses import PulseSequence
 from qibolab.sweeper import Parameter, Sweeper
 
-from qibocal.auto.operation import Qubits, Routine
+from qibocal.auto.operation import Qubits, Results, Routine
 
-from ..two_qubit_interaction.chevron import ChevronData, ChevronParameters, _fit, _plot
+from ..two_qubit_interaction.chevron import ChevronData, ChevronParameters, _plot
 from ..two_qubit_interaction.utils import order_pair
 
 
+@dataclass
+class ChevronCouplersParameters(ChevronParameters):
+    """ChevronCouplers protocol parameters.
+
+    Amplitude and duration are referred to the coupler pulse.
+    """
+
+
+@dataclass
+class ChevronCouplersData(ChevronData):
+    """Data structure for chevron couplers protocol."""
+
+
 def _aquisition(
-    params: ChevronParameters,
+    params: ChevronCouplersParameters,
     platform: Platform,
     qubits: Qubits,
 ) -> ChevronData:
     r"""
-    Routine to find the optimal flux pulse amplitude and duration for a CZ/iSWAP gate.
+    Routine to find the optimal coupler flux pulse amplitude and duration for a CZ/iSWAP gate.
 
     The qubits must be at specific frequencies such that the high frequency qubit
     1 to 2 (CZ) / 0 to 1 (iSWAP) transition is at the same frequency as the low frequency qubit 0 to 1 transition.
@@ -45,7 +60,6 @@ def _aquisition(
 
     # create a DataUnits object to store the results,
     data = ChevronData()
-
     # sort high and low frequency qubit
     for pair in qubits:
         sequence = PulseSequence()
@@ -61,27 +75,18 @@ def _aquisition(
 
         sequence.add(initialize_highfreq)
 
-        # TODO: Is this the best way to assume you have a 2q gate on the runcard
-        # instead of using platform.create_flux_pulse and platform.create_coupler_pulse ?
         if params.native_gate == "CZ":
-            native_gate, virtual_z_phase = platform.create_CZ_pulse_sequence(
+            native_gate, _ = platform.create_CZ_pulse_sequence(
                 (ordered_pair[1], ordered_pair[0]),
-                start=0,
+                start=sequence.finish + params.dt,
             )
         elif params.native_gate == "iSWAP":
-            native_gate, virtual_z_phase = platform.create_iSWAP_pulse_sequence(
+            native_gate, _ = platform.create_iSWAP_pulse_sequence(
                 (ordered_pair[1], ordered_pair[0]),
-                start=0,
+                start=sequence.finish + params.dt,
             )
 
-        fq_pulse = native_gate[1]
-        fx_pulse = native_gate[0]
-
-        fq_pulse.start = sequence.finish + params.dt
-        fx_pulse.start = sequence.finish + params.dt
-
-        sequence += fq_pulse
-        sequence += fx_pulse
+        sequence.add(native_gate)
 
         ro_pulse1 = platform.create_MZ_pulse(
             ordered_pair[1], start=sequence.finish + params.dt
@@ -95,12 +100,12 @@ def _aquisition(
         sweeper_amplitude = Sweeper(
             Parameter.amplitude,
             delta_amplitude_range,
-            pulses=[fx_pulse],
+            pulses=[native_gate.coupler_pulses(*pair)],
         )
         sweeper_duration = Sweeper(
             Parameter.duration,
             delta_duration_range,
-            pulses=[fx_pulse],
+            pulses=[native_gate.coupler_pulses(*pair)],
         )
 
         # repeat the experiment as many times as defined by nshots
@@ -119,7 +124,6 @@ def _aquisition(
         data.register_qubit(
             ordered_pair[0],
             ordered_pair[1],
-            # coupler, # Do we need the coupler here to extract some info ?
             delta_duration_range,
             delta_amplitude_range,
             results[ordered_pair[0]].magnitude,
@@ -129,5 +133,19 @@ def _aquisition(
     return data
 
 
-coupler_chevron = Routine(_aquisition, _fit, _plot, two_qubit_gates=True)
+@dataclass
+class ChevronCouplersResults(Results):
+    """Empty fitting outputs for chevron couplers is not implemented in this case."""
+
+
+def _fit(data: ChevronCouplersData) -> ChevronCouplersResults:
+    """ "Results for ChevronCouplers."""
+    return ChevronCouplersResults()
+
+
+def plot(data: ChevronCouplersData, fit: ChevronCouplersResults, qubit):
+    return _plot(data, None, qubit)
+
+
+coupler_chevron = Routine(_aquisition, _fit, plot, two_qubit_gates=True)
 """Coupler cz/swap flux routine."""
