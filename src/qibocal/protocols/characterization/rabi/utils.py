fix: fix rabi correction formula

src/qibocal/protocols/characterization/rabi/utils.py
@@ -213,4 +213,4 @@ def period_correction_factor(phase: float):
 
     """
 
-    return 1 - np.modf(phase / np.pi)[0]
+    return np.round(1 + phase / np.pi) - phase / np.pi