7f78f5bed7b44d8636855cb67116d9e2e87d67e0
Andrea <andreapasquale97@gmail.com>
2024-01-11 17:05:24 +0400

    style: Remove dead code

--- src/qibocal/auto/validators/chi2.py
+++ src/qibocal/auto/validators/chi2.py
@@ -45,26 +45,3 @@ class Chi2Validator:
         except AttributeError:
             log.error(f"Chi2 validation not available for {type(results)}")
             return None
-
-
-# def check_chi2(
-#     results: Results,
-#     target: Union[QubitId, QubitPairId, list[QubitId]],
-#     thresholds: Optional[list] = None,
-# ) -> Optional[float]:
-#     """Performs validation of results using chi2.
-
-#     Find the threshold of the chi2 among thresholds.
-#     """
-
-#     if thresholds is None:
-#         thresholds = [CHI2_MAX]
-
-#     try:
-#         chi2 = results.chi2[target][0]
-#         idx = np.searchsorted(thresholds, chi2)
-#         return idx - 1
-
-#     except AttributeError:
-#         log.error(f"Chi2 validation not available for {type(results)}")
-#         return None
