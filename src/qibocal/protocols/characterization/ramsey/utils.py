0b565ff417b263bdd582f419f6e3465eeb13c5f1
Andrea <andreapasquale97@gmail.com>
2024-02-19 22:24:42 +0100

    fix: detune also first RX90 pulse

--- src/qibocal/protocols/characterization/ramsey/utils.py
+++ src/qibocal/protocols/characterization/ramsey/utils.py
@@ -12,12 +12,14 @@ def ramsey_sequence(
     wait: Optional[int] = 0,
     detuning: Optional[int] = 0,
 ):
+    """Pulse sequence used in Ramsey experiments."""
 
     sequence = PulseSequence()
     first_pi_half_pulse = platform.create_RX90_pulse(qubit, start=0)
     second_pi_half_pulse = platform.create_RX90_pulse(
         qubit, start=first_pi_half_pulse.finish + wait
     )
+    first_pi_half_pulse.frequency += detuning
     second_pi_half_pulse.frequency += detuning
     readout_pulse = platform.create_qubit_readout_pulse(
         qubit, start=second_pi_half_pulse.finish
@@ -27,11 +29,6 @@ def ramsey_sequence(
     return sequence
 
 
-def ramsey_fit(x, p0, p1, p2, p3, p4):
-    # A fit to Superconducting Qubit Rabi Oscillation
-    #   Offset                       : p[0]
-    #   Oscillation amplitude        : p[1]
-    #   DeltaFreq                    : p[2]
-    #   Phase                        : p[3]
-    #   Arbitrary parameter T_2      : 1/p[4]
-    return p0 + p1 * np.sin(x * p2 + p3) * np.exp(-x * p4)
+def ramsey_fit(x, offset, amplitude, delta, phase, decay):
+    """ "Dumped sinusoidal fit."""
+    return offset + amplitude * np.sin(x * delta + phase) * np.exp(-x * decay)
