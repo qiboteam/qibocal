e1c993bbfaed88db7df15f665704e61188e03b84
Andrea <andreapasquale97@gmail.com>
2024-02-23 09:47:36 +0100

    refactor: Add docstrings and minor fixes in tests

--- src/qibocal/auto/runcard.py
+++ src/qibocal/auto/runcard.py
@@ -57,7 +57,9 @@ class Runcard:
     actions: list[Action]
     """List of action to be executed."""
     targets: Optional[Targets] = None
-    """Qubits to be calibrated."""
+    """Qubits to be calibrated.
+       If `None` the protocols will be executed on all qubits
+       available in the platform."""
     backend: str = "qibolab"
     """Qibo backend."""
     platform: str = os.environ.get("QIBO_PLATFORM", "dummy")
