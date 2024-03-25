59bcf165f65bc61c5ac316dc633998123b4b9359
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2024-03-25 17:23:43 +0400

    fix: targets are now a list

--- src/qibocal/protocols/characterization/flux_dependence/qubit_flux_dependence.py
+++ src/qibocal/protocols/characterization/flux_dependence/qubit_flux_dependence.py
@@ -143,7 +143,7 @@ def _acquisition(
             Sweeper(
                 Parameter.bias,
                 delta_bias_flux_range,
-                qubits=list(targets.values()),
+                qubits=targets,
                 type=SweeperType.OFFSET,
             )
         ]
