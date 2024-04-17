741c78a5a675c7ddf35b34210608bf3a158d8aac
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2024-04-17 17:20:02 +0400

    refactor: abstract rb acquisition function

--- src/qibocal/protocols/characterization/randomized_benchmarking/standard_rb.py
+++ src/qibocal/protocols/characterization/randomized_benchmarking/standard_rb.py
@@ -4,17 +4,14 @@ from typing import Iterable, Optional, TypedDict, Union
 import numpy as np
 import numpy.typing as npt
 import plotly.graph_objects as go
-from qibo.backends import GlobalBackend
 from qibolab.platform import Platform
 from qibolab.qubits import QubitId
 
 from qibocal.auto.operation import Data, Parameters, Results, Routine
-from qibocal.config import raise_error
-from qibocal.protocols.characterization.randomized_benchmarking import noisemodels
 
 from ..utils import table_dict, table_html
 from .fitting import exp1B_func, fit_exp1B_func
-from .utils import RB_Generator, data_uncertainties, number_to_str, random_circuits
+from .utils import data_uncertainties, number_to_str, rb_acquisition
 
 NPULSES_PER_CLIFFORD = 1.875
 
@@ -129,73 +126,13 @@ def _acquisition(
     Args:
         params (StandardRBParameters): All parameters in one object.
         platform (Platform): Platform the experiment is executed on.
-        qubits (dict[int, Union[str, int]] or list[Union[str, int]]): list of qubits the experiment is executed on.
+        target (dict[int, Union[str, int]] or list[Union[str, int]]): list of qubits the experiment is executed on.
 
     Returns:
         RBData: The depths, samples and ground state probability of each experiment in the scan.
     """
 
-    backend = GlobalBackend()
-    # For simulations, a noise model can be added.
-    noise_model = None
-    if params.noise_model is not None:
-        if backend.name == "qibolab":
-            raise_error(
-                ValueError,
-                "Backend qibolab (%s) does not perform noise models simulation. ",
-            )
-
-        noise_model = getattr(noisemodels, params.noise_model)(params.noise_params)
-        params.noise_params = noise_model.params.tolist()
-    # 1. Set up the scan (here an iterator of circuits of random clifford gates with an inverse).
-    nqubits = len(targets)
-    data = RBData(
-        depths=params.depths,
-        uncertainties=params.uncertainties,
-        seed=params.seed,
-        nshots=params.nshots,
-        niter=params.niter,
-    )
-
-    circuits = []
-    indexes = {}
-    samples = []
-    qubits_ids = targets
-    rb_gen = RB_Generator(params.seed)
-    for depth in params.depths:
-        # TODO: This does not generate multi qubit circuits
-        circuits_depth, random_indexes = random_circuits(
-            depth, qubits_ids, params.niter, rb_gen, noise_model
-        )
-        circuits.extend(circuits_depth)
-        for qubit in random_indexes.keys():
-            indexes[(qubit, depth)] = random_indexes[qubit]
-    # Execute the circuits
-    if params.unrolling:
-        executed_circuits = backend.execute_circuits(circuits, nshots=params.nshots)
-    else:
-        executed_circuits = [
-            backend.execute_circuit(circuit, nshots=params.nshots)
-            for circuit in circuits
-        ]
-
-    for circ in executed_circuits:
-        samples.extend(circ.samples())
-    samples = np.reshape(samples, (-1, nqubits, params.nshots))
-
-    for i, depth in enumerate(params.depths):
-        index = (i * params.niter, (i + 1) * params.niter)
-        for nqubit, qubit_id in enumerate(targets):
-            data.register_qubit(
-                RBType,
-                (qubit_id, depth),
-                dict(
-                    samples=samples[index[0] : index[1]][:, nqubit],
-                ),
-            )
-    data.circuits = indexes
-
-    return data
+    return rb_acquisition(params, targets)
 
 
 def _fit(data: RBData) -> StandardRBResult:
