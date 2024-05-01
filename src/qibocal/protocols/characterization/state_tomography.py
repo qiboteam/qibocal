62105d25855087caa84cffe25d4fc6241007d089
andrea-pasquale <andreapasquale97@gmail.com>
2024-05-01 09:08:46 +0400

    feat: First prototype working

--- src/qibocal/protocols/characterization/state_tomography.py
+++ src/qibocal/protocols/characterization/state_tomography.py
@@ -1,7 +1,10 @@
 from copy import deepcopy
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
+import numpy as np
+import numpy.typing as npt
+import plotly.graph_objects as go
 from qibo import Circuit, gates
 from qibo.backends import NumpyBackend
 from qibolab.platform import Platform
@@ -18,9 +21,22 @@ class StateTomographyParameters(Parameters):
     """Circuit to prepare initial state"""
 
 
+TomographyType = np.dtype(
+    [
+        ("sim", np.int64),
+        ("hardware", np.int64),
+    ]
+)
+"""Custom dtype for resonator spectroscopy."""
+
+
 @dataclass
 class StateTomographyData(Data):
     """Tomography data"""
+
+    data: dict[tuple[str, QubitId], npt.NDArray[TomographyType]] = field(
+        default_factory=dict
+    )
 
 
 @dataclass
@@ -37,19 +53,32 @@ def _acquisition(
     hardware = {}
     if params.circuit is None:
         params.circuit = Circuit(
-            len(targets), wire_names=[str(i) for i in range(len(targets))]
+            platform.nqubits, wire_names=[str(i) for i in range(platform.nqubits)]
         )
-        params.circuit.add(gates.X(i) for i in targets)
+        # params.circuit.add(gates.M(i) for i in targets)
+
+    data = StateTomographyData()
 
     for basis in ["X", "Y", "Z"]:
         basis_circuit = deepcopy(params.circuit)
-        basis_circuit.add(gates.M(i, basis=getattr(gates, basis)) for i in targets)
+        if basis != "Z":
+            basis_circuit.add(getattr(gates, basis)(0).basis_rotation())
 
-        simulation[basis] = NumpyBackend().execute_circuit(basis_circuit).samples()
-        hardware[basis] = basis_circuit(nshots=params.nshots).samples()
+        # basis_circuit.add(gates.M(i, basis=getattr(gates, basis)) for i in targets)
 
-    print(simulation)
-    return StateTomographyData()
+        basis_circuit.add(gates.M(0))
+        data.register_qubit(
+            TomographyType,
+            (basis, 0),
+            dict(
+                sim=NumpyBackend()
+                .execute_circuit(basis_circuit, nshots=params.nshots)
+                .samples(),
+                hardware=basis_circuit(nshots=params.nshots).samples(),
+            ),
+        )
+
+    return data
 
 
 def _fit(data: StateTomographyData) -> StateTomographyResults:
@@ -60,7 +89,13 @@ def _fit(data: StateTomographyData) -> StateTomographyResults:
 
 def _plot(data: StateTomographyData, fit: StateTomographyResults, target: QubitId):
     """Plotting for state tomography"""
-    return [], ""
+    fig = go.Figure()
+    simulation = [2 * np.mean(data[basis, 0].sim) - 1 for basis in ["X", "Y", "Z"]]
+    hardware = [2 * np.mean(data[basis, 0].hardware) - 1 for basis in ["X", "Y", "Z"]]
+    fig.add_trace(go.Bar(x=["X", "Y", "Z"], y=simulation, name="Simulation"))
+    fig.add_trace(go.Bar(x=["X", "Y", "Z"], y=hardware, name="Hardware"))
+    fig.update_layout(barmode="group")
+    return [fig], ""
 
 
 state_tomography = Routine(_acquisition, _fit, _plot)
