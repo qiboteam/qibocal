a87f7b540b3d73eddf561329966aaa9fe35f02ff
Jacfomg <juan.a.c.f.99@gmail.com>
2024-05-22 16:16:22 +0400

    fix: forgot function

--- src/qibocal/protocols/randomized_benchmarking/utils.py
+++ src/qibocal/protocols/randomized_benchmarking/utils.py
@@ -832,3 +832,36 @@ def fit(qubits, data):
         popts[qubit] = popt
 
     return StandardRBResult(fidelity, pulse_fidelity, popts, perrs, error_barss)
+
+
+def layer_2q_interleaved_circuit(
+    rb_gen: Callable, depth: int, qubits, interleaved: str
+) -> tuple[Circuit, dict]:
+    """Creates a circuit of `depth` layers from a generator `layer_gen` yielding `Circuit` or `Gate`
+    and a dictionary with random indexes used to select the clifford gates.
+    Args:
+        layer_gen (Callable): Should return gates or a full circuit specifying a layer.
+        depth (int): Number of layers.
+    Returns:
+        Circuit: with `depth` many layers.
+    """
+
+    full_circuit = None
+    random_indexes = []
+    # Build each layer, there will be depth many in the final circuit.
+    for _ in range(depth):
+        # Generate a layer.
+        new_layer, random_index = rb_gen.layer_gen()
+        new_circuit = Circuit(2)
+        for gate in new_layer:
+            new_circuit.add(gate)
+        random_indexes.append(random_index)
+
+        if interleaved == "CZ":
+            new_circuit.add(gates.CZ(0, 1))
+            random_indexes.append("13")  # FIXME: Check the index
+
+        if full_circuit is None:  # instantiate in first loop
+            full_circuit = Circuit(new_circuit.nqubits)
+        full_circuit = full_circuit + new_circuit
+    return full_circuit, random_indexes
