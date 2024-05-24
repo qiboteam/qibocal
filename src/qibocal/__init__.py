108f2871df50cc60ac9d3ae84da9822e814a5b35
Alessandro Candido <candido.ale@gmail.com>
2024-05-24 13:15:19 +0200

    docs: Document the default executor definition
    I'm not even sure it is that useful to have it there, but in case it will remain, the scope should be clear, at least

--- src/qibocal/__init__.py
+++ src/qibocal/__init__.py
@@ -9,4 +9,13 @@ from .cli import command
 __version__ = im.version(__package__)
 __all__ = ["Executor", "protocols", "command"]
 
-Executor.create("qibocal.routines")
+DEFAULT_EXECUTOR = Executor.create("qibocal.routines")
+"""Default executor, registered as a qibocal submodule.
+
+It is defined for streamlined usage of qibocal protocols in simple
+contexts, where no additional options has to be defined for the
+executor.
+
+This is not meant to be used directly, thus is not meant to be publicly
+exposed.
+"""
