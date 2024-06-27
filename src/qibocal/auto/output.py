a084b63b9cca842c15e4f1bfe04b7e58261dbcd3
Alessandro Candido <candido.ale@gmail.com>
2024-06-27 16:58:16 +0200

    feat: Migrate utils functions

--- src/qibocal/auto/output.py
+++ src/qibocal/auto/output.py
@@ -1,8 +1,12 @@
+import getpass
 import json
+import shutil
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import Optional
+
+from qibocal.config import log
 
 from .history import History
 
@@ -11,9 +15,6 @@ from .history import History
 class Versions:
     qibocal: str
     other: dict
-
-
-META_FILE = "meta.json"
 
 
 @dataclass
@@ -47,6 +48,34 @@ class Metadata:
             versions=versions,
         )
 
+    def end(self):
+        """Register completion time."""
+        self.end_time = datetime.now(timezone.utc)
+
+
+def _new_output() -> Path:
+    user = getpass.getuser().replace(".", "-")
+    date = datetime.now().strftime("%Y-%m-%d")
+
+    num = 0
+    while True:
+        path = Path.cwd() / f"{date}-{str(num).rjust(3, '0')}-{user}"
+        log.info(f"Trying to create directory {path}")
+
+        if not path.exists():
+            break
+
+        log.info(f"Directory {path} already exists.")
+        num += 1
+
+    return path
+
+
+RUNCARD = "runcard.yml"
+UPDATED_PLATFORM = "new_platform"
+PLATFORM = "platform"
+META = "meta.json"
+
 
 @dataclass
 class Output:
@@ -57,5 +86,17 @@ class Output:
     def load(cls, path: Path):
         return cls(
             history=History.load(path),
-            meta=Metadata(**json.loads((path / META_FILE).read_text())),
+            meta=Metadata(**json.loads((path / META).read_text())),
         )
+
+    def dump(self, path: Optional[Path] = None, force: bool = False):
+        if path is None:
+            path = _new_output()
+        elif path.exists() and not force:
+            raise RuntimeError(f"Directory {path} already exists.")
+        elif path.exists() and force:
+            log.warning(f"Deleting previous directory {path}.")
+            shutil.rmtree(path)
+
+        log.info(f"Creating directory {path}.")
+        path.mkdir(parents=True)
