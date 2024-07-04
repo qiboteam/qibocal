d8f4a7cdda8bc07b04cf7d4d78848b741b20ff2e
Andrea <andreapasquale97@gmail.com>
2024-07-04 19:47:19 +0400

    test: Test period > 1

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -4,11 +4,14 @@ import pytest
 from qibocal.protocols.utils import guess_period
 
 
-@pytest.mark.parametrize("period", [0.2, 0.5, 1])
+@pytest.mark.parametrize("period", [0.2, 0.5, 1, 2])
 def test_guess_period(period):
     """Testing guess period function with different periods."""
     t = np.linspace(0, 1, 100, endpoint=False)
     signal = np.sin(2 * np.pi / period * t)
 
     extracted_period = guess_period(t, signal)
-    np.testing.assert_allclose(period, extracted_period)
+    if period < 1:
+        np.testing.assert_allclose(period, extracted_period)
+    else:
+        extracted_period == 1
