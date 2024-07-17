3d7c3f0c952e741526c744e4be131bb7370a6068
Alessandro Candido <candido.ale@gmail.com>
2024-07-17 18:43:55 +0200

    feat: Reduce init and close parameters to the minimal amount

--- runcards/single_shot.py
+++ runcards/single_shot.py
@@ -1,16 +1,13 @@
-from pathlib import Path
-
 from qibocal.auto.execute import Executor
 from qibocal.cli.report import report
 
 executor = Executor.create(name="myexec", platform="dummy")
-path = Path("test_x")
 
 from myexec import close, init, single_shot_classification
 
-output, meta = init(path, force=True)
+init("test_x", force=True)
 
 completed = single_shot_classification(nshots=1000)
 
-close(path, output, meta)
-report(path, executor.history)
+close()
+report(executor.path, executor.history)
