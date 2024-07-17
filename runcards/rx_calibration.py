468d96142d309377185a5c3e9532304206f4f05c
Alessandro Candido <candido.ale@gmail.com>
2024-07-17 19:10:28 +0200

    test: Remove boilerplate from rx calibration as well

--- runcards/rx_calibration.py
+++ runcards/rx_calibration.py
@@ -1,41 +1,13 @@
-from pathlib import Path
-
-from qibo.backends import construct_backend
-
 from qibocal.auto.execute import Executor
-from qibocal.auto.history import History
-from qibocal.auto.output import Metadata, Output
 from qibocal.cli.report import report
 
+executor = Executor.create(name="myexec", platform="dummy")
+
+from myexec import init, close, drag_tuning, rabi_amplitude, ramsey
+
 target = 0
-folder = Path("test_rx_calibration")
-force = True
-
-backend = construct_backend(backend="qibolab", platform="dummy")
-platform = backend.platform
-if platform is None:
-    raise ValueError("Qibocal requires a Qibolab platform to run.")
-
-executor = Executor(
-    name="myexec", history=History(), platform=platform, targets=[target]
-)
-
-# generate output folder
-path = Output.mkdir(folder, force)
-
-# generate meta
-meta = Metadata.generate(path.name, backend)
-output = Output(History(), meta, platform)
-output.dump(path)
-
-from myexec import drag_tuning, rabi_amplitude, ramsey
-
-# connect and initialize platform
-platform.connect()
-
-# run
-meta.start()
-
+platform = executor.platform
+init("test_rx_calibration", force=True, targets=[target])
 
 rabi_output = rabi_amplitude(
     min_amp_factor=0.5,
@@ -44,7 +16,6 @@ rabi_output = rabi_amplitude(
     pulse_length=platform.qubits[target].native_gates.RX.duration,
     nshots=2048,
 )
-
 # update only if chi2 is satisfied
 if rabi_output.results.chi2[target][0] > 2:
     raise RuntimeError(
@@ -59,12 +30,10 @@ ramsey_output = ramsey(
     detuning=1_000_000,
     nshots=2048,
 )
-
 if ramsey_output.results.chi2[target][0] > 2:
     raise RuntimeError(
         f"Ramsey fit has chi2 {ramsey_output.results.chi2[target][0]} greater than 2. Stopping."
     )
-
 if ramsey_output.results.delta_phys[target][0] < 1e4:
     print(
         f"Ramsey frequency not updated, correctio to small { ramsey_output.results.delta_phys[target][0]}"
@@ -79,8 +48,6 @@ rabi_output_2 = rabi_amplitude(
     pulse_length=platform.qubits[target].native_gates.RX.duration,
     nshots=2048,
 )
-
-
 # update only if chi2 is satisfied
 if rabi_output_2.results.chi2[target][0] > 2:
     raise RuntimeError(
@@ -89,7 +56,6 @@ if rabi_output_2.results.chi2[target][0] > 2:
 rabi_output_2.update_platform(platform)
 
 drag_output = drag_tuning(beta_start=-4, beta_end=4, beta_step=0.5)
-
 if drag_output.results.chi2[target][0] > 2:
     raise RuntimeError(
         f"Drag fit has chi2 {drag_output.results.chi2[target][0]} greater than 2. Stopping."
@@ -103,8 +69,6 @@ rabi_output_3 = rabi_amplitude(
     pulse_length=platform.qubits[target].native_gates.RX.duration,
     nshots=2048,
 )
-
-
 # update only if chi2 is satisfied
 if rabi_output_3.results.chi2[target][0] > 2:
     raise RuntimeError(
@@ -112,14 +76,5 @@ if rabi_output_3.results.chi2[target][0] > 2:
     )
 rabi_output_3.update_platform(platform)
 
-meta.end()
-
-# stop and disconnect platform
-platform.disconnect()
-
-history = executor.history
-# dump history, metadata, and updated platform
-output.history = history
-output.dump(path)
-
-report(path, history)
+close()
+report(executor.path, executor.history)
