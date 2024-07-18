9ef5ffec4d423e6b98a1b00bb8685b408e763f77
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2024-07-18 23:53:55 +0200

    refactor: Update qq compare with latest modifications from main

--- src/qibocal/cli/compare.py
+++ src/qibocal/cli/compare.py
@@ -1,19 +1,27 @@
-import datetime
-import json
+import io
+import pathlib
 from pathlib import Path
-from typing import List
+from typing import Union
 
 import pandas as pd
 import plotly.graph_objects as go
-import yaml
+from jinja2 import Environment, FileSystemLoader
 from plotly.subplots import make_subplots
-from qibolab.qubits import QubitId
+from qibolab.qubits import QubitId, QubitPairId
 
-from qibocal.auto.execute import Executor
-from qibocal.auto.runcard import Runcard
-from qibocal.auto.task import TaskId
-from qibocal.cli.report import META, RUNCARD, ReportBuilder, generate_figures_and_report
-from qibocal.cli.utils import generate_output_folder
+from qibocal.auto.output import Output
+from qibocal.auto.task import Completed, TaskId
+from qibocal.cli.report import generate_figures_and_report
+from qibocal.web.report import STYLES, TEMPLATES
+
+
+class FakeHistory:
+
+    def flush(self, output=None):
+        pass
+
+    def items(self):
+        return tuple()
 
 
 def compare_reports(folder: Path, path_1: Path, path_2: Path, force: bool):
@@ -30,94 +38,82 @@ def compare_reports(folder: Path, path_1: Path, path_2: Path, force: bool):
         force (bool): if set to true, overwrites folder (if it already exists).
 
     """
-    combined_meta = json.loads((path_1 / META).read_text())
-    e = datetime.datetime.now(datetime.timezone.utc)
-    combined_meta["start-time"] = e.strftime("%H:%M:%S")
-    combined_meta["date"] = e.strftime("%Y-%m-%d")
-    combined_report_path = generate_output_folder(folder, force)
-    combined_meta["title"] = combined_report_path.name
+    combined_meta = Output.load(path_1).meta
+    combined_meta.start()
     paths = [path_1, path_2]
-    builders = []
-    for path in paths:
-        # load meta
-        meta = json.loads((path / META).read_text())
-        # load runcard
-        runcard = Runcard.load(yaml.safe_load((path / RUNCARD).read_text()))
 
-        # load executor
-        executor = Executor.load(runcard, path, targets=runcard.targets)
-        # produce html
-        builder = ReportBuilder(path, runcard.targets, executor, meta)
-        builders.append(builder)
-    comparison_report = CompareReportBuilder(builders, folder)
-    comparison_report.run(combined_report_path)
-    e = datetime.datetime.now(datetime.timezone.utc)
-    combined_meta["end-time"] = e.strftime("%H:%M:%S")
-    (combined_report_path / META).write_text(json.dumps(combined_meta, indent=4))
+    css_styles = f"<style>\n{pathlib.Path(STYLES).read_text()}\n</style>"
+
+    env = Environment(loader=FileSystemLoader(TEMPLATES))
+    template = env.get_template("template.html")
+    combined_report = Output(history=FakeHistory(), meta=combined_meta)
+    combined_report_path = combined_report.mkdir(folder, force)
+    combined_meta.title = combined_report_path.name
+    combined_meta.end()
+    combined_report.meta = combined_meta
+
+    html = template.render(
+        is_static=True,
+        css_styles=css_styles,
+        path=folder,
+        title=combined_report_path.name,
+        report=ComparedReport(
+            report_paths=paths,
+            folder=folder,
+            meta=combined_meta.dump(),
+        ),
+    )
+    combined_report.dump(folder)
+    (folder / "index.html").write_text(html)
 
 
-class CompareReportBuilder:
+class ComparedReport:
 
-    def __init__(self, report_builders: List[ReportBuilder], path: Path):
-        """Combine two reports.
-
-        Args:
-            report_builders (List[ReportBuilder]): List containing the two ReportBuilder
-                objects to be combined.
-            path (Path): Path of the combined report.
-        """
-        self.report_builders = report_builders
-        self.metadata = self.combine_metadata(report_builders)
-        self.path = self.title = path
-        self.targets = report_builders[0].targets
-        self.executor = report_builders[0].executor
-
-    @staticmethod
-    def combine_metadata(report_builders):
-        metadata = report_builders[0].metadata
-
-        combined_keys = ["platform", "date", "start-time", "end-time"]
-        for key in combined_keys:
-            report_dates = [report.metadata[key] for report in report_builders]
-            metadata[key] = " | ".join(report_dates)
-        return metadata
+    def __init__(self, report_paths: list[Path], folder, meta):
+        self.report_paths = report_paths
+        self.folder = folder
+        self.history = self.create_common_history()
+        self.meta = meta
+        # self.plotter = plotter
 
     def history_uids(self):
         experiment_ids = []
-        for report_builder in self.report_builders:
-            experiment_ids.append({x for x in report_builder.history})
+        for path in self.report_paths:
+            report = Output.load(path)
+            experiment_ids.append({x for x in report.history})
 
         common_experiment_ids = experiment_ids[0]
         for exp_id in experiment_ids[1:]:
             common_experiment_ids = common_experiment_ids & exp_id
         return common_experiment_ids
 
-    @property
-    def history(self):
-        # find common history
+    def create_common_history(self):
         common_experiment_ids = self.history_uids()
         history = {}
         # create a dictionary of task_uid: List[Completed(...)]
         for common_experiment_id in common_experiment_ids:
             history[common_experiment_id] = []
-            for report_builder in self.report_builders:
+            for path in self.report_paths:
+                report = Output.load(path)
                 history[common_experiment_id].append(
-                    report_builder.history[common_experiment_id]
+                    report.history[common_experiment_id]
                 )
         return history
 
-    def routine_name(self, routine, iteration):  # can be defined outside the class
+    @staticmethod
+    def routine_name(routine: TaskId):
         """Prettify routine's name for report headers."""
-        name = routine.replace("_", " ").title()
-        return f"{name} - {iteration}"
+        return routine.id.title()
 
-    def routine_targets(self, task_id: TaskId):  # ????
-        """Get local targets parameter from Task if available otherwise use global one."""
-        # local_qubits = self.history[task_id].task.targets
-        # return local_qubits if len(local_qubits) > 0 else self.targets
-        return self.targets
+    def routine_targets(self, task_id: TaskId):
+        """Extract local targets parameter from Task.
 
-    def merge_plots(self, plots: List[List[go.Figure]]) -> str:
+        If not available use the global ones.
+        """
+        local_targets = self.history[task_id][0].task.targets
+        return local_targets if len(local_targets) > 0 else self.meta["targets"]
+
+    def merge_plots(self, plots: list[list[go.Figure]]) -> list[go.Figure]:
         """Merge plots from different reports.
 
         Scatter plots are plotted in the same figure.
@@ -127,7 +123,7 @@ class CompareReportBuilder:
             merged_plot_data = plots[0][0].data
             for plot in plots[1:]:
                 merged_plot_data = merged_plot_data + plot[0].data
-            fig = go.Figure(data=merged_plot_data, layout=plots[0][0].layout).to_html()
+            fig = [go.Figure(data=merged_plot_data, layout=plots[0][0].layout)]
         elif plots[0][0].data[0].type == "heatmap":
             fig = make_subplots(rows=2, cols=2)
             for i, plot in enumerate(plots):
@@ -148,47 +144,56 @@ class CompareReportBuilder:
                     col=1,
                 )
             fig.update_layout(legend=plots[0][0].layout.legend)
-            fig = fig.to_html()
+            fig = [fig]
 
         return fig
 
-    def single_qubit_plot(self, task_id: TaskId, qubit: QubitId):
-        """Generate single qubit plot."""
+    def plotter(
+        self, nodes: list[Completed], target: Union[QubitId, QubitPairId, list[QubitId]]
+    ) -> tuple[str, str]:
         tables = []
         plots = []
-        for report_builder in self.report_builders:
-            _, table = report_builder.single_qubit_plot(task_id, qubit)
-            node = report_builder.history[task_id]
-            figures, _ = generate_figures_and_report(node, qubit)
-            tables.append(table)
-            plots.append(figures)
+        for node in nodes:
+            report_figures, fitting_report = generate_figures_and_report(node, target)
+            tables.append(fitting_report)
+            plots.append(report_figures)
+
+        figures = self.merge_plots(plots)
+        buffer = io.StringIO()
+        html_list = []
+        for figure in figures:
+            figure.write_html(buffer, include_plotlyjs=False, full_html=False)
+            buffer.seek(0)
+            html_list.append(buffer.read())
+        buffer.close()
+        all_html = "".join(html_list)
 
         try:
+            merged_table = None
             merge_columns = {"Qubit", "Parameters"}
-            merged_table = pd.read_html(tables[0])[0]
-            merged_table = merged_table.rename(
-                columns={
-                    col: f"{col}\n{self.report_builders[0].title}" for col in set(merged_table.columns) - merge_columns
-                }
-            )
-            for i, table in enumerate(tables[1:]):
+            for i, table in enumerate(tables):
                 a = pd.read_html(table)[0]
-                merged_table = pd.merge(merged_table, a, on=list(merge_columns)).rename(
+                a = a.rename(
                     columns={
-                       col: f"{col}\n{self.report_builders[1].title}" for col in set(a.columns) - merge_columns
+                        col: f"{col}_{i}" for col in set(a.columns) - merge_columns
                     }
                 )
+                if merged_table is None:
+                    merged_table = a
+                else:
+                    merged_table = pd.merge(merged_table, a, on=list(merge_columns))
         except ValueError:
             merged_table = pd.DataFrame(
                 [], columns=["An error occurred when performing the fit."]
             )
-        merged_plot = self.merge_plots(plots)
-        return merged_plot, merged_table.to_html(
+        fitting_report = merged_table.to_html(
             classes="fitting-table", index=False, border=0, escape=False
         )
+        return all_html, fitting_report
 
-    def run(self, path):
-        """Generation of html report."""
-        from qibocal.web.report import create_report
 
-        create_report(path, self)
+if __name__ == "__main__":
+    path_1 = Path.home() / "qqrun1"
+    path_2 = Path.home() / "qqrun2"
+    folder = Path.home() / "qqcomparison"
+    compare_reports(folder, path_1, path_2, force=True)
