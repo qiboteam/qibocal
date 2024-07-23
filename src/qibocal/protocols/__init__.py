381959002d6d9bcd0c8019549377757e28f85663
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-07-24 01:14:38 +0400

    acquisition working

--- src/qibocal/protocols/__init__.py
+++ src/qibocal/protocols/__init__.py
@@ -39,6 +39,7 @@ from .flux_dependence.qubit_crosstalk import qubit_crosstalk
 from .flux_dependence.qubit_flux_dependence import qubit_flux
 from .flux_dependence.resonator_flux_dependence import resonator_flux
 from .flux_gate import flux_gate
+from .qua import rb_ondevice
 from .qubit_power_spectroscopy import qubit_power_spectroscopy
 from .qubit_spectroscopy import qubit_spectroscopy
 from .qubit_spectroscopy_ef import qubit_spectroscopy_ef
@@ -167,5 +168,6 @@ __all__ = [
     "cpmg",
     "drag_simple",
     "rb_qiskit",
+    "rb_ondevice",
 ]
 >>>>>>> 18904cd3 (feat: implement 2q RB using Qiskit circuits)
