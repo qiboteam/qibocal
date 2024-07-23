3963e5ca7bd08e495da2c50315237c2cc0b0d0e6
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-07-24 01:14:38 +0400

    acquisition working

--- src/qibocal/protocols/qua/rb_ondevice.py
+++ src/qibocal/protocols/qua/rb_ondevice.py
@@ -1,3 +1,4 @@
+import json
 import time
 from dataclasses import dataclass, field
 from typing import Optional
@@ -47,6 +48,10 @@ class RbOnDeviceParameters(Parameters):
     "Flag to enable state discrimination if the readout has been calibrated (rotated blobs and threshold)"
     debug: bool = False
     "If enabled it dumps the qua script."
+
+    def __post_init__(self):
+        if self.seed is None:
+            self.seed = np.random.randint(0, int(1e6))
 
     def __post_init__(self):
         if self.seed is None:
@@ -391,6 +396,9 @@ def _acquisition(
     job = qm.execute(
         rb, compiler_options=CompilerOptionArguments(flags=["not-strict-timing"])
     )
+
+    with open("qua_script.py", "w") as file:
+        file.write(generate_qua_script(rb, config))
 
     # Get results from QUA program
     if state_discrimination:
