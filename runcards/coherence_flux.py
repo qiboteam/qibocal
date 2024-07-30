0f733aa20052cc5cd047a0bd1ea5a6b9329f8883
Jacfomg <juan.a.c.f.99@gmail.com>
2024-07-30 18:55:27 +0400

    fix : comments

--- runcards/coherence_flux.py
+++ runcards/coherence_flux.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
@@ -9,7 +10,7 @@ from qibolab.qubits import QubitId
 
 from qibocal.auto.execute import Executor
 from qibocal.auto.history import History
-from qibocal.auto.operation import Data, Parameters
+from qibocal.auto.operation import Data
 from qibocal.cli.report import report
 from qibocal.protocols.flux_dependence.utils import transmon_frequency
 
@@ -25,75 +26,70 @@ CoherenceFluxType = np.dtype(
 """Custom dtype for CoherenceFlux routines."""
 
 
-# TODO: Make this parameters a dict of 4 parameters classes for each routine ???
-@dataclass
-class CoherenceFluxSignalParameters(Parameters):
-    """Coherence runcard inputs."""
+biases = np.arange(-0.2, 0.1, 0.01)
+"bias points to sweep"
 
-    biases = np.arange(-0.2, 0.1, 0.01)
-    "bias points to sweep"
+# Qubit spectroscopy
+freq_width = 50_000_000
+"""Width [Hz] for frequency sweep relative  to the qubit frequency."""
+freq_step = 1_000_000
+"""Frequency [Hz] step for sweep."""
+drive_duration = 1000
+"""Drive pulse duration [ns]. Same for all qubits."""
 
-    # Qubit spectroscopy
-    freq_width = 50_000_000
-    """Width [Hz] for frequency sweep relative  to the qubit frequency."""
-    freq_step = 1_000_000
-    """Frequency [Hz] step for sweep."""
-    drive_duration = 1000
-    """Drive pulse duration [ns]. Same for all qubits."""
+# Rabi amp signal
+min_amp_factor = 0.0
+"""Minimum amplitude multiplicative factor."""
+max_amp_factor = 0.5
+"""Maximum amplitude multiplicative factor."""
+step_amp_factor = 0.01
+"""Step amplitude multiplicative factor."""
 
-    # Rabi amp signal
-    min_amp_factor = 0.0
-    """Minimum amplitude multiplicative factor."""
-    max_amp_factor = 0.5
-    """Maximum amplitude multiplicative factor."""
-    step_amp_factor = 0.01
-    """Step amplitude multiplicative factor."""
+# Flipping
+nflips_max = 200
+"""Maximum number of flips ([RX(pi) - RX(pi)] sequences). """
+nflips_step = 10
+"""Flip step."""
 
-    # Flipping
-    nflips_max = 200
-    """Maximum number of flips ([RX(pi) - RX(pi)] sequences). """
-    nflips_step = 10
-    """Flip step."""
+# T1 signal
+delay_before_readout_start = 16
+"""Initial delay before readout [ns]."""
+delay_before_readout_end = 100_000
+"""Final delay before readout [ns]."""
+delay_before_readout_step = 4_000
+"""Step delay before readout [ns]."""
 
-    # T1 signal
-    delay_before_readout_start = 16
-    """Initial delay before readout [ns]."""
-    delay_before_readout_end = 100_000
-    """Final delay before readout [ns]."""
-    delay_before_readout_step = 4_000
-    """Step delay before readout [ns]."""
+#  Ramsey signal
+detuning = 3_000_000
+"""Frequency detuning [Hz]."""
+delay_between_pulses_start = 16
+"""Initial delay between RX(pi/2) pulses in ns."""
+delay_between_pulses_end = 5_000
+"""Final delay between RX(pi/2) pulses in ns."""
+delay_between_pulses_step = 200
+"""Step delay between RX(pi/2) pulses in ns."""
 
-    #  Ramsey signal
-    detuning = 3_000_000
-    """Frequency detuning [Hz]."""
-    delay_between_pulses_start = 16
-    """Initial delay between RX(pi/2) pulses in ns."""
-    delay_between_pulses_end = 5_000
-    """Final delay between RX(pi/2) pulses in ns."""
-    delay_between_pulses_step = 200
-    """Step delay between RX(pi/2) pulses in ns."""
+# T2 and Ramsey signal
+delay_between_pulses_start_T2 = 16
+"""Initial delay between RX(pi/2) pulses in ns."""
+delay_between_pulses_end_T2 = 25_000
+"""Final delay between RX(pi/2) pulses in ns."""
+delay_between_pulses_step_T2 = 500
+"""Step delay between RX(pi/2) pulses in ns."""
+single_shot_T2: bool = False
+"""If ``True`` save single shot signal data."""
 
-    # T2 and Ramsey signal
-    delay_between_pulses_start_T2 = 16
-    """Initial delay between RX(pi/2) pulses in ns."""
-    delay_between_pulses_end_T2 = 25_000
-    """Final delay between RX(pi/2) pulses in ns."""
-    delay_between_pulses_step_T2 = 500
-    """Step delay between RX(pi/2) pulses in ns."""
-    single_shot_T2: bool = False
-    """If ``True`` save single shot signal data."""
-
-    # Optional qubit spectroscopy
-    drive_amplitude: Optional[float] = None
-    """Drive pulse amplitude (optional). Same for all qubits."""
-    hardware_average: bool = True
-    """By default hardware average will be performed."""
-    # Optional rabi amp signal
-    pulse_length: Optional[float] = 40
-    """RX pulse duration [ns]."""
-    # Optional T1 signal
-    single_shot_T1: bool = False
-    """If ``True`` save single shot signal data."""
+# Optional qubit spectroscopy
+drive_amplitude: Optional[float] = None
+"""Drive pulse amplitude (optional). Same for all qubits."""
+hardware_average: bool = True
+"""By default hardware average will be performed."""
+# Optional rabi amp signal
+pulse_length: Optional[float] = 40
+"""RX pulse duration [ns]."""
+# Optional T1 signal
+single_shot_T1: bool = False
+"""If ``True`` save single shot signal data."""
 
 
 @dataclass
@@ -104,38 +100,46 @@ class CoherenceFluxSignalData(Data):
     """Raw data acquired."""
 
 
-target = 0
-# target = "D2"
+parser = ArgumentParser()
+parser.add_argument("--target", type=int, default=0, help="Target qubit index")
+parser.add_argument("--platform", type=str, default="dummy", help="Platform name")
+parser.add_argument(
+    "--path", type=str, default="CoherenceFlux", help="Path for the output"
+)
+args = parser.parse_args()
+
+target = args.target
+platform = args.platform
+path = args.path
+
 with Executor.open(
     "myexec",
-    path="CoherenceFlux",
-    # platform="qw11q",
-    platform="dummy",
+    path=args.path,
+    platform=platform,
     targets=[target],
     update=True,
     force=True,
 ) as e:
 
     data = CoherenceFluxSignalData()
-    params = CoherenceFluxSignalParameters()
 
     for target in e.targets:
 
         # TODO: Get the right parameters from the platform
-        # params_qubit = {
-        #     "w_max": platform.qubits[
-        #         target
-        #     ].drive_frequency,  # FIXME: this is not the qubit frequency
-        #     "xj": 0,
-        #     "d": platform.qubits[target].asymmetry,
-        #     "normalization": platform.qubits[target].crosstalk_matrix[target],
-        #     "offset": -platform.qubits[target].sweetspot
-        #     * platform.qubits[target].crosstalk_matrix[
-        #         target
-        #     ],  # Check is this the right one ???
-        #     "crosstalk_element": 1,
-        #     "charging_energy": platform.qubits[target].Ec,
-        # }
+        params_qubit = {
+            "w_max": e.platform.qubits[
+                target
+            ].drive_frequency,  # FIXME: this is not the qubit frequency
+            "xj": 0,
+            "d": e.platform.qubits[target].asymmetry,
+            "normalization": e.platform.qubits[target].crosstalk_matrix[target, target],
+            "offset": -e.platform.qubits[target].sweetspot
+            * e.platform.qubits[target].crosstalk_matrix[
+                target
+            ],  # Check is this the right one ???
+            "crosstalk_element": 1,
+            "charging_energy": e.platform.qubits[target].Ec,
+        }
 
         # D2
         params_qubit = {
@@ -151,7 +155,7 @@ with Executor.open(
         fit_function = transmon_frequency
 
         # TODO: Center around the sweetspot
-        biases = params.biases
+        # biases += e.platform.qubits[target].sweetspot
 
         i = 0
         for bias in biases:
@@ -170,10 +174,10 @@ with Executor.open(
             e.platform.qubits[target].native_gates.RX.frequency = qubit_frequency
 
             qubit_spectroscopy_output = e.qubit_spectroscopy(
-                freq_width=params.freq_width,
-                freq_step=params.freq_step,
-                drive_duration=params.drive_duration,
-                drive_amplitude=params.drive_amplitude,
+                freq_width=freq_width,
+                freq_step=freq_step,
+                drive_duration=drive_duration,
+                drive_amplitude=drive_amplitude,
                 relaxation_time=5000,
                 nshots=1024,
             )
@@ -182,12 +186,12 @@ with Executor.open(
             e.platform.qubits[target].native_gates.RX.amplitude = (
                 0.5  # FIXME: For QM this should be 0.5
             )
-            e.platform.qubits[target].native_gates.RX.duration = params.pulse_length
+            e.platform.qubits[target].native_gates.RX.duration = pulse_length
 
             rabi_output = e.rabi_amplitude_signal(
-                min_amp_factor=params.min_amp_factor,
-                max_amp_factor=params.max_amp_factor,
-                step_amp_factor=params.step_amp_factor,
+                min_amp_factor=min_amp_factor,
+                max_amp_factor=max_amp_factor,
+                step_amp_factor=step_amp_factor,
                 pulse_length=e.platform.qubits[target].native_gates.RX.duration,
             )
 
@@ -198,36 +202,36 @@ with Executor.open(
                 continue
 
             ramsey_output = e.ramsey_signal(
-                delay_between_pulses_start=params.delay_between_pulses_start,
-                delay_between_pulses_end=params.delay_between_pulses_end,
-                delay_between_pulses_step=params.delay_between_pulses_step,
-                detuning=params.detuning,
+                delay_between_pulses_start=delay_between_pulses_start,
+                delay_between_pulses_end=delay_between_pulses_end,
+                delay_between_pulses_step=delay_between_pulses_step,
+                detuning=detuning,
             )
 
             flipping_output = e.flipping_signal(
-                nflips_max=params.nflips_max,
-                nflips_step=params.nflips_step,
+                nflips_max=nflips_max,
+                nflips_step=nflips_step,
             )
 
             t1_output = e.t1_signal(
-                delay_before_readout_start=params.delay_before_readout_start,
-                delay_before_readout_end=params.delay_before_readout_end,
-                delay_before_readout_step=params.delay_before_readout_step,
-                single_shot=params.single_shot_T1,
+                delay_before_readout_start=delay_before_readout_start,
+                delay_before_readout_end=delay_before_readout_end,
+                delay_before_readout_step=delay_before_readout_step,
+                single_shot=single_shot_T1,
             )
 
             # TODO: Estimate T2 with Ramsey signal without detuning
             t2_output = e.t2_signal(
-                delay_between_pulses_start=params.delay_between_pulses_start_T2,
-                delay_between_pulses_end=params.delay_between_pulses_end_T2,
-                delay_between_pulses_step=params.delay_between_pulses_step_T2,
-                single_shot=params.single_shot_T2,
+                delay_between_pulses_start=delay_between_pulses_start_T2,
+                delay_between_pulses_end=delay_between_pulses_end_T2,
+                delay_between_pulses_step=delay_between_pulses_step_T2,
+                single_shot=single_shot_T2,
             )
 
             ramsey_t2_output = e.ramsey_signal(
-                delay_between_pulses_start=params.delay_between_pulses_start_T2,
-                delay_between_pulses_end=params.delay_between_pulses_end_T2,
-                delay_between_pulses_step=params.delay_between_pulses_step_T2,
+                delay_between_pulses_start=delay_between_pulses_start_T2,
+                delay_between_pulses_end=delay_between_pulses_end_T2,
+                delay_between_pulses_step=delay_between_pulses_step_T2,
                 detuning=0,
             )
 
@@ -245,12 +249,11 @@ with Executor.open(
                 ),
             )
 
-            # close()
             report(e.path, e.history)
             e.history = History()
 
 
-def _plot(data: CoherenceFluxSignalData, target: QubitId, fit=None):
+def plot(data: CoherenceFluxSignalData, target: QubitId, fit=None):
     """Plotting function for Coherence experiment."""
 
     figures = []
@@ -289,13 +292,13 @@ def _plot(data: CoherenceFluxSignalData, target: QubitId, fit=None):
         )
     )
 
-    # last part
     figure.update_layout(
         showlegend=True,
         xaxis_title="Frequency [GHZ]",
         yaxis_title="Coherence [ns]",
     )
 
-    figures.append(figure)
+    figure.write_image(e.path / ".pdf")
 
-    return figures, ""
+
+plot(data, target)
