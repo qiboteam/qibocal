3dfa75e2c793b5a72a23a7c1746b819aea290cfa
Alessandro Candido <candido.ale@gmail.com>
2024-07-30 11:23:14 +0200

    test: Make platform explicit, make use of routine result
    To be more explicit as an example and template

--- runcards/single_shot.py
+++ runcards/single_shot.py
@@ -20,9 +20,11 @@ from qibocal.cli.report import report
 from qibocal.routines import close, init, single_shot_classification
 
 path = Path("test_x")
+init(platform="dummy", path=path, force=True)
 
-init(path=path, force=True)
-single_shot_classification(nshots=1000)
+# Here
+ssc = single_shot_classification(nshots=1000)
+print("\nfidelities:\n", ssc.results.fidelity, "\n")
+
 close()
-
 report(path)
