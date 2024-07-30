194099f2c1f2c0a5e2233bebdb81aaba45992e5d
Alessandro Candido <candido.ale@gmail.com>
2024-07-30 11:32:39 +0200

    test: Replace repeated updates with global option

--- runcards/rx_calibration.py
+++ runcards/rx_calibration.py
@@ -3,7 +3,12 @@ from qibocal.cli.report import report
 
 target = 0
 with Executor.open(
-    "myexec", path="test_rx_calibration", platform="dummy", targets=[target], force=True
+    "myexec",
+    path="test_rx_calibration",
+    platform="dummy",
+    targets=[target],
+    update=True,
+    force=True,
 ) as e:
     e.platform.settings.nshots = 2000
 
@@ -18,13 +23,13 @@ with Executor.open(
         raise RuntimeError(
             f"Rabi fit has chi2 {rabi_output.results.chi2[target][0]} greater than 2. Stopping."
         )
-    rabi_output.update_platform(e.platform)
 
     ramsey_output = e.ramsey(
         delay_between_pulses_start=10,
         delay_between_pulses_end=5000,
         delay_between_pulses_step=100,
         detuning=1_000_000,
+        update=False,
     )
     if ramsey_output.results.chi2[target][0] > 2:
         raise RuntimeError(
@@ -48,14 +53,12 @@ with Executor.open(
         raise RuntimeError(
             f"Rabi fit has chi2 {rabi_output_2.results.chi2[target][0]} greater than 2. Stopping."
         )
-    rabi_output_2.update_platform(e.platform)
 
     drag_output = e.drag_tuning(beta_start=-4, beta_end=4, beta_step=0.5)
     if drag_output.results.chi2[target][0] > 2:
         raise RuntimeError(
             f"Drag fit has chi2 {drag_output.results.chi2[target][0]} greater than 2. Stopping."
         )
-    drag_output.update_platform(e.platform)
 
     rabi_output_3 = e.rabi_amplitude(
         min_amp_factor=0.5,
@@ -68,6 +71,5 @@ with Executor.open(
         raise RuntimeError(
             f"Rabi fit has chi2 {rabi_output_3.results.chi2[target][0]} greater than 2. Stopping."
         )
-    rabi_output_3.update_platform(e.platform)
 
 report(e.path, e.history)
