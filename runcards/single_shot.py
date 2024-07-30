2b68e8c8c29b5abc0497436860fd57a310cad1e4
Alessandro Candido <candido.ale@gmail.com>
2024-07-30 11:22:27 +0200

    docs: Document minimal script example

--- runcards/single_shot.py
+++ runcards/single_shot.py
@@ -1,3 +1,19 @@
+"""Minimal Qibocal script example.
+
+In this example, the default Qibocal executor is used. Additional
+configurations can still be passed through the `init()` call, but there
+is no explicit API to access the execution details.
+
+If more fine grained control is needed, refer to the `rx_calibration.py` example.
+
+.. note::
+
+    though simple, this example is not limited to single protocol execution, but
+    multiple protocols can be added as well, essentially in the same fashion of a plain
+    runcard - still with the advantage of handling execution and results
+    programmatically
+"""
+
 from pathlib import Path
 
 from qibocal.cli.report import report
