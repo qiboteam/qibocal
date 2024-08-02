970776b87a6213612139579a374ae2a98f2f09b1
Alessandro Candido <candido.ale@gmail.com>
2024-08-02 19:12:46 +0200

    test: Test executor open constructor

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from copy import deepcopy
 from dataclasses import dataclass
 from importlib import reload
@@ -186,10 +187,21 @@ def test_default_executor(tmp_path: Path, fake_platform: str, monkeypatch):
 
 
 def test_context_manager(tmp_path: Path, executor: Executor):
-    path = tmp_path / "my-open-folder"
+    path = tmp_path / "my-ctx-folder"
 
     executor.init(path)
 
     with executor:
         assert executor.meta is not None
         assert executor.meta.start is not None
+
+
+def test_open(tmp_path: Path):
+    path = tmp_path / "my-open-folder"
+
+    with Executor.open("myexec", path) as e:
+        assert isinstance(e.t1, Callable)
+        assert e.meta is not None
+        assert e.meta.start is not None
+
+    assert e.meta.end is not None
