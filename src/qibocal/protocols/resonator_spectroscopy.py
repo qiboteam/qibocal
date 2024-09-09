6c0ee02948005c07b06b04a5c29d0cd6f8a0f5e8
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-09-09 18:08:09 +0400

    chore: update resonator spectroscopy after moving results to qibocal

--- src/qibocal/protocols/resonator_spectroscopy.py
+++ src/qibocal/protocols/resonator_spectroscopy.py
@@ -7,12 +7,12 @@ import numpy.typing as npt
 from _collections_abc import Callable
 from qibolab.identifier import QubitId
 from qibolab.platform import Platform
-from qibolab.result import magnitude, phase
 from qibolab.sequence import PulseSequence
 from qibolab.sweeper import Parameter, Sweeper
 
 from qibocal import update
 from qibocal.auto.operation import Data, Parameters, Results, Routine
+from qibocal.result import magnitude, phase
 
 from .utils import (
     PowerLevel,
