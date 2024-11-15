478304b809b33e45f9396ab8f427ecd71d2b5158
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-11-15 15:23:33 +0400

    fix: drop redundant line

--- src/qibocal/protocols/qua/rb_ondevice.py
+++ src/qibocal/protocols/qua/rb_ondevice.py
@@ -52,10 +52,6 @@ class RbOnDeviceParameters(Parameters):
         if self.seed is None:
             self.seed = np.random.randint(0, int(1e6))
 
-    def __post_init__(self):
-        if self.seed is None:
-            self.seed = np.random.randint(0, int(1e6))
-
 
 def generate_sequence(max_circuit_depth, seed):
     sequence = declare(int, size=max_circuit_depth + 1)
@@ -395,9 +391,6 @@ def _acquisition(
     job = qm.execute(
         rb, compiler_options=CompilerOptionArguments(flags=["not-strict-timing"])
     )
-
-    with open("qua_script.py", "w") as file:
-        file.write(generate_qua_script(rb, config))
 
     # Get results from QUA program
     if state_discrimination:
