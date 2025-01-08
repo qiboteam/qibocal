fb165fc1eb9dc57fd6344873b4169ad1abed323c
Andrea Pasquale <andreapasquale97@gmail.com>
2025-01-08 10:57:16 +0100

    fix: Replace > with >=

--- src/qibocal/protocols/coherence/cpmg.py
+++ src/qibocal/protocols/coherence/cpmg.py
@@ -51,7 +51,7 @@ def _acquisition(
         # this is assuming that RX and RX90 have the same duration
         assert (
             params.delay_between_pulses_start / 2 / params.n
-            > platform.natives.single_qubit[q].RX()[0][1].duration
+            >= platform.natives.single_qubit[q].RX()[0][1].duration
         ), (
             f"Initial delay too short for qubit {q}, "
             f"minimum delay should be {platform.natives.single_qubit[q].RX()[0][1].duration * 2 * params.n}"
