47f0320fc60ef178a8e5da928443fba3f035d245
Andrea Pasquale <andreapasquale97@gmail.com>
2025-02-26 21:51:37 +0100

    feat: Add mechanis to load platforms

--- src/qibocal/__init__.py
+++ src/qibocal/__init__.py
@@ -1,11 +1,35 @@
 """qibocal: Quantum Calibration Verification and Validation using Qibo."""
 
+import os
+from pathlib import Path
+
 from . import protocols
 from .auto.execute import Executor
+from .calibration import create_calibration_platform
 from .cli import command
 from .version import __version__
 
-__all__ = ["Executor", "protocols", "command", "__version__"]
+__all__ = [
+    "Executor",
+    "protocols",
+    "command",
+    "__version__",
+    "create_calibration_platform",
+]
+
+FOLDER = Path(__file__).parents[2] / "platforms"
+
+
+def _add_platforms():
+    new_folder = str(FOLDER)
+    existing_platforms = os.getenv("QIBOLAB_PLATFORMS", "")
+    platforms_set = {str(Path(p)) for p in existing_platforms.split(os.pathsep) if p}
+    if new_folder not in platforms_set:
+        platforms_set.add(new_folder)
+        os.environ["QIBOLAB_PLATFORMS"] = os.pathsep.join(platforms_set)
+
+
+_add_platforms()
 
 DEFAULT_EXECUTOR = Executor.create(".routines", platform="dummy")
 """Default executor, registered as a qibocal submodule.
