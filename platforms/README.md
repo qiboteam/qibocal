b1311c4eaaf87f7fe84abb6efb6c66fcf3215135
Andrea Pasquale <andreapasquale97@gmail.com>
2025-02-27 12:01:16 +0100

    feat: Add mock to platforms folder

---
+++ platforms/README.md
@@ -0,0 +1,3 @@
+# Example platforms
+
+In this folder there are a few example platforms that can be used with Qibocal. To use these platforms we need to add the `platforms` folder to the `QIBOLAB_PLATFORMS` variable.
--- src/qibocal/__init__.py
+++ src/qibocal/__init__.py
@@ -2,7 +2,14 @@
 
 from . import protocols
 from .auto.execute import Executor
+from .calibration import create_calibration_platform
 from .cli import command
 from .version import __version__
 
-__all__ = ["Executor", "protocols", "command", "__version__"]
+__all__ = [
+    "Executor",
+    "protocols",
+    "command",
+    "__version__",
+    "create_calibration_platform",
+]
