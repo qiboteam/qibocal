6a8ffb8d1832e43d5c9eba68780a98e5aa0495a4
Andrea Pasquale <andreapasquale97@gmail.com>
2025-02-27 13:02:29 +0100

    refactor: Do not load platforms at import time

--- src/qibocal/__init__.py
+++ src/qibocal/__init__.py
@@ -1,8 +1,5 @@
 """qibocal: Quantum Calibration Verification and Validation using Qibo."""
 
-import os
-from pathlib import Path
-
 from . import protocols
 from .auto.execute import Executor
 from .calibration import create_calibration_platform
@@ -16,20 +13,6 @@ __all__ = [
     "__version__",
     "create_calibration_platform",
 ]
-
-FOLDER = Path(__file__).parents[2] / "platforms"
-
-
-def _add_platforms():
-    new_folder = str(FOLDER)
-    existing_platforms = os.getenv("QIBOLAB_PLATFORMS", "")
-    platforms_set = {str(Path(p)) for p in existing_platforms.split(os.pathsep) if p}
-    if new_folder not in platforms_set:
-        platforms_set.add(new_folder)
-        os.environ["QIBOLAB_PLATFORMS"] = os.pathsep.join(platforms_set)
-
-
-_add_platforms()
 
 DEFAULT_EXECUTOR = Executor.create(".routines", platform="dummy")
 """Default executor, registered as a qibocal submodule.
