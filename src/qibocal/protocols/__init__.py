4fd65f67ded394274e9ed5ec374989bd25c13bd8
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2025-03-11 18:16:35 +0400

    feat: implement 2q RB using Qiskit circuits

--- src/qibocal/protocols/__init__.py
+++ src/qibocal/protocols/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from . import (
     allxy,
     classification,
@@ -16,6 +17,58 @@ from . import (
     signal_experiments,
     tomographies,
     two_qubit_interaction,
+=======
+from .allxy.allxy import allxy
+from .allxy.allxy_resonator_depletion_tuning import allxy_resonator_depletion_tuning
+from .classification import single_shot_classification
+from .coherence.cpmg import cpmg
+from .coherence.spin_echo import spin_echo
+from .coherence.spin_echo_signal import spin_echo_signal
+from .coherence.t1 import t1
+from .coherence.t1_signal import t1_signal
+from .coherence.t2 import t2
+from .coherence.t2_signal import t2_signal
+from .coherence.zeno import zeno
+from .dispersive_shift import dispersive_shift
+from .dispersive_shift_qutrit import dispersive_shift_qutrit
+from .drag import drag_tuning
+from .drag_simple import drag_simple
+from .flipping import flipping
+from .flux_amplitude_frequency import flux_amplitude_frequency
+from .flux_dependence.qubit_crosstalk import qubit_crosstalk
+from .flux_dependence.qubit_flux_dependence import qubit_flux
+from .flux_dependence.resonator_flux_dependence import resonator_flux
+from .flux_gate import flux_gate
+from .qubit_power_spectroscopy import qubit_power_spectroscopy
+from .qubit_spectroscopy import qubit_spectroscopy
+from .qubit_spectroscopy_ef import qubit_spectroscopy_ef
+from .qubit_vz import qubit_vz
+from .qutrit_classification import qutrit_classification
+from .rabi.amplitude import rabi_amplitude
+from .rabi.amplitude_frequency import rabi_amplitude_frequency
+from .rabi.amplitude_frequency_signal import rabi_amplitude_frequency_signal
+from .rabi.amplitude_signal import rabi_amplitude_signal
+from .rabi.ef import rabi_amplitude_ef
+from .rabi.length import rabi_length
+from .rabi.length_frequency import rabi_length_frequency
+from .rabi.length_frequency_signal import rabi_length_frequency_signal
+from .rabi.length_signal import rabi_length_signal
+from .ramsey.ramsey import ramsey
+from .ramsey.ramsey_signal import ramsey_signal
+from .ramsey.ramsey_zz import ramsey_zz
+from .randomized_benchmarking.filtered_rb import filtered_rb
+from .randomized_benchmarking.standard_rb import standard_rb
+from .randomized_benchmarking.standard_rb_2q import standard_rb_2q
+from .randomized_benchmarking.standard_rb_2q_inter import standard_rb_2q_inter
+from .rb_qiskit import rb_qiskit
+from .readout_characterization import readout_characterization
+from .readout_mitigation_matrix import readout_mitigation_matrix
+from .readout_optimization.resonator_amplitude import resonator_amplitude
+from .resonator_punchout import resonator_punchout
+from .resonator_spectroscopy import resonator_spectroscopy
+from .signal_experiments.calibrate_state_discrimination import (
+    calibrate_state_discrimination,
+>>>>>>> 18904cd3 (feat: implement 2q RB using Qiskit circuits)
 )
 from .allxy import *
 from .classification import *
@@ -35,6 +88,7 @@ from .signal_experiments import *
 from .tomographies import *
 from .two_qubit_interaction import *
 
+<<<<<<< HEAD
 __all__ = []
 __all__ += ["allxy"]
 __all__ += ["coherence"]
@@ -54,3 +108,64 @@ __all__ += ["tomographies"]
 __all__ += ["resonator_spectroscopies"]
 __all__ += ["qubit_spectroscopies"]
 __all__ += ["two_qubit_interaction"]
+=======
+__all__ = [
+    "allxy",
+    "single_shot_classification",
+    "spin_echo",
+    "spin_echo_signal",
+    "t1",
+    "t1_signal",
+    "t2",
+    "t2_signal",
+    "zeno",
+    "dispersive_shift",
+    "dispersive_shift_qutrit",
+    "drag_tuning",
+    "flipping",
+    "qubit_crosstalk",
+    "qubit_flux",
+    "resonator_flux",
+    "qubit_spectroscopy",
+    "qubit_spectroscopy_ef",
+    "qubit_vz",
+    "qutrit_classification",
+    "rabi_amplitude",
+    "rabi_amplitude_signal",
+    "rabi_length",
+    "rabi_amplitude_ef",
+    "rabi_length_signal",
+    "ramsey",
+    "ramsey_signal",
+    "filtered_rb",
+    "standard_rb",
+    "readout_characterization",
+    "readout_mitigation_matrix",
+    "resonator_amplitude",
+    "resonator_punchout",
+    "resonator_spectroscopy",
+    "calibrate_state_discrimination",
+    "time_of_flight_readout",
+    "chevron",
+    "chevron_signal",
+    "correct_virtual_z_phases",
+    "state_tomography",
+    "allxy_resonator_depletion_tuning",
+    "two_qubit_state_tomography",
+    "qubit_power_spectroscopy",
+    "rabi_amplitude_frequency",
+    "rabi_amplitude_frequency_signal",
+    "rabi_length_frequency",
+    "rabi_length_frequency_signal",
+    "standard_rb_2q",
+    "standard_rb_2q_inter",
+    "optimize_two_qubit_gate",
+    "cryoscope",
+    "ramsey_zz",
+    "flux_gate",
+    "flux_amplitude_frequency",
+    "cpmg",
+    "drag_simple",
+    "rb_qiskit",
+]
+>>>>>>> 18904cd3 (feat: implement 2q RB using Qiskit circuits)
