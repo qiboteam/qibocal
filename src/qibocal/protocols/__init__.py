1a622529c9184ca56acc368d3253a735649b9bb6
ElStabilini <el.stabilini@gmail.com>
2025-03-12 11:10:17 +0100

    fix import error

--- src/qibocal/protocols/__init__.py
+++ src/qibocal/protocols/__init__.py
@@ -19,6 +19,7 @@ from .flux_dependence.qubit_crosstalk import qubit_crosstalk
 from .flux_dependence.qubit_flux_dependence import qubit_flux
 from .flux_dependence.resonator_flux_dependence import resonator_flux
 from .flux_gate import flux_gate
+from .flux_reconstruction import flux_reconstruction
 from .qubit_power_spectroscopy import qubit_power_spectroscopy
 from .qubit_spectroscopy import qubit_spectroscopy
 from .qubit_spectroscopy_ef import qubit_spectroscopy_ef
