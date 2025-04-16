12edef76aa60cb3d64dcb1fba878b3f641a747db
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2025-04-16 18:52:57 +0400

    fix: execute single qubit RB on 0.2

--- src/qibocal/protocols/qua/two_qubit_rb/__init__.py
+++ src/qibocal/protocols/qua/two_qubit_rb/__init__.py
@@ -1,5 +1,3 @@
-from .gates import gate_db
-from .RBBaker import RBBaker
-from .RBResult import QuaTwoQubitRbData
-from .simple_tableau import SimpleTableau
-from .TwoQubitRB import TwoQubitRb
+from .TwoQubitRB import TwoQubitRb as TwoQubitRb
+
+__all__ = ["TwoQubitRb"]
