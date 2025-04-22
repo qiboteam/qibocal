4f87e24aca515869dcd7fc26c5ef47a66dd0d64d
Andrea Pasquale <andreapasquale97@gmail.com>
2025-04-22 14:08:44 +0200

    refactor: Adding tomographies protocols

--- src/qibocal/protocols/two_qubit_interaction/__init__.py
+++ src/qibocal/protocols/two_qubit_interaction/__init__.py
@@ -5,7 +5,12 @@ from .snz_optimize import snz_optimize
 from .snz_optimize_t_idle import snz_optimize_t_idle
 from .snz_optimize_t_idle_vs_t_tot import snz_optimize_t_idle_vs_t_tot
 from .virtual_z_phases import correct_virtual_z_phases
-from .cross_resonance import cross_resonance_length, cross_resonance_amplitude, hamiltonian_tomography_cr
+from .cross_resonance import (
+    cross_resonance_length, 
+    cross_resonance_amplitude, 
+    hamiltonian_tomography_cr_length,
+    hamiltonian_tomography_cr_amplitude,
+)
 
 __all__ = []
 __all__ += ["chevron", "chevron_signal"]
@@ -13,3 +18,4 @@ __all__ += ["optimize_two_qubit_gate", "correct_virtual_z_phases"]
 __all__ += ["snz_optimize", "snz_optimize_t_idle", "snz_optimize_t_idle_vs_t_tot"]
 __all__ += ["chsh"]
 __all__ += ["cross_resonance_length", "cross_resonance_amplitude", "hamiltonian_tomography_cr"]
+__all__ += ["hamiltonian_tomography_cr_length", "hamiltonian_tomography_cr_amplitude"]
--- src/qibocal/protocols/two_qubit_interaction/cross_resonance/__init__.py
+++ src/qibocal/protocols/two_qubit_interaction/cross_resonance/__init__.py
@@ -1,5 +1,8 @@
 from .amplitude import cross_resonance_amplitude as cross_resonance_amplitude
-from .hamiltonian_tomography import (
-    hamiltonian_tomography_cr as hamiltonian_tomography_cr,
-)
 from .length import cross_resonance_length as cross_resonance_length
+from .tomographies import (
+    hamiltonian_tomography_cr_amplitude as hamiltonian_tomography_cr_amplitude,
+)
+from .tomographies import (
+    hamiltonian_tomography_cr_length as hamiltonian_tomography_cr_length,
+)
