a8c1f6a0daf24fc519a4fe5957ffabc575aaa316
Andrea Pasquale <andreapasquale97@gmail.com>
2025-04-22 11:37:05 +0200

    feat: Adding hamiltonian_tomography_cr (no fitting)

--- src/qibocal/protocols/two_qubit_interaction/__init__.py
+++ src/qibocal/protocols/two_qubit_interaction/__init__.py
@@ -5,11 +5,11 @@ from .snz_optimize import snz_optimize
 from .snz_optimize_t_idle import snz_optimize_t_idle
 from .snz_optimize_t_idle_vs_t_tot import snz_optimize_t_idle_vs_t_tot
 from .virtual_z_phases import correct_virtual_z_phases
-from .cross_resonance import cross_resonance_length, cross_resonance_amplitude
+from .cross_resonance import cross_resonance_length, cross_resonance_amplitude, hamiltonian_tomography_cr
 
 __all__ = []
 __all__ += ["chevron", "chevron_signal"]
 __all__ += ["optimize_two_qubit_gate", "correct_virtual_z_phases"]
 __all__ += ["snz_optimize", "snz_optimize_t_idle", "snz_optimize_t_idle_vs_t_tot"]
 __all__ += ["chsh"]
-__all__ += ["cross_resonance_length", "cross_resonance_amplitude"]
+__all__ += ["cross_resonance_length", "cross_resonance_amplitude", "hamiltonian_tomography_cr"]
