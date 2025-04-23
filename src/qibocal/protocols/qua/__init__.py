0daa5b1d8e55b4c25772c4cab419ae998ed58b52
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2025-04-23 21:11:37 +0000

    feat: add qua-libs cryoscope routine

--- src/qibocal/protocols/qua/__init__.py
+++ src/qibocal/protocols/qua/__init__.py
@@ -1,4 +1,5 @@
+from .cryoscope import qua_cryoscope
 from .rb_single_qubit import qua_standard_rb_1q
 from .rb_two_qubit import qua_standard_rb_2q
 
-__all__ = ["qua_standard_rb_1q", "qua_standard_rb_2q"]
+__all__ = ["qua_standard_rb_1q", "qua_standard_rb_2q", "qua_cryoscope"]
