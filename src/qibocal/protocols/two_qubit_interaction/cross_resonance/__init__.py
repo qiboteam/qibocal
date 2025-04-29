67ccc096d0d6571eac892bc1ad1bc3f13f4af2bd
Andrea Pasquale <andreapasquale97@gmail.com>
2025-04-29 13:31:31 +0200

    refactor: Document and simplify tomographies

--- src/qibocal/protocols/two_qubit_interaction/cross_resonance/__init__.py
+++ src/qibocal/protocols/two_qubit_interaction/cross_resonance/__init__.py
@@ -1,8 +1,5 @@
 from .amplitude import cross_resonance_amplitude as cross_resonance_amplitude
-from .length import cross_resonance_length as cross_resonance_length
-from .tomographies import (
-    hamiltonian_tomography_cr_amplitude as hamiltonian_tomography_cr_amplitude,
-)
-from .tomographies import (
+from .hamiltonian_tomography import (
     hamiltonian_tomography_cr_length as hamiltonian_tomography_cr_length,
 )
+from .length import cross_resonance_length as cross_resonance_length
