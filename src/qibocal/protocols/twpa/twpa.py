fix: use formula for converting voltage to dB instead of power to dB
@andrea-pasquale I believe the correct formula to convert voltage to dB is with a factor 20 instead of 10 (the factor 10 should be used when converting power to dB)

src/qibocal/protocols/twpa/twpa.py
@@ -245,7 +245,7 @@ def _plot(data: TwpaCalibrationData, fit: TwpaCalibrationResults, target):
         )
         averaged_gain = fit.data[target]
     else:
-        averaged_gain = 10 * np.log10(
+        averaged_gain = 20 * np.log10(
             np.mean(magnitude(data[target]), axis=2)
             / np.mean(magnitude(data.reference_value_array(target)), axis=0)
         )