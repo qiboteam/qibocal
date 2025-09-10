5736aa3f2d730dadefd6cd00bdf193026663f966
Andrea <andreapasquale97@gmail.com>
2025-09-10 20:02:28 +0000

    refactor: Single sweeper

--- src/qibocal/protocols/flux_dependence/long_cryoscope.py
+++ src/qibocal/protocols/flux_dependence/long_cryoscope.py
@@ -18,6 +18,7 @@ from qibocal.calibration import CalibrationPlatform
 from qibocal.result import magnitude
 
 from ..utils import (
+    GHZ_TO_HZ,
     HZ_TO_GHZ,
     extract_feature,
     readout_frequency,
@@ -34,7 +35,6 @@ class LongCryoscopeParameters(Parameters):
     duration_max: float
     duration_step: float
     flux_pulse_amplitude: float
-    flux_pulse_duration: float
     freq_width: float
     freq_step: float
 
@@ -51,8 +51,6 @@ class LongCryoscopeParameters(Parameters):
 class LongCryoscopeResults(Results):
     """LongCryoscope outputs."""
 
-    fitting_parameters: dict[QubitId, list]
-
 
 @dataclass
 class LongCryoscopeData(Data):
@@ -67,6 +65,26 @@ class LongCryoscopeData(Data):
         return x.ravel(), y.ravel(), magnitude(self.data[qubit]).ravel()
 
 
+def sequence(platform, target, flux_pulse_amplitude, delay):
+    seq = PulseSequence()
+    natives = platform.natives.single_qubit[target]
+    qd_channel, qd_pulse = natives.RX()[0]
+    ro_channel, ro_pulse = natives.MZ()[0]
+    flux_channel = platform.qubits[target].flux
+    flux_pulse = Pulse(
+        duration=2 * delay + qd_pulse.duration,
+        amplitude=flux_pulse_amplitude,
+        envelope=Rectangular(),
+    )
+
+    seq.append((flux_channel, flux_pulse))
+    seq.append((qd_channel, Delay(duration=delay)))
+    seq.append((qd_channel, qd_pulse))
+    seq.append((ro_channel, Delay(duration=flux_pulse.duration)))
+    seq.append((ro_channel, ro_pulse))
+    return seq
+
+
 def _acquisition(
     params: LongCryoscopeParameters,
     platform: CalibrationPlatform,
@@ -74,79 +92,64 @@ def _acquisition(
 ) -> LongCryoscopeData:
     """Data acquisition for LongCryoscope Experiment."""
 
-    sequence = PulseSequence()
     freq_sweepers = []
-    delays = [Delay(duration=0) for _ in range(2 * len(targets))]
-    for i, q in enumerate(targets):
-        natives = platform.natives.single_qubit[q]
-        qd_channel, qd_pulse = natives.RX()[0]
-        ro_channel, ro_pulse = natives.MZ()[0]
-        flux_channel = platform.qubits[q].flux
-        flux_pulse = Pulse(
-            duration=params.flux_pulse_duration,
-            amplitude=params.flux_pulse_amplitude,
-            envelope=Rectangular(),
-        )
-
-        sequence.append((flux_channel, flux_pulse))
-        sequence.align([flux_channel, qd_channel, ro_channel])
-        sequence.append((qd_channel, delays[2 * i]))
-        sequence.append((qd_channel, qd_pulse))
-        sequence.append((ro_channel, delays[2 * i + 1]))
-        sequence.append((ro_channel, Delay(duration=qd_pulse.duration)))
-        sequence.append((ro_channel, ro_pulse))
-
-        freq_sweepers.append(
-            Sweeper(
-                parameter=Parameter.frequency,
-                # we should correct this including the expected shift due to the flux pulse
-                values=platform.config(qd_channel).frequency + params.frequency_range,
-                channels=[qd_channel],
+    data_ = {q: [] for q in targets}
+    for delay in params.duration_range:
+        seq = PulseSequence()
+        for i, q in enumerate(targets):
+            seq += sequence(platform, q, params.flux_pulse_amplitude, delay)
+            qd_channel = platform.qubits[q].drive
+            freq_sweepers.append(
+                Sweeper(
+                    parameter=Parameter.frequency,
+                    values=platform.config(qd_channel).frequency
+                    + platform.calibration.single_qubits[q].qubit.detuning(
+                        params.flux_pulse_amplitude
+                    )
+                    * GHZ_TO_HZ
+                    + params.frequency_range,
+                    channels=[qd_channel],
+                )
             )
+
+        results = platform.execute(
+            [seq],
+            [freq_sweepers],
+            updates=[
+                {
+                    platform.qubits[q].probe: {
+                        "frequency": readout_frequency(q, platform)
+                    }
+                }
+                for q in targets
+            ],
+            nshots=params.nshots,
+            relaxation_time=params.relaxation_time,
+            acquisition_type=AcquisitionType.INTEGRATION,
+            averaging_mode=AveragingMode.CYCLIC,
         )
 
-    duration_sweeper = Sweeper(
-        parameter=Parameter.duration,
-        values=params.duration_range,
-        pulses=delays,
-    )
-
+        for qubit in targets:
+            acq_handle = list(seq.channel(platform.qubits[qubit].acquisition))[-1].id
+            data_[qubit].append(results[acq_handle])
     data = LongCryoscopeData(
         frequency_swept={
             qubit: freq_sweepers[i].values.tolist() for i, qubit in enumerate(targets)
         },
-        duration_swept=duration_sweeper.values.tolist(),
+        duration_swept=params.duration_range.tolist(),
+        data={qubit: np.stack(result) for qubit, result in data_.items()},
     )
-    results = platform.execute(
-        [sequence],
-        [[duration_sweeper], freq_sweepers],
-        updates=[
-            {platform.qubits[q].probe: {"frequency": readout_frequency(q, platform)}}
-            for q in targets
-        ],
-        nshots=params.nshots,
-        relaxation_time=params.relaxation_time,
-        acquisition_type=AcquisitionType.INTEGRATION,
-        averaging_mode=AveragingMode.CYCLIC,
-    )
-
-    for qubit in targets:
-        acq_handle = list(sequence.channel(platform.qubits[qubit].acquisition))[-1].id
-        data.data[qubit] = results[acq_handle]
     return data
 
 
 def _fit(data: LongCryoscopeData) -> LongCryoscopeResults:
-    fitting_parameters = {}
-
-    return LongCryoscopeResults(fitting_parameters=fitting_parameters)
+    return LongCryoscopeResults()
 
 
 def _plot(data: LongCryoscopeData, fit: LongCryoscopeResults, target: QubitId):
     """Plotting function for LongCryoscope Experiment."""
     fig = go.Figure()
     freq, delay = extract_feature(*data.grid(target), find_min=False)
-
     fig.add_trace(
         go.Heatmap(
             x=data.duration_swept,
