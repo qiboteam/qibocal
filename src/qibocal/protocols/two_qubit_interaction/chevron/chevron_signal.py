16194925112ef54774cc20ca296f597fe7703bec
Andrea Pasquale <andreapasquale97@gmail.com>
2025-10-13 11:57:50 +0200

    refactor: Simplify acquisition

--- src/qibocal/protocols/two_qubit_interaction/chevron/chevron_signal.py
+++ src/qibocal/protocols/two_qubit_interaction/chevron/chevron_signal.py
@@ -1,9 +1,8 @@
 """SWAP experiment for two qubit gates, chevron plot."""
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 import numpy as np
-import numpy.typing as npt
 from qibolab import AcquisitionType, AveragingMode, Parameter, Sweeper
 
 from qibocal.auto.operation import QubitPairId, Routine
@@ -35,43 +34,9 @@ class ChevronSignalResults(ChevronResults):
     """ChevronSignal outputs when fitting will be done."""
 
 
-ChevronSignalType = np.dtype(
-    [
-        ("amp", np.float64),
-        ("length", np.float64),
-        ("signal_high", np.float64),
-        ("signal_low", np.float64),
-    ]
-)
-"""Custom dtype for Chevron."""
-
-
 @dataclass
 class ChevronSignalData(ChevronData):
     """Chevron acquisition outputs."""
-
-    data: dict[QubitPairId, npt.NDArray[ChevronSignalType]] = field(
-        default_factory=dict
-    )
-
-    def register_qubit(
-        self, low_qubit, high_qubit, length, amp, signal_low, signal_high
-    ):
-        """Store output for single qubit."""
-        size = len(length) * len(amp)
-        amplitude, duration = np.meshgrid(amp, length)
-        ar = np.empty(size, dtype=ChevronSignalType)
-        ar["length"] = duration.ravel()
-        ar["amp"] = amplitude.ravel()
-        ar["signal_low"] = signal_low.ravel()
-        ar["signal_high"] = signal_high.ravel()
-        self.data[low_qubit, high_qubit] = np.rec.array(ar)
-
-    def low_frequency(self, pair):
-        return self[pair].signal_low
-
-    def high_frequency(self, pair):
-        return self[pair].signal_high
 
 
 def _aquisition(
@@ -91,20 +56,25 @@ def _aquisition(
         ChevronData: Acquisition data.
     """
 
-    # create a DataUnits object to store the results,
-    data = ChevronSignalData(native=params.native)
-    for pair in targets:
-        # order the qubits so that the low frequency one is the first
-        ordered_pair = order_pair(pair, platform)
+    data = ChevronSignalData(
+        native=params.native,
+        _sorted_pairs=[order_pair(pair, platform) for pair in targets],
+        amplitude=np.arange(
+            params.amplitude_min, params.amplitude_max, params.amplitude_step
+        ).tolist(),
+        duration=np.arange(
+            params.duration_min, params.duration_max, params.duration_step
+        ).tolist(),
+    )
+    for pair in data.sorted_pairs:
         sequence, flux_pulse, parking_pulses, delays = chevron_sequence(
             platform=platform,
-            ordered_pair=ordered_pair,
+            ordered_pair=pair,
             duration_max=params.duration_max,
             parking=params.parking,
             dt=params.dt,
             native=params.native,
         )
-
         sweeper_amplitude = Sweeper(
             parameter=Parameter.amplitude,
             range=(params.amplitude_min, params.amplitude_max, params.amplitude_step),
@@ -116,14 +86,8 @@ def _aquisition(
             pulses=[flux_pulse] + delays + parking_pulses,
         )
 
-        ro_high = list(sequence.channel(platform.qubits[ordered_pair[1]].acquisition))[
-            -1
-        ]
-        ro_low = list(sequence.channel(platform.qubits[ordered_pair[0]].acquisition))[
-            -1
-        ]
-
-        data.native_amplitude[ordered_pair] = flux_pulse.amplitude
+        ro_high = list(sequence.channel(platform.qubits[pair[1]].acquisition))[-1]
+        ro_low = list(sequence.channel(platform.qubits[pair[0]].acquisition))[-1]
 
         results = platform.execute(
             [sequence],
@@ -141,14 +105,16 @@ def _aquisition(
             acquisition_type=AcquisitionType.INTEGRATION,
             averaging_mode=AveragingMode.CYCLIC,
         )
-        data.register_qubit(
-            ordered_pair[0],
-            ordered_pair[1],
-            sweeper_duration.values,
-            sweeper_amplitude.values,
-            magnitude(results[ro_low.id]),
-            magnitude(results[ro_high.id]),
+
+        data.data[pair[0], pair[1]] = magnitude(
+            np.stack(
+                [
+                    results[ro_low.id],
+                    results[ro_high.id],
+                ]
+            )
         )
+
     return data
 
 
