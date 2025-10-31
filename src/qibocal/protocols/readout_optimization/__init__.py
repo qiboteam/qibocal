cfba5d9b9cb6c09cbfa21cb71e3a779c27bf6593
Andrea Pasquale <andreapasquale97@gmail.com>
2025-10-31 12:38:03 +0100

    refactor: Update 2d readout optimization

--- src/qibocal/protocols/readout_optimization/__init__.py
+++ src/qibocal/protocols/readout_optimization/__init__.py
@@ -1,7 +1,7 @@
 from .amplitude import readout_amplitude_optimization
 from .frequency import readout_frequency_optimization
-from .resonator_optimization import resonator_optimization
+from .optimization import readout_optimization
 
 __all__ = ["readout_amplitude_optimization"]
-__all__ += ["resonator_optimization"]
+__all__ += ["readout_optimization"]
 __all__ += ["readout_frequency_optimization"]
