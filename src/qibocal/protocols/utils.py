e726fe8a7c1ad1daffe35356df00f541b0397de8
lballerio <lorenzo@ballerio.com>
2025-12-30 15:03:14 +0000

    resolve bug in extract_feature (missing qubitID when calling merging function)

--- src/qibocal/protocols/utils.py
+++ src/qibocal/protocols/utils.py
@@ -1006,6 +1006,7 @@ def extract_feature(
         min_points,
         distance_xy=DISTANCE_XY,
         distance_z=DISTANCE_Z,
+        target=qubit
     )
 
     return peaks_dict["x"]["val"][signal_classification], peaks_dict["y"]["val"][
