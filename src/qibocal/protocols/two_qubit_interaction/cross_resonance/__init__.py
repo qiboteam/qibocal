98eb9fa826a371ed70c6af769d698dc8cb139650
lballerio <lorenzo@ballerio.com>
2026-01-23 14:53:30 +0000

    adding Bloch vector evolution for hamiltonian tomography and adding target cancellation pulse calibration

--- src/qibocal/protocols/two_qubit_interaction/cross_resonance/__init__.py
+++ src/qibocal/protocols/two_qubit_interaction/cross_resonance/__init__.py
@@ -1,5 +1,13 @@
-from .amplitude import cross_resonance_amplitude as cross_resonance_amplitude
+from .amplitude import cross_resonance_amplitude
 from .hamiltonian_tomography import (
-    hamiltonian_tomography_cr_length as hamiltonian_tomography_cr_length,
+    hamiltonian_tomography_cr_amplitude,
+    hamiltonian_tomography_cr_length,
 )
-from .length import cross_resonance_length as cross_resonance_length
+from .length import cross_resonance_length
+
+__all__ = [
+    "cross_resonance_amplitude",
+    "hamiltonian_tomography_cr_length",
+    "cross_resonance_length",
+    "hamiltonian_tomography_cr_amplitude",
+]
