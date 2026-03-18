5bbd044d516bad022adba73fd62a1ca8cb5d8dc7
Alessandro Candido <candido.ale@gmail.com>
2026-03-18 11:20:00 +0400

    feat: Introduce base set types and placeholders

--- src/qibocal/protocols/__init__.py
+++ src/qibocal/protocols/__init__.py
@@ -1,3 +1,11 @@
+from typing import Any, Optional
+
+from qibolab import Platform
+
+from qibocal.auto.mode import AUTOCALIBRATION, ExecutionMode
+from qibocal.auto.operation import Data, OperationId, Parameters, Results, Routine
+from qibocal.auto.task import Action, Completed, Id, Targets, Task
+
 from . import (
     allxy,
     classification,
@@ -57,3 +65,59 @@ __all__ += ["resonator_spectroscopies"]
 __all__ += ["qubit_spectroscopies"]
 __all__ += ["two_qubit_interaction"]
 __all__ += ["twpa"]
+
+
+class CalibrationProtocol:
+    """Protocol to calibrate a chip."""
+
+    def __call__(
+        self,
+        *args: Any,
+        parameters: Optional[dict] = None,
+        id: str = "",
+        mode: ExecutionMode = AUTOCALIBRATION,
+        update: bool = True,
+        targets: Optional[Targets] = None,
+        **kwargs,
+    ) -> Completed:
+        """Invoke calibration experiment.
+
+        The signature depend on the specific protocol. For the core set, the documentation
+        can be found at <url>. Otherwise compare the documentation for the Qibocal extension
+        providing the protocol invoked.
+        """
+        _ = self, args, parameters, id, mode, update, targets, kwargs
+
+        def _acquisition(x: Parameters) -> Data:
+            _ = x
+            return Data()
+
+        def _fit(x: Data) -> Results:
+            _ = x
+            return Results()
+
+        def _report(x: Data, y: Results) -> None:
+            _ = x, y
+
+        def _update(x: Results, y: Platform) -> None:
+            _ = x, y
+
+        return Completed(
+            task=Task(
+                action=Action(id=Id(""), operation=OperationId("")),
+                operation=Routine(_acquisition, _fit, _report, _update),
+            )
+        )
+
+
+_ph = CalibrationProtocol()
+"Placeholder."
+
+
+class BaseSet:
+    a: CalibrationProtocol = _ph
+    b: CalibrationProtocol = _ph
+    c: CalibrationProtocol = _ph
+
+
+PROTOCOLS: dict[str, Routine] = {}
