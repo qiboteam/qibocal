d556791c5140c59b6039a4dd8563b22bcaeb6ecd
Alessandro Candido <candido.ale@gmail.com>
2026-03-18 12:05:26 +0400

    feat: Expose hook for protocols' set extensions (aka plugins)

--- src/qibocal/auto/execute.py
+++ src/qibocal/auto/execute.py
@@ -2,14 +2,17 @@
 
 import importlib
 import importlib.util
+import operator
 import os
 import sys
 from contextlib import contextmanager
 from copy import deepcopy
-from dataclasses import dataclass, fields
+from dataclasses import fields
+from functools import reduce
 from pathlib import Path
-from typing import Optional, Union
+from typing import Any, Optional, Union
 
+from pydantic import BaseModel, ConfigDict, Field
 from qibo.backends import construct_backend
 
 from qibocal import protocols
@@ -18,7 +21,7 @@ from qibocal.config import log
 from ..calibration import CalibrationPlatform, create_calibration_platform
 from .history import History
 from .mode import AUTOCALIBRATION, ExecutionMode
-from .operation import Routine
+from .operation import ProtocolsCollection, Routine
 from .output import Metadata, Output
 from .task import Action, Completed, Targets, Task
 
@@ -63,9 +66,10 @@ def _register(name: str, obj):
     obj.__spec__ = None
 
 
-@dataclass
-class Executor:
+class Executor(BaseModel, protocols.BaseSet):
     """Execute a tasks' graph and tracks its history."""
+
+    model_config = ConfigDict(frozen=True, arbitrary_types_allowed=True)
 
     history: History
     """The execution history, with results and exit states."""
@@ -75,6 +79,8 @@ class Executor:
     """Qubits' platform."""
     update: bool = True
     """Runcard update mechanism."""
+    sources: list[ProtocolsCollection] = Field(default_factory=list)
+    """Sources to extend core protocol set."""
     name: Optional[str] = None
     """Symbol for the executor.
 
@@ -92,10 +98,15 @@ class Executor:
     path: Optional[Path] = None
     meta: Optional[Metadata] = None
 
-    def __post_init__(self):
+    def model_post_init(self, context: Any) -> None:
         """Register as a module, if a name is specified."""
         if self.name is not None:
             _register(self.name, self)
+        _ = context
+
+        protocols_ = reduce(operator.or_, [protocols.PROTOCOLS] + self.sources)
+        for name, protocol in protocols_.items():
+            object.__setattr__(self, name, self.wrapped_protocol(protocol))
 
     @classmethod
     def create(cls, name: str, platform: Union[CalibrationPlatform, str, None] = None):
@@ -206,13 +217,13 @@ class Executor:
         """
 
         def wrapper(
-            *args,
+            *args: Any,
             parameters: Optional[dict] = None,
             id: str = operation,
             mode: ExecutionMode = AUTOCALIBRATION,
             update: bool = True,
             targets: Optional[Targets] = None,
-            **kwargs,
+            **kwargs: Any,
         ):
             positional = dict(
                 zip((f.name for f in fields(protocol.parameters_type)), args)
