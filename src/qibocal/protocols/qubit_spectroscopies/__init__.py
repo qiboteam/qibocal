461cfd17f628609ca0d95e15f554190b27f77ed2
lballerio <lorenzo@ballerio.com>
2026-04-17 12:57:27 +0400

    adding conditional spectroscopy in inti file

--- src/qibocal/protocols/qubit_spectroscopies/__init__.py
+++ src/qibocal/protocols/qubit_spectroscopies/__init__.py
@@ -1,5 +1,11 @@
 from .qubit_power_spectroscopy import qubit_power_spectroscopy
 from .qubit_spectroscopy import qubit_spectroscopy
+from .qubit_spectroscopy_conditional import qubit_conditional_spectroscopy
 from .qubit_spectroscopy_ef import qubit_spectroscopy_ef
 
-__all__ = ["qubit_power_spectroscopy", "qubit_spectroscopy", "qubit_spectroscopy_ef"]
+__all__ = [
+    "qubit_power_spectroscopy",
+    "qubit_spectroscopy",
+    "qubit_spectroscopy_ef",
+    "qubit_conditional_spectroscopy",
+]
