96401050dcf16304830085531928ed0cbfadd780
Paul Tan <paultan_1@hotmail.com>
2026-05-22 13:55:02 +0800

    feat(coupler): add coupler bias tuning ramsey protocol

--- src/qibocal/protocols/two_qubit_interaction/chevron/__init__.py
+++ src/qibocal/protocols/two_qubit_interaction/chevron/__init__.py
@@ -2,5 +2,12 @@ from .chevron import chevron
 from .chevron_couplers import chevron_couplers
 from .chevron_signal import chevron_signal
 from .coupler_amplitude import coupler_amplitude
+from .coupler_ramsey import coupler_ramsey
 
-__all__ = ["chevron", "chevron_signal", "chevron_couplers", "coupler_amplitude"]
+__all__ = [
+    "chevron",
+    "chevron_signal",
+    "chevron_couplers",
+    "coupler_amplitude",
+    "coupler_ramsey",
+]
