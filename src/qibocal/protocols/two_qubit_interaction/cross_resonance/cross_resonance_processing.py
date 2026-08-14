1e8585f53464dd522344d29107371a4b4483c395
lballerio <lorenzo@ballerio.com>
2026-08-14 13:18:02 +0400

    applying changes after @alecandido review

--- src/qibocal/protocols/two_qubit_interaction/cross_resonance/cross_resonance_processing.py
+++ src/qibocal/protocols/two_qubit_interaction/cross_resonance/cross_resonance_processing.py
@@ -409,13 +409,11 @@ def tomography_cr_fit(
                 ]
             ).reshape(len(Basis), -1)
 
-            total_omega_guess = float(
-                np.median(
+            total_omega_guess = np.median(
                     quinn_fernandes_algorithm(
                         concatenated_signal, vector_x, speedup_flag=True
                     )
-                )
-            )
+                ).tolist()
 
             if fit_with_evolution:
                 popt = dynamic_evolution_optimizer(
