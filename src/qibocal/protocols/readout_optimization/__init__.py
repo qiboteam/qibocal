7696b1a92e68ef6566a5ef9caed7ab4b974e59c6
lballerio <lorenzo@ballerio.com>
2026-09-07 18:16:55 +0400

    moving `readout_optimization` protocols inside `readout` folder.

--- src/qibocal/protocols/readout_optimization/__init__.py
+++
@@ -1,5 +0,0 @@
-from .amplitude_frequency_optimization import ro_amplitude_frequency
-from .amplitude_optimization import ro_amplitude
-from .frequency_optimization import ro_frequency
-
-__all__ = ["ro_amplitude", "ro_amplitude_frequency", "ro_frequency"]
